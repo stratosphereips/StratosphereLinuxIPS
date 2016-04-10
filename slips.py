066b8220e5df05fd1e06f226265c4a1b799d6e03
eldraco <eldraco@gmail.com>
2016-04-10 19:08:08 +0200

    Change the version number to 0.3.1
    By some mistake I had changed it back to 0.2

--- slips.py
+++ slips.py
@@ -15,7 +15,7 @@ from modules.markov_models_1 import __markov_models__
 from os import listdir
 from os.path import isfile, join
 
-version = '0.2alpha'
+version = '0.3.1alpha'
 
 ###################
 class Tuple(object):
