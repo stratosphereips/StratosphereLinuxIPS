759bd1465f18f133d65236c30753ef3b3e33db50
eldraco <eldraco@gmail.com>
2017-03-29 12:34:04 +0200

    Add deep_getsizeof to measure how much memory we use

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python -u
 # This file is part of the Stratosphere Linux IPS
 # See the file 'LICENSE' for copying permission.
-#Author: Ondrej Lukas - ondrej.lukas95@gmail.com, lukasond@fel.cvut.cz
+# Authors: 
+# Ondrej Lukas - ondrej.lukas95@gmail.com, lukasond@fel.cvut.cz
+# Sebastian Garcia - sebastian.garcia@agents.fel.cvut.cz, eldraco@gmail.com
 
 import signal
 import time
@@ -95,8 +97,29 @@ class WhoisHandler(object):
             f.write('{}___{}\n'.format(item[0],item[1]));
         f.close();
 
-
-
-
-
-
+def deep_getsizeof(o, ids):
+    """
+    https://code.tutsplus.com/tutorials/understand-how-much-memory-your-python-objects-use--cms-25609
+    Find the memory footprint of a Python object
+    This is a recursive function that drills down a Python object graph
+    like a dictionary holding nested dictionaries with lists of lists
+    and tuples and sets.
+    The sys.getsizeof function does a shallow size of only. It counts each
+    object inside a container as pointer only regardless of how big it
+    really is.
+    :param o: the object
+    :param ids:
+    :return:
+    """
+    d = deep_getsizeof
+    if id(o) in ids:
+        return 0
+    r = getsizeof(o)
+    ids.add(id(o))
+    if isinstance(o, str) or isinstance(0, unicode):
+        return r
+    if isinstance(o, Mapping):
+        return r + sum(d(k, ids) + d(v, ids) for k, v in o.iteritems())
+    if isinstance(o, Container):
+        return r + sum(d(x, ids) for x in o)
+    return r 
