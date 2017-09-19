1299d12d257b31ea245021e53a86bf8db4f1be50
eldraco <eldraco@gmail.com>
2017-09-19 13:18:47 +0200

    Do not reblock if it is already block
    - Test with one ip

--- ip_blocker.py
+++ ip_blocker.py
@@ -1,14 +1,22 @@
 import sys, subprocess
 
-
 def add_reject_rule(address):
-	command = 'ipset add blacklist '+ address
-	subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
+    #command = 'ipset add blacklist '+ address
+    command_test = 'iptables -v -S FORWARD'
+    for line in command_test:
+        if '192.168.1.214' in line and 'REJECT' in line:
+            # We are already blocking
+            return True
+        elif:
+           command = 'iptables -I FORWARD 3 -s 192.168.1.213 -j REJECT'
+           subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
 
 def remove_reject_rule(address):
-	command = 'ipset del blacklist '+ address
-	subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
+    #command = 'ipset del blacklist '+ address
+    command = 'iptables -D FORWARD -s 192.168.1.213 -j REJECT'
+    subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
 
 if __name__ == '__main__':
-	#remove_reject_rule(sys.argv[1])
-	add_reject_rule(sys.argv[1])
+    #remove_reject_rule(sys.argv[1])
+    add_reject_rule(sys.argv[1])
+
