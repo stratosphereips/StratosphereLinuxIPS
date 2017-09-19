17a3ee4518492e213250f2454e58c3a47e691526
eldraco <eldraco@gmail.com>
2017-09-19 17:42:59 +0200

    Good version. Now its is deleting the rules and not adding new

--- ip_blocker.py
+++ ip_blocker.py
@@ -4,12 +4,14 @@ from subprocess import Popen, PIPE
 def add_reject_rule(address):
     #command = 'ipset add blacklist '+ address
     command_test = 'iptables -v -S FORWARD'
-    results = Popen(command_test, shell=True, stdin=PIPE, stdout=PIPE).communicate()[0].split('\n')
+    results = Popen(command_test, shell=True, stdin=PIPE, stdout=PIPE).communicate()
     for line in results:
         if '192.168.1.213' in line and 'REJECT' in line:
             # We are already blocking
+            print 'already blocked'
             return True
         else:
+            print 'block!'
             command = 'iptables -I FORWARD 3 -s 192.168.1.213 -j REJECT'
             subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
             return True
@@ -20,6 +22,5 @@ def remove_reject_rule(address):
     subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
 
 if __name__ == '__main__':
-    #remove_reject_rule(sys.argv[1])
-    add_reject_rule(sys.argv[1])
-
+    remove_reject_rule(sys.argv[1])
+    #add_reject_rule(sys.argv[1])
