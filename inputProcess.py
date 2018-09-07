a6bd09906b1b05d72edd7737a7410334bcf03747
eldraco <eldraco@gmail.com>
2018-09-07 22:54:51 +0200

    Read files from disk and also read stdin

--- inputProcess.py
+++ inputProcess.py
@@ -5,7 +5,7 @@ import sys
 # Input Process
 class InputProcess(multiprocessing.Process):
     """ A class process to run the process of the flows """
-    def __init__(self, inputqueue, outputqueue, profilerqueue, verbose, debug, stdin, config):
+    def __init__(self, inputqueue, outputqueue, profilerqueue, verbose, debug, input, config):
         multiprocessing.Process.__init__(self)
         self.verbose = verbose
         self.debug = debug
@@ -13,21 +13,42 @@ class InputProcess(multiprocessing.Process):
         self.outputqueue = outputqueue
         self.profilerqueue = profilerqueue
         self.config = config
-        self.stdin = stdin
+        self.input = input
 
     def run(self):
         try:
-            while True:
-                # While the communication queue is empty
-                if self.inputqueue.empty():
-                        for line in self.stdin:
-                            self.profilerqueue.put(line)
-                else:
-                    # The communication queue is not empty process
-                    line = self.inputqueue.get()
-                    if 'stop' == line:
-                        print('Stopping Input Process')
-                        return True
+            if type(self.input) == str:
+                filed = open(self.input)
+                try:
+                    line  = filed.readline()
+                except EOFError:
+                    return True
+                while True:
+                    # While the communication queue is empty
+                    if self.inputqueue.empty():
+                        self.profilerqueue.put(line)
+                        try:
+                            line  = filed.readline()
+                        except EOFError:
+                            return True
+                    else:
+                        # The communication queue is not empty process
+                        line = self.inputqueue.get()
+                        if 'stop' == line:
+                            print('Stopping Input Process')
+                            return True
+            else:
+                while True:
+                    # While the communication queue is empty
+                    if self.inputqueue.empty():
+                            for line in self.input:
+                                self.profilerqueue.put(line)
+                    else:
+                        # The communication queue is not empty process
+                        line = self.inputqueue.get()
+                        if 'stop' == line:
+                            print('Stopping Input Process')
+                            return True
         except KeyboardInterrupt:
             return True
         except Exception as inst:
