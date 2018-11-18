f8ed5d8e65b48dec2292c07ad622ae094beb0bf7
eldraco <eldraco@gmail.com>
2018-11-18 16:09:38 +0100

    Add timewindows basic support in the db

--- slips/core/database.py
+++ slips/core/database.py
@@ -12,6 +12,7 @@ r = redis.Redis(connection_pool=pool)
 # Struture of the DB
 # Set 'profile'
 #  Holds a set of all profile ids
+# For each profile, there is a set for the timewindows. The name of the sets is the names of profiles
 
 class Database(object):
     """ Database object management """
@@ -28,14 +29,49 @@ class Database(object):
 
     def getProfiles(self):
         """ Get a list of all the profiles """
-        return self.r.smembers('profiles')
+        profiles = self.r.smembers('profiles')
+        if profiles != set():
+            return profiles
+        else:
+            return False
 
     def hasProfile(self, profileid):
         """ Check if we have the given profile """
         return self.r.sismember('profiles', profileid)
 
-    def getProfilesLen(self ):
+    def getProfilesLen(self):
         """ Return the amount of profiles. Redis should be faster than python to do this count """
-        return self.r.scard('profiles')
+        return self.r.scard('profiles') 
+   
+    def getLastTWforProfile(self, profilename):
+        """ Return the last TW id for the given profile id """
+        return self.r.zrange(profilename, -1, -1)
+
+    def addNewTW(self, profilename, twid, startoftw, width):
+        """ Add the twid to the ordered set of a given profile """
+        self.r.zadd(profilename, float(startoftw), twid)
+        # Create the hashmap of this TW, add the width as an int
+        self.r.hset(profilename + ':' + twid, 'width', int(width))
+
+    def getAmountTW(self, profilename):
+        """ Return the amount of tw for this profile id """
+        return self.r.zcard(profilename)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 __database__ = Database()
