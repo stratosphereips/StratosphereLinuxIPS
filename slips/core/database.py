a8ed70cae3c3e406bfb3f70da6ac04c12565e688
eldraco <eldraco@gmail.com>
2018-11-18 12:52:21 +0100

    Database management using Redis

---
+++ slips/core/database.py
@@ -0,0 +1,37 @@
+import redis
+
+"""
+Connection Pools
+Behind the scenes, redis-py uses a connection pool to manage connections to a Redis server. By default, each Redis instance you create will in turn create its own connection pool. You can override this behavior and use an existing connection pool by passing an already created connection pool instance to the connection_pool argument of the Redis class. You may choose to do this in order to implement client side sharding or have finer grain control of how connections are managed.
+
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+r = redis.Redis(connection_pool=pool)
+"""
+
+
+# Struture of the DB
+# Set 'profile'
+#  Holds a set of all profile ids
+
+class Database(object):
+    """ Database object management """
+
+    def __init__(self):
+        # Get the connection to redis database
+        self.r = redis.StrictRedis(host='localhost', port=6379, db=0) #password='password')
+        # For now, do not remember between runs of slips. Just delete the database when we start
+        self.r.flushdb()
+
+    def addProfile(self, profileid):
+        """ Add a new profile to the DB."""
+        self.r.sadd('profiles', str(profileid))
+
+    def getProfiles(self):
+        """ Get a list of all the profiles """
+        return self.r.smembers('profiles')
+
+    def hasProfile(self, profileid):
+        """ Check if we have the given profile """
+        return self.r.sismember('profiles', profileid)
+
+__database__ = Database()
