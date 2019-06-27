f6ae1f698a800d775ec47e408e67c38b5d0867e6
Dita Hollmannová <hollmdit@fel.cvut.cz>
2019-06-27 16:11:49 +0200

    Module now listens for new IPs

---
+++ modules/avirustotal/avirustotal.py
@@ -0,0 +1,65 @@
+# Must imports
+from slips.common.abstracts import Module
+import multiprocessing
+from slips.core.database import __database__
+
+# Your imports
+import time
+
+
+class Module(Module, multiprocessing.Process):
+    # Name: short name of the module. Do not use spaces
+    name = 'aVirusTotal'
+    description = 'IP address lookup on VirusTotal'
+    authors = ['Dita']
+
+    def __init__(self, outputqueue, config):
+        multiprocessing.Process.__init__(self)
+        # All the printing output should be sent to the outputqueue, which is connected to OutputProcess
+        self.outputqueue = outputqueue
+        # In case you need to read the slips.conf configuration file for your own configurations
+        self.config = config
+        # Start the DB
+        __database__.start(self.config) # TODO: What does this line do? It changes nothing.
+        # To which channels do you wnat to subscribe? When a message arrives on the channel the module will wake up
+        # The options change, so the last list is on the slips/core/database.py file. However common options are:
+        # - new_ip
+        # - tw_modified
+        # - evidence_added
+        self.c1 = __database__.subscribe('new_ip')
+        print("VT", self.c1)
+
+    def print(self, text, verbose=1, debug=0):
+        """ 
+        Function to use to print text using the outputqueue of slips.
+        Slips then decides how, when and where to print this text by taking all the processes into account
+
+        Input
+         verbose: is the minimum verbosity level required for this text to be printed
+         debug: is the minimum debugging level required for this text to be printed
+         text: text to print. Can include format like 'Test {}'.format('here')
+
+        If not specified, the minimum verbosity level required is 1, and the minimum debugging level is 0
+        """
+
+        vd_text = str(int(verbose) * 10 + int(debug))
+        self.outputqueue.put(vd_text + '|' + self.name + '|[' + self.name + '] ' + str(text))
+
+    def run(self):
+        try:
+            # Main loop function
+            while True:
+                message = self.c1.get_message(timeout=-1)
+                # Check that the message is for you. Probably unnecessary...
+                # Ignore the first message
+                if message['channel'] == 'new_ip' and message["type"] == "message":
+                    self.print('New IP: {}'.format(message["data"]))
+
+        except KeyboardInterrupt:
+            return True
+        except Exception as inst:
+            self.print('Problem on the run()', 0, 1)
+            self.print(str(type(inst)), 0, 1)
+            self.print(str(inst.args), 0, 1)
+            self.print(str(inst), 0, 1)
+            return True
