8fd47dba40e4d005c5ab064ac8d6adc9ceec798a
Dita Hollmannová <hollmdit@fel.cvut.cz>
2019-07-02 10:55:58 +0200

    Created demo function to process datasets, added comments

--- modules/avirustotal/vt_api_test.py
+++ modules/avirustotal/vt_api_test.py
@@ -3,6 +3,7 @@ import json
 import re
 import socket
 import time
+import os
 import numpy as np
 from api_key import key
 from matplotlib import pyplot as plt
@@ -167,65 +168,131 @@ def count_positives(response, response_key, positive_key, total_key):
 
 
 def check_ip_from_file(ip):
+    """
+    Loads json data from the file ip.txt and computes the score for this ip
+    :param ip: IP address to work with
+    :return: None
+    """
     filename = ip + ".txt"
     if filename:
         with open(filename, 'r') as f:
-            datastore = json.load(f)
-            print(interpret_response(datastore))
+            data = json.load(f)
+            print(interpret_response(data))
 
 
-def process_ips_in_file(filename):
-    with open(filename, 'r') as f:
-        with open(filename + ".out", 'a') as o:
+def dataset_demo(benign_infile, benign_outfile, b_is_ips, malicious_infile, malicious_outfile, m_is_ips, call_api=True):
+    """
+    Demo script to visualise the dataset. API will be called on all records in both input files (this can be skipped if
+    scores were computed previously). Score data is then loaded and data is visualised in four histograms.
+    :param benign_infile: input file with benign addresses
+    :param benign_outfile: output file to store scores for benign addresses
+    :param b_is_ips: True if benign file contains IPs, False if it contains hostnames
+    :param malicious_infile: input file with malicious addresses
+    :param malicious_outfile: output file to store scores for malicious addresses
+    :param m_is_ips: True if malicious file contains IPs, False if it contains hostnames
+    :param call_api: True by default. If the API call was done already, you can set this to False and work with outfiles
+    :return: None
+    """
+
+    if call_api:
+        process_input_file(benign_infile, benign_outfile, b_is_ips)
+        process_input_file(malicious_infile, malicious_outfile, m_is_ips)
+
+    benign_data = read_score_from_file(benign_outfile)
+    malicious_data = read_score_from_file(malicious_outfile)
+
+    show_histograms(benign_data, malicious_data)
+
+
+def process_input_file(infile, outfile, is_ips):
+    """
+    Reads hosts from a file and fetches their score from virustotal.
+    :param infile: On each line of the input file, a hostname or IP is expected. These addresses will be read (host
+    names are resolved first) and then the IP is checked against VT. A 4-tuple score is returned, as computed by
+    VTTest:interpret_response. The query will pause if max. API request rate is reached. Any unresolvable hostnames
+    will be skipped (you will be notified in stdout).
+    :param outfile: The output file will be deleted if found, and new one will be created. Lines will be appended to
+    the end of the file, one line per each ip/resolvable hostname. 
+    :param is_ips: True if input file contains IP addresses, False for file with hostnames.
+    :return: None
+    """
+
+    def get_score_for_ip(vt_test, ip):
+        return vt_test.check_ip(ip)
+
+    def get_score_for_hostname(vt_test, hostname):
+        ip = socket.gethostbyname(hostname)
+        return vt_test.check_ip(ip)
+
+    # use different function to compute the score based on input format
+    if is_ips:
+        get_score = get_score_for_ip
+    else:
+        get_score = get_score_for_hostname
+
+    if os.path.exists(outfile):
+        os.remove(outfile)
+    with open(infile, 'r') as f:
+        with open(outfile, 'a') as o:
             lines = f.read().split("\n")
-            vt = VTTest()
-            for l in lines:
-                scores = vt.check_ip(l)
-                o.write(str(scores) + "\n")
-
-
-def process_hostnames_in_file(filename):
-    with open(filename, 'r') as f:
-        with open(filename + ".out", 'a') as o:
-            lines = f.read().split("\n")
+            lines.remove("")
             vt = VTTest()
             for l in lines:
                 try:
-                    ip = socket.gethostbyname(l)
-                    scores = vt.check_ip(ip)
+                    scores = get_score(vt, l)
+                    # print("Line", l, "resolved to", scores)
                     o.write(str(scores) + "\n")
                 except socket.gaierror:
+                    # unresolvable hostnames will throw this error, ignore those
                     print("Service " + l + " not known")
 
 
-def read_score_from_file(filename):
+def read_score_from_file(filename, verbose=False):
+    """
+    Take a human readable file with scores and load the numbers into numpy array.
+    :param filename: file with computed scores to read
+    :param verbose: if set to true, basic stats will be printed (mean, min and max)
+    :return: numpy array, 4 columns, one row for each score line
+    """
     with open(filename, 'r') as f:
         lines = f.read().split("\n")
         lines = list(map(lambda s: s.replace("(", ""), lines))
         lines = list(map(lambda s: s.replace(")", ""), lines))
         lines = list(map(lambda s: s.split(","), lines))
         lines = list(map(lambda s: list(map(float, s)), lines[:-1]))
-        import numpy as np
 
         data = np.array(lines)
-        print(np.mean(data, 0))
-        print(np.max(data, 0))
-        print(np.min(data, 0))
+
+        if verbose:
+            print("Mean value:", np.mean(data, 0))
+            print("Max value:", np.max(data, 0))
+            print("Min value:", np.min(data, 0))
 
         return data
 
 
 def show_histograms(benign, malicious):
-    b_urls = benign[:, 3]
-    m_urls = malicious[:, 3]
-    b, bins = np.histogram(b_urls)
-    m, _ = np.histogram(m_urls, bins=bins)
-
+    """
+    Visualise four histograms, one for each category of scores. The input arrays must have 4 columns, number of rows
+    do not need to match
+    :param benign: np.ndarray of benign scores
+    :param malicious: np.ndarray of malicious scores
+    :return: None
+    """
     bins = np.linspace(0, 0.12, 20)
+    titles = ["Resolved URLs", "Downloaded files", "Referring files", "Communicating files"]
 
-    plt.hist(b_urls, bins, alpha=0.5, label='Benign', color="red")
-    plt.hist(m_urls, bins, alpha=0.5, label='Malicious', color="blue")
-    plt.legend(loc='upper right')
+    for i in range(0, 4):
+        b_urls = benign[:, i]
+        m_urls = malicious[:, i]
+
+        subplot_id = 221 + i
+
+        plt.subplot(subplot_id)
+        plt.hist(b_urls, bins, alpha=0.5, label='Benign', color="blue")
+        plt.hist(m_urls, bins, alpha=0.5, label='Malicious', color="red")
+        plt.title(titles[i])
+        plt.legend(loc='upper right')
     plt.show()
 
 
@@ -241,9 +308,13 @@ if __name__ == "__main__":
     # check_ip_from_file("216.58.201.78")  # google.com
     # check_ip_from_file("47.88.158.115")  # laurengraham.com (malicious)
 
-    process_ips_in_file("data/malicious-ips2.txt")
+    # process_ips_in_file("data/malicious-ips2.txt")
     # process_hostnames_in_file("data/hostnames")
     # malicious_data = read_score_from_file("data/scores-malicious.txt")
     # benign_data = read_score_from_file("data/scores-benign.txt")
 
     # show_histograms(benign_data, malicious_data)
+
+    dataset_demo("data/demo/benign_in", "data/demo/benign_out", False,
+                 "data/demo/malicious_in", "data/demo/malicious_out", True,
+                 call_api=False)
