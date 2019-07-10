245731565f84f8ab37b592c677586008b4606281
Dita Hollmannová <hollmdit@fel.cvut.cz>
2019-07-10 17:13:23 +0200

    Unstable changes in ipv4 caching

--- modules/whoisip/test_whoisip.py
+++ modules/whoisip/test_whoisip.py
@@ -18,5 +18,29 @@ def run(infile, outfile):
                 wi.check_ip(l)
 
 
+def try_random_addresses():
+    import random
+    wi = WhoisIP(None, get_default_config(), testing=True)
+    while True:
+        ip = str(random.randint(0, 255)) + "." + str(random.randint(0, 255)) + "."\
+             + str(random.randint(0, 255)) + "." + str(random.randint(0, 255))
+        wi.check_ip(ip)
+
+
+def test_tricky(ips):
+    ips = []
+    ips.append("71.163.76.208")  # there is a list of cidrs
+    ips.append("223.43.28.222")  # Korean whois with encoding errs
+
+     # TODO: Tomorrow check this:
+     # 21) "4294959104"
+     # 22) "{\"asn\": \"17964\", \"country\": \"CN\", \"cidr\": \"101.246.0.0/19\", \"name\": \"GHSX_NET\"}"
+     # 23) "4294966784"
+     # 24) "{\"asn\": \"8517\", \"country\": \"TR\", \"cidr\": \"193.140.124.0/23\", \"name\": \"OGU-NET\"}"
+     # 25) "4294966272"
+     # 26) "{\"asn\": \"8517\", \"country\": \"TR\", \"cidr\": \"193.140.120.0/22\", \"name\": \"OGU-NET\"}"
+
+
 if __name__ == "__main__":
-    run("modules/whoisip/data/malicious_ips.txt", "modules/whoisip/data/malicious_ips_out.txt")
+    # run("modules/whoisip/data/malicious_ips.txt", "modules/whoisip/data/malicious_ips_out.txt")
+    try_random_addresses()
