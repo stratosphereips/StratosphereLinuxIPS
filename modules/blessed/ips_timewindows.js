7a2ecde79f2d92b5189e57b53ca0daabf5d3a786
Kamila Babayeva <kamifai14@gmail.com>
2019-08-03 13:08:31 +0200

    Update for Node js version 10

--- modules/blessed/ips_timewindows.js
+++ modules/blessed/ips_timewindows.js
@@ -25,7 +25,7 @@ const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table,
   , label: "Timeline"
   , columnWidth:[200]})
 
-  ,table_outTuples =  grid.set(2.5,1,1.8,2.5, contrib.table, 
+  ,table_outTuples =  grid.set(2.5,1,1.7,2.5, contrib.table, 
   { keys: true
   , fg: 'green'
   , label: "OutTuples"
@@ -36,7 +36,7 @@ const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table,
   , template: { lines: true }
   , label: 'Ips from slips'})
 
-  , box_ip = grid.set(5.1, 0, 0.5, 6, blessed.box,
+  , box_ip = grid.set(5.1, 0, 0.5, 3, blessed.box,
       {top: 'center',
       left: 'center',
       width: '50%',
@@ -53,7 +53,7 @@ const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table,
       },
     }})
 
- , box_detections = grid.set(4.2, 1, 1, 2.5,blessed.box,{
+ , box_detections = grid.set(4.2, 1, 0.9, 2.5,blessed.box,{
   		top: 'center',
   		left: 'center',
   		width: '50%',
@@ -75,7 +75,7 @@ const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table,
     	}
   		}
 	})
- , bar = grid.set(2.5,3.5,2.7,2.5,contrib.stackedBar,
+ , bar = grid.set(2.5,3.5,2.6,2.5,contrib.stackedBar,
        { label: 'Connection Port Established'
        , barWidth: 3
        , barSpacing: 5
@@ -96,9 +96,16 @@ var stacked_bar = new interface_element(0)
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 
+// var pic = grid.set(5.1, 2, 1, 0.5,contrib.picture,
+//    { file: 'greece.png'
+//    , cols: 20
+//    , onReady: ready})
+// function ready() { screen.render() }
+
 
 
  function timewindows_list_per_ip(tw){
+
  	/*
  	create a list of dictionaries with tws(to set the tree). [{'tw1':{},'tw2':{}"}]
  	*/
@@ -132,18 +139,20 @@ function getIpInfo(ip){
 
 
 
-function set_tree_data(ips_with_profiles, timewindows_list){
+function set_tree_data(timewindows_list){
 	/*
 	sets ips and their tws for the tree.
 	*/
+  var ips_with_profiles = Object.keys(timewindows_list);
   var explorer = { extended: true
   , children: function(self){
       var result = {};
       try {
         if (!self.childrenContent) {
           for(i=0;i<ips_with_profiles.length;i++){
+          	var tw = timewindows_list[ips_with_profiles[i]]
             child = ips_with_profiles[i];
-            result[child] = { name: child, extended:false, children: timewindows_list[i]};
+            result[child] = { name: child, extended:false, children: tw[0]};
             }
         }else
         result = self.childrenContent;
@@ -155,38 +164,55 @@ return explorer;};
 
 async.waterfall([
 	/*async_waterfall to fill the data for the tree*/
-
-	
 	function get_IPs_with_profiles(callback){
 		/*
 		retrieve ips with profile from redis key 'Profiles'
 		*/
 		var ips_with_profiles = [];
+
 		redis_ips_with_profiles.smembers("profiles", (err,reply)=>{
 		if(err){callback(err)}
-		for(i=0; i<reply.length; i++){
- 			ips_with_profiles.push(reply[i].split('_')[1]);}
- 			callback(null, ips_with_profiles);
+		async.each(reply, function(ip_profile, callback) {
+			ips_with_profiles.push(ip_profile.split('_')[1]);
+
+			callback(null)
+		}, function(err) {
+
+		 if( err ) {
+		 	console.log('unable to create user');
+		 }else {
+		 	callback(null, ips_with_profiles);
+		 }
+		});
+
 })},
 
 	function get_tws_for_ips(ips_with_profiles, callback){
+		var tree_dict = {};
+		function createUser(ip_profile, reply, callback)
+		{
+			tree_dict[ip_profile]=timewindows_list_per_ip(reply);	
+		 	callback(null);
+		}
+		async.each(ips_with_profiles, function(ip_profile, callback) {
+		redis_tws_for_ip.zrangebyscore("twsprofile_"+ip_profile,
+		   			Number.NEGATIVE_INFINITY,Number.POSITIVE_INFINITY, (err,reply)=>{
+		    			if(err){
+		      				callback(err);
+		   				}else{
+		   					createUser(ip_profile,reply, callback);
+		        	}})
+		}, function(err,res) {
+		 if( err ) {
+		 console.log('unable to create user');
+		 } else {		 
+		 callback(null,  tree_dict);
 
-		var timewindows_list = [];
-		for(i=0; i<ips_with_profiles.length;i++){
+		 }
+		})}, 
 
-    		redis_tws_for_ip.zrangebyscore("twsprofile_"+ips_with_profiles[i],
-   			Number.NEGATIVE_INFINITY,Number.POSITIVE_INFINITY, (err,reply)=>{
-    		if(err){
-      			callback(err);
-   			}else{
-   				timewindows_list.push(...timewindows_list_per_ip(reply));
-        		
-        	}});}
-    callback(null, ips_with_profiles, timewindows_list);
-  }, 
-
-  	function setTree(ips_with_profiles,timewindows_list,callback){
-  		tree.setData(set_tree_data(ips_with_profiles,timewindows_list));
+  	function setTree(timewindows_list,callback){
+  		tree.setData(set_tree_data(timewindows_list));
   		screen.render();
   		callback(null)
   	}
@@ -203,25 +229,33 @@ tree.on('select',function(node){
     	box_detections.setContent(reply)
     })
     redis_outtuples_timewindow.hgetall("profile_"+node.parent.name+"_"+node.name, (err,reply)=>{
-        if(reply == null){return;}
-	    const mapOutTuples = async _ => {
+        if(reply == null){
+        	table_outTuples.setData({headers: [''], data: []})
+        	return;}
+	    
 	    var obj_outTuples = JSON.parse(reply["OutTuples"])
 	    var keys = Object.keys(obj_outTuples);
 	    var data = [];
-	    const promises_outTuples = keys.map(async key => {
+
+	    async.each(keys, function(key,callback){
 	      var row = [];
 	      var tuple_info = obj_outTuples[key]
 	      row.push(key,tuple_info[0].trim());
 	      data.push(row);
-  		})
-  		await Promise.all(promises_outTuples)
-  		table_outTuples.setData({headers: [''], data: data});
-		screen.render();}
+	      callback(null);
 
-		mapOutTuples()
+  		},function(err) {
+ 		if( err ) {
+			console.log('unable to create user');
+ 		} else {
+ 			table_outTuples.setData({headers: [''], data: data});
+		screen.render();
+ 			
+ 		}
+		});
+  		
+  		
 
-		// const mapTCPEstablishe
-		// stackedbar();
 		function stackedbar(first, second){
 		const mapTCPEstablished = async () => {
 		var bar_categories_protocol_port = []
@@ -276,19 +310,19 @@ tree.on('select',function(node){
         , data: data_tcp_est
         })
 		screen.render();}
-		mapTCPEstablished()}
-		screen.key(['e'], function(ch, key) {
-			if(stacked_bar.state == 0){
-				stackedbar("DstPortsClientTCPEstablished","DstPortsClientUDPEstablished");
-				stacked_bar.state=1;}
-			else{
-				stackedbar("DstIPsClientTCPEstablished","DstIPsClientUDPEstablished");
-				stacked_bar.state = 0;
+		mapTCPEstablished()};
+// 		  screen.key(['e'], function(ch, key) {
+// 			if(stacked_bar.state == 0){
+// 				stackedbar("DstPortsClientTCPEstablished","DstPortsClientUDPEstablished");
+// 				stacked_bar.state=1;}
+// 			else{
+// 				stackedbar("DstIPsClientTCPEstablished","DstIPsClientUDPEstablished");
+// 				stacked_bar.state = 0;
 
-			}
+// 			}
 
   
-});
+// });
     	stackedbar("DstIPsClientTCPEstablished","DstIPsClientUDPEstablished");
     }) 
 
