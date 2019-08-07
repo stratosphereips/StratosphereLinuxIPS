a1036c9162dc56296e6c270d1e6bf3f165492237
Kamila Babayeva <kamifai14@gmail.com>
2019-08-07 22:06:04 +0200

    updated tree, created map, new stacked bars

--- modules/blessed/ips_timewindows.js
+++ modules/blessed/ips_timewindows.js
@@ -1,5 +1,6 @@
-const redis = require('redis')
+var redis = require('redis')
   , redis_ips_with_profiles = redis.createClient()
+  , redis_tree = redis.createClient()
   , redis_tws_for_ip = redis.createClient()
   , redis_ip_info = redis.createClient()
   , redis_get_timeline = redis.createClient()
@@ -9,20 +10,39 @@ const redis = require('redis')
   , async = require('async')
   ,	blessed = require('blessed')
   , contrib = require('blessed-contrib')
+  ,  fs = require('fs')
   , screen = blessed.screen()
+
+//read countries  location
+let country_loc = {};
+fs.readFile('country.txt', 'utf8', function(err,data) {
+    if(err) throw err;
+    
+    let splitted = data.toString().split(";");
+    for (let i = 0; i<splitted.length; i++) {
+        let splitLine = splitted[i].split(":");
+        try{
+        country_loc[splitLine[0]] = splitLine[1].split(",");}
+        catch(err){
+        }
+    }
+});
 
 
 // set up elements of the interface.
 
-const grid = new contrib.grid({
+var grid = new contrib.grid({
   rows: 6,
   cols: 6,
   screen: screen
 });
-const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table, 
+var table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table, 
   { keys: true
   , fg: 'green'
   , label: "Timeline"
+  , vi:true
+  , keys:true
+  , mouse: true
   , columnWidth:[200]})
 
   ,table_outTuples =  grid.set(2.5,1,1.7,2.5, contrib.table, 
@@ -31,12 +51,12 @@ const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table,
   , label: "OutTuples"
   , columnWidth:[25,30]})
 
-  , tree =  grid.set(0,0,5.1,1,contrib.tree,
+  , tree =  grid.set(0,0,5,1,contrib.tree,
   { style: { text: "red" }
   , template: { lines: true }
   , label: 'Ips from slips'})
 
-  , box_ip = grid.set(5.1, 0, 0.5, 3, blessed.box,
+  , box_ip = grid.set(5, 0, 0.5, 3.5, blessed.box,
       {top: 'center',
       left: 'center',
       width: '50%',
@@ -59,14 +79,12 @@ const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table,
   		width: '50%',
   		height: '50%',
   		label:'Detections',
-  		// content:'',
   		tags: true,
  		border: {
    		type: 'line'
  		},
  		style: {
     	fg: 'white',
-    	// bg: 'magenta',
     	border: {
       	fg: '#f0f0f0'
     	},
@@ -75,32 +93,237 @@ const table_timeline =  grid.set(0, 1, 2.5, 5, contrib.table,
     	}
   		}
 	})
- , bar = grid.set(2.5,3.5,2.6,2.5,contrib.stackedBar,
-       { label: 'Connection Port Established'
-       , barWidth: 3
-       , barSpacing: 5
-       , xOffset: 2
-       , height: "90%"
-       , width: "100%"
-       , barBgColor: [ 'red', 'blue', 'green' ]})
 
-
-
-function interface_element(state){
-  this.state = state;
-  };
-
-var stacked_bar = new interface_element(0)
+,
+box_hotkeys = grid.set(2.5, 3.5, 0.8, 1, blessed.box,
+      {top: 'center',
+      left: 'center',
+      width: '50%',
+      height: '50%',
+      content: "{bold}-e{/bold} -> dstPortClient\n{bold}-b{/bold} -> dstPortServer\n{bold}-c{/bold} -> SrcPortsClient\n{bold}-m{/bold} -> map",
+      tags: true,
+      border: {
+      type: 'line'
+    },
+    style: {
+      fg: 'white',
+      border: {
+        fg: '#f0f0f0'
+      },
+    }})
+, map = grid.set(0, 0, 6, 6,contrib.map,{label: 'World Map'})
+map.hide()
 
  function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+};
+
+
+function ip_tcp_bars(key, key2,reply,bar){
+	if(reply[key]==null){
+ 		ip_udp_bars(key2,reply,[],[],bar);
+ 		return;
+ 	}
+
+ 	var bar_category_ips = [];
+ 	var data_stacked_bar = [];
+ 	try{
+	    	var obj_ip = JSON.parse(reply[key]);
+			var keys_ip = Object.keys(obj_ip);
+		}
+	catch(err){
+	    	var obj_ip = reply[key];	
+	    	var keys_ip = Object.keys(obj_ip);
+	    }
+	async.each(keys_ip, function(ip, callback) {
+		bar_category_ips.push('TCP/'+ip);
+		var ip_info = obj_ip[ip];
+		var row = [];
+		row.push(ip_info['totalflows'],ip_info['totalpkt']);
+		data_stacked_bar.push(row);
+		callback();
+	}, function(err){
+		if(err){
+			console.log('sasdfsaa')
+		}
+		else{
+			ip_udp_bars(key2,reply, bar_category_ips, data_stacked_bar,bar)
+
+		}
+	});
+
 }
 
-// var pic = grid.set(5.1, 2, 1, 0.5,contrib.picture,
-//    { file: 'greece.png'
-//    , cols: 20
-//    , onReady: ready})
-// function ready() { screen.render() }
+function ip_udp_bars(key,reply, bar_category_ips, data_stacked_bar,bar){
+	if(reply[key]==null){
+ 		set_stacked_bar_ip(data_stacked_bar,bar_category_ips,bar);
+ 		return;
+ 	}
+
+ 	try{
+	    	var obj_ip = JSON.parse(reply[key]);
+			var keys_ip = Object.keys(obj_ip);
+		}
+	catch(err){
+	    	var obj_ip = reply[key];	
+	    	var keys_ip = Object.keys(obj_ip);
+	    }
+	async.each(keys_ip, function(ip, callback) {
+		bar_category_ips.push('TCP/'+ip);
+		var ip_info = obj_ip[ip];
+		var row = [];
+		row.push(ip_info['totalflows'],ip_info['totalpkt']);
+		data_stacked_bar.push(row);
+		callback();
+	}, function(err){
+		if(err){
+			console.log('sasdfsaa')
+		}
+		else{
+			set_stacked_bar_ip(data_stacked_bar, bar_category_ips, bar)
+
+		}
+	});
+function set_stacked_bar_ip(data,bars,bar){
+			if(data.length==0){
+				bar.setData(
+        	{ barCategory: bars
+        	, stackedCategory: ['EMPTY']
+        	, data: data})
+			screen.render();
+
+			}
+			else{
+			bar.setData(
+        	{ barCategory: bars
+        	, stackedCategory: ['totalflows', 'totalpkt']
+        	, data: data})
+			screen.render();}
+		}
+
+}
+
+function setMap(ips){
+	
+	redis_ip_info.hgetall('IPsInfo', (err,reply)=>{
+		async.each(ips, function(ip,callback){
+			try{
+			var inf = JSON.parse(reply[ip])
+			}
+			catch(err){
+				return;
+			}
+			
+			var country = inf["geocountry"]
+			
+			geoloc = country_loc[" "+country]
+			
+			if(geoloc!=undefined && geoloc !='Private'){
+			
+			var loc =country_loc[" "+country]
+		 	map.addMarker({"lon" : loc[1], "lat" : loc[0], color: "red", char: "X" })}			
+			callback();
+
+
+	}, function(err){
+		if(err){
+			console.log(err)
+		};
+	
+	})
+
+
+	})
+	
+}
+ function tcp_connections(key, key2,reply,bar){
+ 	if(reply[key]==null){
+ 		udp_connections(key2,[],[],reply,bar);
+ 		return;
+ 	}
+		var bar_categories_protocol_port = [];
+		var data_stacked_bar = []
+  		
+  		try{
+	    	var obj_tcp = JSON.parse(reply[key]);
+			var keys_tcp = Object.keys(obj_tcp);
+		}
+	    catch(err){
+	    	var obj_tcp = reply[key];	
+	    	var keys_tcp = Object.keys(obj_tcp);
+
+	    }
+
+  		async.each(keys_tcp, function(key_TCP_est, callback) {
+  			bar_categories_protocol_port.push('TCP/'+key_TCP_est);
+	    	var service_info = obj_tcp[key_TCP_est];
+	    	var row = [];
+	    	row.push(round(Math.log(service_info['totalflows']),0), round(Math.log(service_info['totalpkt']),0), round(Math.log(service_info['totalbytes']),0));
+	    	data_stacked_bar.push(row);
+	    	
+	    	callback();
+	    	}, function(err) {
+
+ 		if( err ) {
+ 			console.log('unable to create user');
+ 		} else {
+ 			
+ 			udp_connections(key2,data_stacked_bar,bar_categories_protocol_port,reply,bar);
+		}
+		});}
+
+
+		function udp_connections(key,data_stacked_bar,bar_categories_protocol_port,reply,bar){
+			if(reply[key]==null){
+ 		set_stacked_bar(data_stacked_bar,bar_categories_protocol_port,bar)
+ 		return;
+ 	}
+  		
+  		try{
+	    	var obj_udp = JSON.parse(reply[key]);
+			var keys_udp = Object.keys(obj_udp);
+		}
+	    catch(err){
+	    	var obj_udp = reply[key];
+	    	var keys_udp = Object.keys(obj_udp);
+
+	    }
+
+  		async.each(keys_udp, function(key_UDP_est, callback) {
+  			bar_categories_protocol_port.push('UDP/'+key_UDP_est);
+	    	var service_info = obj_udp[key_UDP_est];
+	    	var row = [];
+	    	row.push(round(Math.log(service_info['totalflows']),0), round(Math.log(service_info['totalpkt']),0), round(Math.log(service_info['totalbytes']),0));
+	    	data_stacked_bar.push(row);
+	    	callback()
+		}, function(err) {
+
+ 		if( err ) {
+ 			console.log('unable to create user');
+ 		} else {
+ 			set_stacked_bar(data_stacked_bar,bar_categories_protocol_port,bar)
+		}
+		});
+
+		}
+		
+
+		function set_stacked_bar(data,bars,bar){
+			if(data.length==0){
+				bar.setData(
+        	{ barCategory: bars
+        	, stackedCategory: ['EMPTY']
+        	, data: data})
+			screen.render();
+
+			}
+			else{
+			bar.setData(
+        	{ barCategory: bars
+        	, stackedCategory: ['totalflows', 'totalpkt', 'totalbytes']
+        	, data: data})
+			screen.render();}
+		}
 
 
 
@@ -119,7 +342,7 @@ var stacked_bar = new interface_element(0)
 
 
 
-function getIpInfo(ip){
+function getIpInfo_box_ip(ip){
 	/*
 	retrieves IPsInfo from redis.
 	*/
@@ -131,7 +354,9 @@ function getIpInfo(ip){
       	screen.render();}
       	catch (err){
       		box_ip.setContent(reply[ip]);
-      	    screen.render();}
+
+      	    screen.render();
+      	}
     });
 }
 
@@ -169,21 +394,22 @@ async.waterfall([
 		retrieve ips with profile from redis key 'Profiles'
 		*/
 		var ips_with_profiles = [];
-
-		redis_ips_with_profiles.smembers("profiles", (err,reply)=>{
-		if(err){callback(err)}
-		async.each(reply, function(ip_profile, callback) {
-			ips_with_profiles.push(ip_profile.split('_')[1]);
-
-			callback(null)
-		}, function(err) {
+		redis_tree.keys('*', (err,reply)=>{
+			if(err){callback(err)}
+			async.each(reply, function(key,callback){
+				if(key.includes('timewindow')){
+					ips_with_profiles.push(key.split('_')[1]);
+				}
+				callback(null)
+			},function(err) {
 
 		 if( err ) {
 		 	console.log('unable to create user');
 		 }else {
 		 	callback(null, ips_with_profiles);
-		 }
-		});
+		 };
+		})
+
 
 })},
 
@@ -222,17 +448,17 @@ async.waterfall([
 tree.on('select',function(node){
 
     if(!node.name.includes('timewindow')){
-    	getIpInfo(node.name)}
+    	getIpInfo_box_ip(node.name)}
   		
     else{
-    redis_detections_timewindow.hget("profile_"+node.parent.name+"_"+node.name,'Detections',(err,reply)=>{
-    	box_detections.setContent(reply)
-    })
+    
     redis_outtuples_timewindow.hgetall("profile_"+node.parent.name+"_"+node.name, (err,reply)=>{
         if(reply == null){
         	table_outTuples.setData({headers: [''], data: []})
+        	box_detections.setContent('');
         	return;}
-	    
+	    box_detections.setContent(reply['Detections'])
+
 	    var obj_outTuples = JSON.parse(reply["OutTuples"])
 	    var keys = Object.keys(obj_outTuples);
 	    var data = [];
@@ -248,116 +474,187 @@ tree.on('select',function(node){
  		if( err ) {
 			console.log('unable to create user');
  		} else {
- 			table_outTuples.setData({headers: [''], data: data});
-		screen.render();
  			
+ 			table_outTuples.setData({headers: [''], data: data});
+		screen.render();	
  		}
 		});
-  		
-  		
 
-		function stackedbar(first, second){
-		const mapTCPEstablished = async () => {
-		var bar_categories_protocol_port = []
-		try{
-	    var obj_dstPorts_tcp_established = JSON.parse(reply[first])
-		var keys_dstPorts_tcp_established = Object.keys(obj_dstPorts_tcp_established)
-		var data_tcp_est = [];
-}
-	    catch(err){
-	     var obj_dstPorts_tcp_established = reply[first]	
-	     var keys_dstPorts_tcp_established = Object.keys(obj_dstPorts_tcp_established)
-		var data_tcp_est = [];
 
-	    }
-		
-	    const promises_TCP_est = keys_dstPorts_tcp_established.map(async key_TCP_est => {
-	    	bar_categories_protocol_port.push('TCP/'+key_TCP_est)
-	    	var service_info = obj_dstPorts_tcp_established[key_TCP_est]
-	    	var row = []
-
-	    	row.push(round(Math.log(service_info['totalflows']),0), round(Math.log(service_info['totalpkt']),0), round(Math.log(service_info['totalbytes']),0))
-	    	
-	    	data_tcp_est.push(row)
-  		})
-
-  		try{
-  		var obj_dstPorts_udp_established = JSON.parse(reply[second])
-  		var keys_dstPorts_udp_established = Object.keys(obj_dstPorts_udp_established)
-  	    var data_udp_est = [];}
-  		catch(err){
-  			var obj_dstPorts_udp_established = reply[second]
-  			var keys_dstPorts_udp_established = Object.keys(obj_dstPorts_udp_established)
-  	    var data_udp_est = []
+	var conn_state = true;
+	var conn_state2 = true;
+	var bar = grid.set(0,0,3,6,contrib.stackedBar,
+       	{ 
+         barWidth: 6
+       , barSpacing: 10
+       , xOffset: 2
+       , height: "50%"
+       , border : 'green'
+       ,scrollbar: true
+       , width: "50%"
+       , barBgColor: [ 'red', 'blue', 'green' ]})
+	var bar2 = grid.set(3,0,3,6,contrib.stackedBar,
+       { 
+         barWidth: 6
+       , barSpacing: 10
+       , xOffset: 2
+       , height: "100%"
+       , border : 'green'
+       ,scrollbar: true
+       , width: "100%"
+       , barBgColor: [ 'red', 'blue', 'green' ]})
+	bar.hide()		
+	bar2.hide()		
+	screen.key('e', function(ch, key) {
+		if(conn_state2){
+			bar.show()
+			bar.focus()
+			bar2.show()
+		tcp_connections("DstPortsClientTCPEstablished","DstPortsClientUDPEstablished",reply,bar);
+		tcp_connections("DstPortsClientTCPNotEstablished","DstPortsClientUDPNotEstablished",reply,bar2);
+		bar.setLabel({text:'DstPortsClientEstablished',side:'left'})
+		bar2.setLabel({text:'DstPortsClientNotEstablished',side:'left'})
+		}
+		else{
+  			bar.hide()	
+  			bar2.hide()	
+  			
 
   		}
+  		conn_state2 = !conn_state2;
+  		screen.render()
+	
+});
+
+	screen.key('b', function(ch, key) {
+		if(conn_state){
+			bar.show()
+			bar.focus()
+			bar2.show()
 		
+		tcp_connections("DstPortsServerTCPEstablished","DstPortsServerUDPEstablished",reply,bar);
+		tcp_connections("DstPortsServerTCPNotEstablished","DstPortsServerUDPNotEstablished",reply,bar2);
+		bar.setLabel({text:'DstPortsServerEstablished',side:'left'})
+		bar2.setLabel({text:'DstPortsServerTNotEstablished',side:'left'})
+		}
+		else{
+  			bar.hide()	
+  			bar2.hide()	
+  			
 
-	    const promises_UDP_est = keys_dstPorts_udp_established.map(async key_UDP_est => {
-	    	bar_categories_protocol_port.push('UDP/'+key_UDP_est)
-	    	var service_info_udp = obj_dstPorts_udp_established[key_UDP_est]
-	    	var row_udp = []
-	    	row_udp.push(round(Math.log(service_info_udp['totalflows']),0), round(Math.log(service_info_udp['totalpkt']),0), round(Math.log(service_info_udp['totalbytes']),0))
-	    	data_udp_est.push(row_udp)
-	    	
-  		})
-  		await Promise.all(promises_TCP_est)
-  		await Promise.all(promises_UDP_est)
-  		data_tcp_est.push(...data_udp_est)
-  		 bar.setData(
-        { barCategory: bar_categories_protocol_port
-        , stackedCategory: ['totalflows', 'totalpkt', 'totalbytes']
-        , data: data_tcp_est
-        })
-		screen.render();}
-		mapTCPEstablished()};
-// 		  screen.key(['e'], function(ch, key) {
-// 			if(stacked_bar.state == 0){
-// 				stackedbar("DstPortsClientTCPEstablished","DstPortsClientUDPEstablished");
-// 				stacked_bar.state=1;}
-// 			else{
-// 				stackedbar("DstIPsClientTCPEstablished","DstIPsClientUDPEstablished");
-// 				stacked_bar.state = 0;
+  		}
+  		conn_state = !conn_state;
+  		screen.render()
+	
+});
+	var conn_state3 = true
+	screen.key('c', function(ch, key) {
+		if(conn_state3){
+			bar.show()
+			bar2.show()
+		
+		tcp_connections("SrcPortsClientTCPEstablished","SrcPortsClientUDPEstablished",reply,bar);
+		tcp_connections("SrcPortsClientTCPNotEstablished","SrcPortsClientUDPNotEstablished",reply,bar2);
+		bar.setLabel({text:'SrcPortsClientEstablished',side:'left'})
+		bar2.setLabel({text:'SrcPortsClientTNotEstablished',side:'left'})
+		}
+		else{
 
-// 			}
+  			bar.hide()	
+  			bar2.hide()	
+  			
 
-  
-// });
-    	stackedbar("DstIPsClientTCPEstablished","DstIPsClientUDPEstablished");
+  		}
+  		conn_state3 = !conn_state3;
+  		screen.render()
+	
+});
+
+	var conn_state4 = true
+	screen.key('o', function(ch, key) {
+		if(conn_state4){
+			bar.show()
+			bar2.show()
+		
+		ip_tcp_bars("DstIPsClientTCPEstablished","DstIPsClientUDPEstablished",reply,bar);
+		ip_tcp_bars("DstIPsClientTCPNotEstablished","DstIPsClientUDPNotEstablished",reply,bar2);
+		bar.setLabel({text:'SrcPortsClientEstablished',side:'left'})
+		bar2.setLabel({text:'SrcPortsClientTNotEstablished',side:'left'})
+		}
+		else{
+
+  			bar.hide()	
+  			bar2.hide()	
+  			
+
+  		}
+  		conn_state4 = !conn_state4;
+  		screen.render()
+	
+});
+	
+
     }) 
 
     
     redis_get_timeline.lrange("profile_"+node.parent.name+"_"+node.name+'_timeline',0,-1, (err,reply)=>{
-    	const mapTimeline = async _ => {
-	    var data = [];
-	    const promises = reply.map(async line => {
-	      	var row = [];
-	      	var line_arr = line.split(" ")
+    	var data = [];
+    	var ips = []	
+
+    	async.each(reply, function(line, callback){
+    		var row = [];
+    		var line_arr = line.split(" ")
 	      	if(line_arr[6].includes('.')){
+	      	ips.push(line_arr[6])
 	      	line_arr[6]= "{bold}"+line_arr[6]+"{/bold}"}
 	      	line_arr[1]= line_arr[1].substring(0, line_arr[1].lastIndexOf('.'));
 			row.push(line_arr.join(" "));
 			data.push(row);
-  		})
-  		await Promise.all(promises)
-  		table_timeline.setData({headers:[node.parent.name+" "+node.name], data: data});
-		screen.render();}
-		mapTimeline();
+			callback();
+    	},function(err) {
+
+ 		if( err ) {
+ 			console.log('unable to create user');
+ 		} else {
+ 			table_timeline.setData({headers:[node.parent.name+" "+node.name], data: data});
+ 			setMap(ips)
+			screen.render();}
+		});
     	})
+    var conn_state5 = true
+	screen.key('m', function(ch, key) {
+		if(conn_state5){
+			map.show()
+			// map.focus()
+		}
+		else{
+  			map.hide()	
+  		}
+  		conn_state5 = !conn_state5;
+  		screen.render()
+	
+});
+
+
+    
     }
 });
+
+
 
 table_timeline.rows.on('select', (item, index) => {
 	var timeline_line = item.content.split(" ");
 	var timeline_ip = timeline_line[6].slice(6,-7)
-	getIpInfo(timeline_ip)
+	getIpInfo_box_ip(timeline_ip)
 });
 
 table_outTuples.rows.on('select', (item, index) => {
 	var outTuple_ip = item.content.trim().split(":")[0]
-	getIpInfo(outTuple_ip)
+	getIpInfo_box_ip(outTuple_ip)
 
 });
+
+
 
 
 
@@ -365,14 +662,16 @@ screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
 
+
+
 screen.key(['tab'], function(ch, key) {
   if(screen.focused == tree.rows)
     table_timeline.focus();
   else if(screen.focused == table_timeline.rows)
   	table_outTuples.focus();
-
   else
     tree.focus();
 });
 tree.focus();
+// screen.append(tree)
 screen.render();
