497f572d8efe822f8881e703b1ded8e606248418
Dita Hollmannová <hollmdit@fel.cvut.cz>
2019-08-20 13:23:04 +0200

    Added function to compare performance and results of cymru requests and custom requests

--- modules/whoisip/test_whoisip.py
+++ modules/whoisip/test_whoisip.py
@@ -1,6 +1,7 @@
 import configparser
 
 import time
+import random
 
 from modules.whoisip.whoisip import WhoisIP
 
@@ -21,7 +22,6 @@ def run(infile, outfile):
 
 
 def try_random_addresses(limit=500):
-    import random
     wi = WhoisIP(None, get_default_config(), testing=True)
     for i in range(0, limit):
         ip = str(random.randint(0, 255)) + "." + str(random.randint(0, 255)) + "."\
@@ -31,6 +31,8 @@ def try_random_addresses(limit=500):
 
 def test_tricky_ips():
     ips = []
+    ips.append("8.8.8.8")
+    ips.append("245.51.167.150")  # manual check returns None
     ips.append("206.60.214.219")  # referring to other servers, unregistered segment, wrong country code
     ips.append("8.218.236.191")  # cymruwhois correctly says its SG, but whois insists on AU
     ips.append("107.78.26.116")  # broken pipe in cymruwhois (didn't happen for the second time..)
@@ -42,17 +44,61 @@ def test_tricky_ips():
     ips.append("86.255.141.19")  # wrong encoding
     ips.append("71.163.76.208")  # no line "Information related to ..."
     ips.append("148.37.198.241")  # no line "Information related to ..."
+    ips.append("47.216.133.94")  # b'fgets: Spojen\xc3\xad zru\xc5\xa1eno druhou stranou\n'
+    ips.append("185.0.192.82")  # Type error
 
     wi = WhoisIP(None, get_default_config(), testing=True)
     for ip in ips:
-        wi.check_ip(ip)
+        wi.check_ip_manual_only(ip, verbose=True)
+
+
+def compare_methods(limit=500):
+    cymru = WhoisIP(None, get_default_config(), testing=True)
+    manual = WhoisIP(None, get_default_config(), testing=True)
+
+    cymru_results = []
+    manual_results = []
+
+    random_addresses = []
+
+    for i in range(0, limit):
+        ip = str(random.randint(0, 255)) + "." + str(random.randint(0, 255)) + "."\
+             + str(random.randint(0, 255)) + "." + str(random.randint(0, 255))
+        random_addresses.append(ip)
+
+    tmp_time = time.time()
+    for ip in random_addresses:
+        cymru_results.append(cymru.check_ip(ip))
+    cymru_time = time.time() - tmp_time
+
+    tmp_time = time.time()
+    for ip in random_addresses:
+        manual_results.append(manual.check_ip_manual_only(ip))
+    manual_time = time.time() - tmp_time
+
+    print("Cymru took", cymru_time)
+    print("Manual took", manual_time)
+
+    for ip, cr, mr in zip(random_addresses, cymru_results, manual_results):
+        if cr[0] != mr[0] or cr[1] != mr[1] or cr[2] != mr[2] or cr[3] != mr[3]:
+            print("IP:", ip, "has different results")
+            if cr[0] != mr[0]:
+                print("ASN - cumru:", cr[0] + ", manual:", mr[0])
+            if cr[1] != mr[1]:
+                print("Country - cumru:", cr[1] + ", manual:", mr[1])
+            if cr[2] != mr[2]:
+                print("CIDR - cumru:", cr[2] + ", manual:", mr[2])
+            if cr[3] != mr[3]:
+                print("Name - cumru:", cr[3] + ", manual:", mr[3])
 
 if __name__ == "__main__":
     t = time.time()
     test_tricky_ips()
     # run("modules/whoisip/data/errs_out_of_erx.txt", "modules/whoisip/data/tmp.txt")
     # run("modules/whoisip/data/asn_lookup_err_ips.txt", "modules/whoisip/data/tmp.txt")
-    try_random_addresses()
+    # try_random_addresses()
     # TODO: check ip ranges in file
     # TODO: run query manually if the query doesn't return enough data
     print(time.time() - t)
+
+    compare_methods(limit=50)
