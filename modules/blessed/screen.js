d9e13aa737745cc67cfb379eee00dfcaa9ce4157
Kamila Babayeva <kamifai14@gmail.com>
2019-08-27 20:28:08 +0200

    screen initialize

--- modules/blessed/screen.js
+++ modules/blessed/screen.js
@@ -4,10 +4,12 @@ const tableWidget = require('./tableWidget.js');
 const treeWidget = require('./treeWidget.js');
 const boxWidget = require('./boxWidget.js');
 const widgets = require( './widgets.js');
+const redis = require('redis')
 class screen {
     constructor (){   //utils = new Map()
         this.screen = undefined;
         this.grid = undefined;
+        // this.redis = undefined;
  
     }
 init(){
@@ -15,11 +17,14 @@ init(){
         title:'without name'
     })
     this.grid = new contrib.grid({rows:6, cols:6, screen: this.screen})
+    // this.redis  = redis.redisClient();
 }
 
 initScreen(){
     var table = new tableWidget({blessed, contrib, screen: this.screen, grid:{ gridObj:this.grid, gridLayout:[0, 1, 2.5, 5]}});
-    var tree = new widgets.WidgetTree({blessed, contrib, screen: this.screen, grid:{ gridObj:this.grid, gridLayout:[0,0,5,1]}});
+    var tree = new treeWidget({blessed, contrib, screen: this.screen, grid:{ gridObj:this.grid, gridLayout:[0,0,5,1]}, redis});
+    tree.setTree();
+    this.screen.render()
     var box = new widgets.WidgetBox({blessed, contrib, screen: this.screen, grid:{ gridObj:this.grid, gridLayout:[5, 0, 0.4, 6]}})
 
     this.screen.render();
