9e6c1c1e1d4466aded90c09e0549f56103b615df
Sebastian Garcia <eldraco@gmail.com>
2020-05-19 11:07:26 +0200

    Changes to make lstm detection better

--- modules/lstm-cc-detection-1/lstm-cc-detection-1.py
+++ modules/lstm-cc-detection-1/lstm-cc-detection-1.py
@@ -17,15 +17,15 @@ import multiprocessing
 from slips.core.database import __database__
 import platform
 import warnings
+# Your imports
+import numpy as np
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.models import load_model
+from keras.utils import to_categorical
+
 warnings.filterwarnings('ignore', category=FutureWarning)
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 
-
-# Your imports
-import time
-import numpy as np
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
 
 class Module(Module, multiprocessing.Process):
     # Name: short name of the module. Do not use spaces
@@ -35,19 +35,25 @@ class Module(Module, multiprocessing.Process):
 
     def __init__(self, outputqueue, config):
         multiprocessing.Process.__init__(self)
-        # All the printing output should be sent to the outputqueue. The outputqueue is connected to another process called OutputProcess
+        # All the printing output should be sent to the outputqueue. The
+        # outputqueue is connected to another process called OutputProcess
         self.outputqueue = outputqueue
-        # In case you need to read the slips.conf configuration file for your own configurations
+        # In case you need to read the slips.conf configuration file for your
+        # own configurations
         self.config = config
         # Start the DB
         __database__.start(self.config)
-        # To which channels do you wnat to subscribe? When a message arrives on the channel the module will wakeup
-        # The options change, so the last list is on the slips/core/database.py file. However common options are:
+        # To which channels do you wnat to subscribe? When a message arrives
+        # on the channel the module will wakeup
+        # The options change, so the last list is on the slips/core/database.py
+        # file. However common options are:
         # - new_ip
         # - tw_modified
         # - evidence_added
         self.c1 = __database__.subscribe('new_letters')
-        # Set the timeout based on the platform. This is because the pyredis lib does not have officially recognized the timeout=None as it works in only macos and timeout=-1 as it only works in linux
+        # Set the timeout based on the platform. This is because the pyredis
+        # lib does not have officially recognized the timeout=None as it works
+        # in only macos and timeout=-1 as it only works in linux
         if platform.system() == 'Darwin':
             # macos
             self.timeout = None
@@ -87,6 +93,45 @@ class Module(Module, multiprocessing.Process):
         description = 'LSTM C&C channels detection, score: ' + str(score)
         __database__.setEvidence(key, threat_level, confidence, description, profileid=profileid, twid=twid)
 
+    def convert_input_for_module(self, pre_behavioral_model):
+        """ 
+        Takes the input from the letters and converts them 
+        to whatever is needed by the model 
+        The pre_behavioral_model is a 1D array of letters in an array
+        """
+
+        # Length of behavioral model with which we trained our module
+        max_length = 500
+        # str_to_ascii = lambda i: [ord(x) for x in i]
+        # behavioral_model = str_to_ascii(behavioral_model)
+        # Convert the string into a list and then into a numpy array
+        pre_behavioral_model = np.array(list(pre_behavioral_model))
+        #print(f'1 BM: {pre_behavioral_model}')
+
+        # Convert each of the stratosphere letters to an integer. There are 50
+        vocabulary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',', '.', '+', '*']
+        int_of_letters = {}
+        for i, letter in enumerate(vocabulary):
+            int_of_letters[letter] = i
+        # vocabulary_size = len(int_of_letters)
+        # Convert each of the letters in the model into the integer
+        convert_to_int = lambda x : [int_of_letters[i] for i in x]
+        pre_behavioral_model = np.array(convert_to_int(pre_behavioral_model), ndmin=2)
+        #print(f'2 BM: {pre_behavioral_model}')
+        # Now  the pre_behavioral_model is a list of ints
+        # Padd the list to 500
+        pre_behavioral_model = np.array(pad_sequences(pre_behavioral_model, maxlen=max_length, padding='post'))
+        #print(f'3 BM: {pre_behavioral_model}')
+        #print(type(pre_behavioral_model))
+        #print(pre_behavioral_model.shape)
+
+        # Convert to one-hot encoding
+        pre_behavioral_model_oh = np.empty((pre_behavioral_model.shape[0], 500, 50))
+        for i, a_list in enumerate(pre_behavioral_model):
+            pre_behavioral_model_oh[i] = to_categorical(a_list, num_classes=50)
+        #print(f'4 BM: {pre_behavioral_model_oh}')
+        return pre_behavioral_model_oh
+
     def run(self):
         try:
             # Download lstm model
@@ -98,26 +143,28 @@ class Module(Module, multiprocessing.Process):
                 if message['data'] == 'stop_process':
                     return True
                 if message['channel'] == 'new_letters' and type(message['data']) is not int:
+                    # Define why this threshold
                     threshold = 0.8
                     data = message['data']
                     data = data.split('-')
-                    behavioral_model = data[0]
+                    pre_behavioral_model = data[0]
                     profileid = data[1]
                     twid = data[2]
                     tupleid = data[3]
-                    # Length of behavioral model with which we trained our module
-                    max_length = 500
                     # Function to convert each letter of behavioral model to ascii
-                    str_to_ascii = lambda i: [ord(x) for x in i]
-                    behavioral_model = str_to_ascii(behavioral_model)
-                    # Pad with zeros. If it is one string - needed to be in square brackets. Otherwise, not iterable.
-                    behavioral_model = pad_sequences([behavioral_model], maxlen=max_length, padding='post')
+                    behavioral_model = self.convert_input_for_module(pre_behavioral_model)
                     # Predict the score of behavioral model being C&C channel
+                    print('a')
                     score = model.predict(behavioral_model, verbose=2)
+                    print('b')
                     # get a float instead of numpy array
+                    print(score)
+                    print(type(score))
                     score = score.item()
+                    print('c')
                     if score > threshold:
                         self.set_evidence(score, tupleid, profileid, twid)
+                    print('d')
 
         except KeyboardInterrupt:
             return True
