0e798211d29e80950908970efb04c085eeef4bcc
kamifai <kamifai14@gmail.com>
2020-06-19 15:58:10 +0200

    Box class with approptiate functions

---
+++ modules/blessed/kalipso_box.js
@@ -0,0 +1,75 @@
+var async = require('async')
+var color = require('chalk')
+
+class Box{
+  constructor(grid, blessed, contrib, redis_database,screen, characteristics){
+      this.contrib = contrib
+      this.screen = screen
+      this.blessed = blessed
+      this.grid = grid
+      this.redis_database = redis_database
+      this.widget = this.initBox(characteristics);
+}
+  initBox(characteristics){
+    return this.grid.set(characteristics[0],characteristics[1],characteristics[2],characteristics[3], this.blessed.box,{
+        top: 'center', 
+        left: 'center',
+        width: '50%',
+        height: '50%',
+        label:characteristics[4],
+        tags: true,
+        keys: true,
+        style:{
+          border:{ fg:'blue',type: 'line'},
+          focus: {border:{ fg:'magenta'}
+      }},
+        vi:true,
+        scrollable: true,
+        alwaysScroll: true,
+        
+        scrollbar: {
+          ch: ' ',
+          inverse: true
+        }
+
+    })
+  }
+  setData(data){
+    this.widget.setContent(data)
+      }
+  hide(){
+    this.widget.hide()
+      }
+  show(){
+    this.widget.show()
+      }
+  focus(){
+    this.widget.focus()
+      }
+
+
+  setEvidence(ip, timewindow){
+    try{
+      var evidence_data = ''
+      this.redis_database.getEvidence(ip, timewindow).then(redis_evidence_data=>{
+        if (redis_evidence_data==null){
+          return this.setData(evidence_data)
+        }
+
+        var evidence_json = JSON.parse(redis_evidence_data);
+        var evidence_keys = Object.keys(evidence_json); 
+        async.each(evidence_keys, (key,callback)=>{
+          evidence_data = evidence_data+'{bold}'+color.green(key)+'{/bold}'+" "+evidence_json[key]+'\n'
+          callback();
+          }, (err)=>{
+            if(err){console.log(err)}
+            else{return this.setData(evidence_data)}
+        });
+        });
+    }
+    catch (err){
+      console.log(err)
+    }
+  }
+}
+    module.exports = Box;
