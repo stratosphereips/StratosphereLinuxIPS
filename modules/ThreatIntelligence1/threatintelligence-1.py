6c5b3606d4cbd23eb9aa40df68a6f7bb4d9c0f45
kamifai <kamifai14@gmail.com>
2020-08-05 15:59:15 +0200

    Deleted print statement

--- modules/ThreatIntelligence1/threatintelligence-1.py
+++ modules/ThreatIntelligence1/threatintelligence-1.py
@@ -123,7 +123,6 @@ class Module(Module, multiprocessing.Process):
         This also helps in having unique ioc accross files
         Returns nothing, but the dictionary should be filled
         """
-        print(data_file_name, 'Should be updated')
         try:
             malicious_ips_dict = {}
             malicious_domains_dict = {}
