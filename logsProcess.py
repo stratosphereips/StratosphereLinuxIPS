228e7f13631928990db71688da6db54f7b4a3c35
Sebastian Garcia <eldraco@gmail.com>
2020-08-11 17:20:08 +0200

    Adaptation to the new  key 'ModifiedTW' as a sorted set that now includes the time when the TW was modified

--- logsProcess.py
+++ logsProcess.py
@@ -224,8 +224,8 @@ class LogsProcess(multiprocessing.Process):
             for profileTW in TWModifiedforProfile:
 
                 # Get the profileid and twid
-                profileid = profileTW.split(self.fieldseparator)[0] + self.fieldseparator + profileTW.split(self.fieldseparator)[1]
-                twid = profileTW.split(self.fieldseparator)[2]
+                profileid = profileTW[0].split(self.fieldseparator)[0] + self.fieldseparator + profileTW[0].split(self.fieldseparator)[1]
+                twid = profileTW[0].split(self.fieldseparator)[2]
                 # Get the time of this TW. For the file name
                 twtime = __database__.getTimeTW(profileid, twid)
                 twtime = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(twtime))
