c7dbb13ed29455213e88f2e7fb0b5ac6c45a4e76
Sebastian Garcia <eldraco@gmail.com>
2021-02-02 15:18:44 +0100

    Dont remove the old zeek files putting the process in background, wait for it

--- inputProcess.py
+++ inputProcess.py
@@ -387,12 +387,12 @@ class InputProcess(multiprocessing.Process):
                 if len(os.listdir(self.zeek_folder)) > 0:
                     # First clear the zeek folder of old .log files
                     # The rm should not be in background because we must wait until the folder is empty
-                    command = "rm " + self.zeek_folder + "/*.log 2>&1 > /dev/null &"
+                    command = "rm " + self.zeek_folder + "/*.log 2>&1 > /dev/null"
                     os.system(command)
 
                 # Run zeek on the pcap or interface. The redef is to have json files
                 # To add later the home net: "Site::local_nets += { 1.2.3.0/24, 5.6.7.0/24 }"
-                command = "cd " + self.zeek_folder + "; "+self.zeek_or_bro +" -C " +bro_parameter + "  " + self.tcp_inactivity_timeout + " local -e 'redef LogAscii::use_json=T;' -f " + self.packet_filter + " 2>&1 > /dev/null &"
+                command = "cd " + self.zeek_folder + "; "+self.zeek_or_bro + " -C " + bro_parameter + "  " + self.tcp_inactivity_timeout + " local -e 'redef LogAscii::use_json=T;' -f " + self.packet_filter + " 2>&1 > /dev/null &"
                 self.print(f'Zeek command: {command}', 3, 0)
                 # Run zeek.
                 os.system(command)
