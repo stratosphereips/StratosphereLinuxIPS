75d0c893709142930b1baac79ef72b5bc7cc81d6
Ondrej Lukas <ondrej.lukas95@gmail.com>
2021-03-23 19:01:24 +0100

    Added argparse for better parameter management

--- modules/lstm-cc-detection-1/rnn_model.py
+++ modules/lstm-cc-detection-1/rnn_model.py
@@ -6,33 +6,42 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from random import shuffle
 
-
+import tensorflow as tf
+import sklearn as sk
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import one_hot
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import to_categorical
 
-# Versions
-import tensorflow as tf
-import sklearn as sk
-print(f'Numpy: {np.__version__}')
-print(f'TensorFlow: {tf.__version__}')
-print(f'Pandas: {pd.__version__}')
-print(f'Sklearn: {sk.__version__}')
+parser = argparse.ArgumentParser()
+parser.add_argument("-D", "--dataset_file", help="File containing data for training", type=str, required=True)
+parser.add_argument("-M", "--max_letters", help="Max sequence length", type=int, required=False, default=500)
+parser.add_argument("-m", "--min_letters", help="Min sequence length", type=int, required=False, default=5)
+parser.add_argument("-v", "--verbose", help="Level of verbosity", type=bool, required=False, default=False)
+parser.add_argument("-b", "--batch_size", help="Size of the minibatch", type=int, required=False, default=100)
+parser.add_argument("-e", "--epochs", help="Number of epochs in training", type=int, required=False, default=200)
+parser.add_argument("-S", "--model_file", help="Where to store the train model", type=str, required=False)
+args = parser.parse_args()
 
+
+
+if args.verbose:
+	# Versions
+	print(f'Numpy: {np.__version__}')
+	print(f'TensorFlow: {tf.__version__}')
+	print(f'Pandas: {pd.__version__}')
+	print(f'Sklearn: {sk.__version__}')
 
 # Load the dataset
-dataset_file='./datasets/all_datasets_raw_v3'
-
 # Cut the max amount of letters in the state to a maximum. 
 # Better to do it here in the read_csv so we dont use memory later. Here those lines never go into memory.
-max_letters = 500
-f = lambda x: x[:max_letters]
-with open(dataset_file, 'rb') as csvfile:
+f = lambda x: x[:args.max_letters]
+with open(args.dataset_file, 'rb') as csvfile:
     df = pd.read_csv(csvfile, delimiter='|', names=[ "note", "label", "model_id", "state"], skipinitialspace=True, converters={'state': f} )
 
-df.describe()
+if args.verbose:
+	df.describe()
 
 
 # Clean the dataset
@@ -40,8 +49,7 @@ df.dropna(axis=0, how='any', inplace=True)
 df.drop(axis=1, columns=['note', 'model_id'], inplace=True)
 
 # Delete the strings of letters with less than a certain amount
-min_letters = 5
-indexNames = df[ df['state'].str.len() < min_letters ].index
+indexNames = df[ df['state'].str.len() < args.min_letters ].index
 df.drop(indexNames, inplace = True)
 
 
@@ -56,12 +64,12 @@ df.label.replace('Normal', 0, inplace=True)
 
 
 # Convert each of the stratosphere letters to an integer. There are 50
-#vocabulary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',', '.', '+', '*']
 vocabulary = list("abcdefghiABCDEFGHIrstuvwxyzRSTUVWXYZ1234567890,.+*")
 int_of_letters = {}
 for i, letter in enumerate(vocabulary):
     int_of_letters[letter] = float(i)
-print(f'There are {len(int_of_letters)} letters in total. From letter index {min(int_of_letters.values())} to letter index {max(int_of_letters.values())}.')
+if args.verbose:
+	print(f'There are {len(int_of_letters)} letters in total. From letter index {min(int_of_letters.values())} to letter index {max(int_of_letters.values())}.')
 vocabulary_size = len(int_of_letters)
 
 
@@ -74,17 +82,21 @@ features_per_sample = 1
 # Convert the data into the appropriate shape
 # x_data is a list of lists. The 1st dimension is the outtuple, the second the letter. Each letter is now an int value. shape=(num_outuples, features_per_sample)
 x_data = df['state'].to_numpy()
-print('There are {} outtuples'.format(len(x_data)))
+if args.verbose:
+	print('There are {} outtuples'.format(len(x_data)))
 # y_data is a list of ints that are 0 or 1. One integer per outtupple. shape=(num_outuples, 1)
 y_data = df['label'].to_numpy()
-print('There are {} labels'.format(len(y_data)))
+if args.verbose:
+	print('There are {} labels'.format(len(y_data)))
 # Search the sample with max len in the training. It should be already cuted by the csv_read function to a max. Here we just check
 max_length_of_outtupple = max([len(sublist) for sublist in df.state.to_list()])
-print('The max len of the letters in all outtuples is: {}'.format(max_length_of_outtupple))
+if args.verbose:
+	print('The max len of the letters in all outtuples is: {}'.format(max_length_of_outtupple))
 
 # Here x_data is a array of lists [[]]
-print(f'x_data type {type(x_data)} of shape {x_data.shape}. x_data[0] type is {type(x_data[0])}')
-print(f'x_data[0] is {x_data[0]}')
+if args.verbose:
+	print(f'x_data type {type(x_data)} of shape {x_data.shape}. x_data[0] type is {type(x_data[0])}')
+	print(f'x_data[0] is {x_data[0]}')
 
 
 # Padding.
@@ -100,7 +112,8 @@ print(f'x_data[0] is {x_data[0]}')
 
 # If the input are integers
 padded_x_data = pad_sequences(x_data, maxlen=max_length_of_outtupple, padding='post')
-print(f'padded_x_data is of type {type(padded_x_data)}, of shape {padded_x_data.shape}. padded_x_data[0] type is {type(padded_x_data[0])}. Shape of second list is {padded_x_data[0].shape}')
+if args.verbose:
+	print(f'padded_x_data is of type {type(padded_x_data)}, of shape {padded_x_data.shape}. padded_x_data[0] type is {type(padded_x_data[0])}. Shape of second list is {padded_x_data[0].shape}')
 
 
 
@@ -122,7 +135,7 @@ train_y_data = y_data
 # Hyperparameters
 # Real data
 # Store the dimensions
-batch_size = 100 # group of outtuples as a batch
+#batch_size = 100 # group of outtuples as a batch
 num_outtuples = train_x_data.shape[0] # number_of_outtuples in general
 # max_length_of_outtupple # max amount of letters in each outtuple (500 now)
 
@@ -137,7 +150,7 @@ timesteps = max_length_of_outtupple
 input_shape = (timesteps, features_per_sample)
 print(f'We have as shape: Num of samples: {num_outtuples}, Num of letters per sample (timesteps): {timesteps}, each letter has {features_per_sample} values. The input shape is {input_shape}')
 
-num_epochs = 500
+#num_epochs = 500
 
 # The shape of the input is now : (2200, 500, 50)
 # 2200, amount of outtuples
@@ -151,20 +164,21 @@ model = tf.keras.models.Sequential()
 model.add(layers.Embedding(vocabulary_size, 16, mask_zero=True))
 # GRU is the main RNN layer, inputs: A 3D tensor, with shape [batch, timesteps, feature]
 model.add(layers.Bidirectional(layers.GRU(32, return_sequences=False),merge_mode="concat"))
-# Fully connected layer with 1 neuron output
 model.add(layers.Dense(32, activation="relu"))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(1, activation="sigmoid"))
+# Fully connected layer with 1 neuron output
 # Final output value between 0 and 1 as probability
-model.compile(loss='binary_crossentropy', optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.0001, momentum=0.05), metrics=['accuracy'],)
+model.compile(loss='binary_crossentropy', optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.0001, momentum=0.05), metrics=['accuracy'])
 
 
 # Train the model
 # This is already separating in trainign and validation
-history = model.fit(train_x_data, train_y_data, epochs=num_epochs, batch_size=batch_size, validation_split=0.1, verbose=1, shuffle=True)
+history = model.fit(train_x_data, train_y_data, epochs=args.epochs, batch_size=args.batch_size, validation_split=0.1, verbose=1, shuffle=True)
 
-model.summary()
-
+if args.verbose:
+	model.summary()
+model.save(args.model_file, overwrite=False) 
 
 # To plot the results
 import matplotlib.pyplot as plt
@@ -186,4 +200,3 @@ plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
 plt.savefig("test_results_loss.png")
-model.save('lstm_model_v_ondra3.h5') 
