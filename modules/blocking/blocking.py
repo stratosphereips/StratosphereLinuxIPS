8c868b576732de4a2c8edf5d41d7046d261c9fee
Alya Gomaa <alyaggomaa@gmail.com>
2021-03-24 17:55:34 +0200

    Delete rules and chain from iptables on stop_message

--- modules/blocking/blocking.py
+++ modules/blocking/blocking.py
@@ -75,7 +75,7 @@ class Module(Module, multiprocessing.Process):
         self.outputqueue.put(f"{levels}|{self.name}|{text}")
 
     def determine_linux_firewall(self):
-        """ Returns the currently installed firewall and installs one if none was found """
+        """ Returns the currently installed firewall and installs iptables if none was found """
         if shutil.which('iptables'):
             return 'iptables'
         elif shutil.which('nftables'):
@@ -87,6 +87,7 @@ class Module(Module, multiprocessing.Process):
             return 'iptables'
 
     def initialize_chains_in_firewall(self):
+        """ For linux: Adds a chain to iptables or a table to nftables called slipsBlocking where all the rules will reside """
         if self.platform_system == 'Linux':
             # Get the user's currently installed firewall
             self.firewall = self.determine_linux_firewall()
@@ -94,6 +95,7 @@ class Module(Module, multiprocessing.Process):
                 print('Executing "sudo iptables -N slipsBlocking"')
                 # Add a new chain to iptables
                 os.system('sudo iptables -N slipsBlocking')
+                #TODO: use python iptc
                 #TODO: determine which one to use OUTPUT INPUT or FORWARD
 
                 # Redirect the traffic from all other chains to slipsBlocking so rules in any pre-existing chains dont override it
@@ -112,11 +114,13 @@ class Module(Module, multiprocessing.Process):
             self.print('Mac OS blocking is not supported yet.')
 
     def block_ip_in_iptables(self,ip_to_block):
+        """ Blocks the ip from iptables only """
         #TODO: determine wether to block traffic to or from the ip
 
-        # Blocking all traffic to and from ip_to_block
+        # Block traffic from destination ip_to_block (-d)
         # -I to insert the rule at the top of the chain so it overrides any existing rule that may conflict
         os.system('sudo iptables -I slipsBlocking -d' + ip_to_block + ' -j DROP')
+        # Block traffic from source ip_to_block (-s)
         os.system('sudo iptables -I slipsBlocking -s' + ip_to_block + ' -j DROP')
         print("Blocked: " + ip_to_block)
 
@@ -124,7 +128,6 @@ class Module(Module, multiprocessing.Process):
         """
             This function determines the user's platform and firewall used and calls the appropriate function to add a rule to block the ip passed in message data
         """
-
         ip_to_block = message['data']
         if type(ip_to_block) == str:
             # Block this ip in iptables
@@ -132,20 +135,36 @@ class Module(Module, multiprocessing.Process):
                 if self.firewall == 'iptables':
                     # Blocking in iptables
                     self.block_ip_in_iptables(ip_to_block)
-
                 elif self.firewall == 'nftables':
                     # Blocking in nftables
                     #TODO: create block_ip_in_nftables
                     #TODO:create slipsBlocking table in nftables first. do that in  slips.py
                     pass
-
             elif self.platform_system == 'Darwin':
                 # Blocking in MacOS
                 self.print('Mac OS blocking is not supported yet.')
 
-    def handle_stop_process_message(self,message):
-       pass
+    def handle_stop_process_message(self, message):
+        """ Deletes slipsBlocking chain and rules based on the user's platform and firewall """
+        if self.platform_system == 'Linux':
+            if self.firewall == 'iptables':
+                # Delete rules in slipsBlocking chain
+                os.system('sudo iptables -F slipsBlocking')
+                # Delete slipsBlocking chain from iptables
+                os.system('sudo iptables -X slipsBlocking')
+                # Delete slipsBlocking redirection rule inserted in INPUT OUTPUT and FORWARD
+                os.system('sudo iptables -D INPUT -j slipsBlocking')
+                os.system('sudo iptables -D OUTPUT -j slipsBlocking')
+                os.system('sudo iptables -D FORWARD -j slipsBlocking')
+            elif self.firewall == 'nftables':
+                # TODO: handle the creation of the slipsBlocking chain in nftables
+                # Delete rules in slipsBlocking chain because you can't delete a chain without flushing first
+                os.system('sudo nft flush chain inet slipsBlocking')
+                # Delete slipsBlocking chain from nftables
+                os.system('sudo nft delete chain inet slipsBlocking')
 
+        elif self.platform_system == 'Darwin':
+            self.print('Mac OS blocking is not supported yet.')
 
     def run(self):
         #TODO: handle MacOS
