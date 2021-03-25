04d190a0952f99067a87101836d0d0a3c048633e
Ondrej Lukas <ondrej.lukas95@gmail.com>
2021-03-25 13:09:50 +0100

    use the newest rnn model

--- modules/lstm-cc-detection-1/lstm-cc-detection-1.py
+++ modules/lstm-cc-detection-1/lstm-cc-detection-1.py
@@ -132,7 +132,7 @@ class Module(Module, multiprocessing.Process):
         # self.print(f'Post Padded Seq sent: {pre_behavioral_model}. Shape: {pre_behavioral_model.shape}')
         return pre_behavioral_model
 
-    def run(self, model_file="modules/lstm-cc-detection-1/detection_tcpmodel-9.h5"):
+    def run(self, model_file="modules/lstm-cc-detection-1/rnn_model.h5"):
         # TODO: set the decision threshold in the function call
         try:
             # Download lstm model
