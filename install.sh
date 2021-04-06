ca30c33251726c7e600327b5cda308e41d91407f
alya <alyaggomaa@gmail.com>
2021-04-06 22:13:27 +0200

    Add docker commands support

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install curl git redis python3.7-minimal python3-redis python3-p
 echo "[+] Executing 'python3 -m pip install --upgrade pip'"
 python3 -m pip install --upgrade pip
 echo "[+] Executing 'pip3 install maxminddb colorama validators urllib3 numpy sklearn pandas certifi keras redis==3.4.1 netaddr slackclient stix2 cabby ipwhois tzlocal psutil'"
-pip3 install maxminddb colorama validators urllib3 numpy sklearn pandas certifi keras redis==3.4.1 netaddr slackclient stix2 cabby ipwhois tzlocal psutil
+pip3 install maxminddb colorama validators urllib3 numpy sklearn requests pandas certifi keras redis==3.4.1 netaddr slackclient stix2 cabby ipwhois tzlocal psutil
 echo "[+] Executing pip3 install --ignore-installed six"
 pip3 install --ignore-installed six
 echo "[+] Executing 'sudo npm install blessed blessed-contrib redis async chalk strip-ansi@6.0.0 clipboardy fs sorted-array-async yargs pytest'"
