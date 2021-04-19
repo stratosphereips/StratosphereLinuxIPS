8f2d1de037938af38a5a6a436f40cc9c86d7815e
alya <alyaggomaa@gmail.com>
2021-04-19 02:53:41 +0200

    Handle name and pattern Indicator attributes

--- modules/ExportingAlerts/ExportingAlerts.py
+++ modules/ExportingAlerts/ExportingAlerts.py
@@ -21,6 +21,9 @@ from slack import WebClient
 from slack.errors import SlackApiError
 import os
 import json
+from stix2 import Indicator
+import ipaddress
+# pip install --ignore-installed six
 #todo add this to requirements.txt
 
 
@@ -106,10 +109,59 @@ class Module(Module, multiprocessing.Process):
                 # You will get a SlackApiError if "ok" is False
                 assert e.response["error"] , "Problem while exporting to slack." # str like 'invalid_auth', 'channel_not_found'
 
-    def send_to_STIX(self):
-        pass
+    def export_to_STIX(self,msg_to_send: tuple) -> bool:
+        """
+        Function to export a STIX json file.
+        msg_to_send should be a tuple containing: (type_evidence, type_detection,detection_info, description)
+            type_evidence: e.g PortScan, ThreatIntelligence etc
+            type_detection: e.g dip sip dport sport
+            detection_info: ip or port
+            description: e.g port scans
+        """
+        # ['PortScanType2', 'dport', '23',
+        # 'New horizontal port scan detected to port 23. Not Estab TCP from IP: 192.168.2.16. Tot pkts sent all IPs: 9']
+        # ---------------- set name attribute ----------------
+        type_evidence, type_detection, detection_info, description =  msg_to_send[0], msg_to_send[1], msg_to_send[2] , msg_to_send[3]
+
+        # in case of ssh connection, type_evidence is set to SSHSuccessful-by-ip (special case) , ip here is variable
+        # so we change that to be able to access it in the below dict
+        if 'SSHSuccessful' in type_evidence:
+            type_evidence = 'SSHSuccessful'
+
+        # this dict contains each type and the way we should describe it in stix
+        type_evidence_descriptions = {
+            'PortScanType1': 'Vertical port scan',
+            'PortScanType2': 'Horizontal port scan',
+            'ThreatIntelligenceBlacklistIP' : 'Blacklisted IP',
+            'SelfSignedCertificate' : 'Self-signed certificate', # todo:  should we make it a stix indicator?
+            'LongConnection' : 'Long Connection', # todo what should be the description for this? should we even make it a stix indicator?
+            'SSHSuccessful' :'SSH connection from ip' #SSHSuccessful-by-ip
+        }
+
+        # Port scans are not passed in type_evidence , they're passed in the description
+        # Get the right description to use in stix
+        try:
+            name = type_evidence_descriptions[type_evidence]
+        except KeyError:
+            self.print("Can't find the description for type_evidence: {}".format(type_evidence),0,1)
+            return False
+        # ---------------- set pattern attribute ----------------
+        if self.is_ip(type_detection):
+            pattern = "[ip-addr:value = '{}']".format(type_detection)
+        else:
+            # It's a port probably coming from a portscan
+            #todo: get the ip
+            pass
+        # Required Indicator Properties: type, spec_version, id, created, modified , all are set automatically
+        # valid_from, created and modified attribute will be set to the current time
+        # id will be generated randomly
+        indicator = Indicator(name=name,
+                              pattern=pattern,
+                              pattern_type="stix") # characterize the pattern language that the indicator pattern is expressed in.
+        return True
 
     def run(self):
+        # todo: should i add support for both stix and slack together?
         # Here's how this module works:
         #1- the user specifies -a slack and adds SLACK_BOT_TOKEN to their environment variables
         #2- if you run slips using sudo use sudo -E instead to pass all env variables
@@ -132,15 +184,13 @@ class Module(Module, multiprocessing.Process):
                     return True
                 if message['channel'] == 'export_alert':
                     if type(message['data']) == str:
+                        # The data dict has two fields: export_to and msg
                         data = json.loads(message['data'])
-                        # this is exactly what will be sent to slack
                         msg_to_send = data.get("msg")
                         if 'slack' in data['export_to']:
                             self.send_to_slack(msg_to_send)
                         elif 'stix' in data['export_to'].lower():
-                            self.send_to_STIX(msg_to_send)
-                            pass
-                        
+                            self.export_to_STIX(msg_to_send)
         except KeyboardInterrupt:
             return True
         except Exception as inst:
