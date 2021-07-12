5b998d4712a064d117af47998dd6a4b5e0410ed4
alya <alyaggomaa@gmail.com>
2021-07-12 15:24:58 +0200

    test_define_type for every input separately

--- tests/test_profilerProcess.py
+++ tests/test_profilerProcess.py
@@ -1,8 +1,12 @@
 """ Unit test for ../profilerProcess.py """
 from ..profilerProcess import ProfilerProcess
+import subprocess
 import configparser
 import pytest
+import json
 
+
+#todo: make this a fixture
 def do_nothing(*args):
     """ Used to override the print function because using the self.print causes broken pipes """
     pass
@@ -16,7 +20,7 @@ def create_profilerProcess_instance(outputQueue, inputQueue):
     profilerProcess.print = do_nothing
     return profilerProcess
 
-def test_read_whitelist(outputQueue, inputQueue):
+def test_read_whitelist(outputQueue, inputQueue, database):
     """
     make sure the content of whitelists is read and stored properly
     uses tests/test_whitelist.conf for testing
@@ -40,27 +44,64 @@ def test_load_org_IPs(org,outputQueue, inputQueue, subnet):
     assert profilerProcess.load_org_IPs(org) != False
     assert subnet in profilerProcess.load_org_IPs(org)
 
-@pytest.mark.parametrize("file,expected_value",[('dataset/sample_zeek_files-2/conn.log','zeek-tabs'),
-                                                ('dataset/dataset/hide-and-seek-short.pcap','zeek'),
-                                                ('dataset/suricata-flows.json','suricata'),
-                                                ('dataset/test.nfdump','nfdump'),
-                                                ])
-def test_define_type(outputQueue, inputQueue, file, expected_value):
+@pytest.mark.parametrize("file,expected_value",[('dataset/suricata-flows.json','suricata')])
+def test_define_type_suricata(outputQueue, inputQueue, file, expected_value):
     profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
     with open(file) as f:
-        sample_flow = f.readline()
-    # sample_flow is always a str, try to convert to a dict
-    try:
-        sample_flow = dict(sample_flow)
-    except ValueError:
-        # if it's not a dict , eg zeek tab separated line, leave it as it is
-        pass
+        while True:
+            sample_flow = f.readline().replace('\n','')
+            # get the first line that isn't a comment
+            if not sample_flow.startswith('#'):
+                break
+    sample_flow = {'data': sample_flow,
+                   'type': expected_value}
+    assert profilerProcess.define_type(sample_flow) == expected_value
+
+@pytest.mark.parametrize("file,expected_value",[('dataset/sample_zeek_files-2/conn.log','zeek-tabs')])
+def test_define_type_zeek_tab(outputQueue, inputQueue, file, expected_value):
+    profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
+    with open(file) as f:
+        while True:
+            sample_flow = f.readline().replace('\n','')
+            # get the first line that isn't a comment
+            if not sample_flow.startswith('#'):
+                break
+    sample_flow = {'data': sample_flow,
+                   'type': expected_value}
+    assert profilerProcess.define_type(sample_flow) == expected_value
+
+@pytest.mark.parametrize("file,expected_value",[('dataset/sample_zeek_files/conn.log','zeek')])
+def test_define_type_zeek_dict(outputQueue, inputQueue, file, expected_value):
+    profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
+    with open(file) as f:
+        sample_flow = f.readline().replace('\n','')
+    sample_flow = json.loads(sample_flow)
     sample_flow = {'data': sample_flow,
                    'type': expected_value}
     assert profilerProcess.define_type(sample_flow) == expected_value
 
 
-@pytest.mark.parametrize("file,separator,expected_value",[('dataset/sample_zeek_files-2/conn.log','	',{'starttime': 1})])
+@pytest.mark.parametrize("nfdump_file",[('dataset/test.nfdump')])
+def test_define_type_nfdump(outputQueue, inputQueue, nfdump_file):
+    # nfdump files aren't text files so we need to process them first
+    command = 'nfdump -b -N -o csv -q -r ' + nfdump_file
+    # Execute command
+    result = subprocess.run(command.split(), stdout=subprocess.PIPE)
+    # Get command output
+    nfdump_output = result.stdout.decode('utf-8')
+    line = {'type': 'nfdump'}
+    for nfdump_line in nfdump_output.splitlines():
+        # this line is taken from stdout we need to remove whitespaces
+        nfdump_line.replace(' ','')
+        ts = nfdump_line.split(',')[0]
+        if not ts[0].isdigit():
+            continue
+        line['data']  = nfdump_line
+        break
+    profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
+    assert profilerProcess.define_type(line) == 'nfdump'
+
+@pytest.mark.parametrize("file,separator,expected_value",[('dataset/sample_zeek_files-2/conn.log','	',{'dur': 9, 'proto': 7, 'state': 12})])
 def test_define_columns(outputQueue, inputQueue,file,separator,expected_value):
     # define_columns is called on header lines
     # line = '#fields ts      uid     id.orig_h       id.orig_p       id.resp_h       id.resp_p       proto   service duration        orig_bytes      resp_bytes       conn_state      local_orig      local_resp      missed_bytes    history orig_pkts       orig_ip_bytes   resp_pkts       resp_ip_bytes   tunnel_parents'
@@ -68,9 +109,28 @@ def test_define_columns(outputQueue, inputQueue,file,separator,expected_value):
         while True:
             # read from the file until you find the header
             line = f.readline()
-            if line.startswith('#types'):
+            if line.startswith('#fields'):
                 break
     profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
     line = {'data': line}
     profilerProcess.separator = separator
     assert profilerProcess.define_columns(line) == expected_value
+
+
+# pcaps are treated as zeek files in slips, no need to test twice
+# @pytest.mark.parametrize("pcap_file",[('dataset/hide-and-seek-short.pcap')])
+# def test_define_type_pcap(outputQueue, inputQueue, pcap_file):
+#     # ('dataset/hide-and-seek-short.pcap','zeek')
+#     profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
+#
+#     # pcap files aren't text files so we need to process them first
+#     bro_parameter = '-r "' + pcap_file + '"'
+#     command =  "zeek -C " + bro_parameter + "  tcp_inactivity_timeout=60mins local -e 'redef LogAscii::use_json=T;' -f 2>&1 > /dev/null &"
+#     os.system(command)
+#     # Give Zeek some time to generate at least 1 file.
+#     time.sleep(3)
+#
+#     assert profilerProcess.define_type(line) == 'zeek'
+
+
+
