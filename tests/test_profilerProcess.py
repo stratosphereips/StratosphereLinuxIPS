17d277cb4e7db1b1fc8b4cf49e1667739640dddb
alya <alyaggomaa@gmail.com>
2021-07-15 23:54:53 +0200

    fix whitelist test to match branch alya-whitelists2

--- tests/test_profilerProcess.py
+++ tests/test_profilerProcess.py
@@ -14,10 +14,11 @@ def do_nothing(*args):
 def create_profilerProcess_instance(outputQueue, inputQueue):
     """ Create an instance of profilerProcess.py
         needed by every other test in this file  """
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(interpolation=None)
     profilerProcess = ProfilerProcess(inputQueue , outputQueue,1,0, config)
     # override the self.print function to avoid broken pipes
     profilerProcess.print = do_nothing
+    profilerProcess.whitelist_path = "tests/test_whitelist.conf"
     return profilerProcess
 
 def test_read_whitelist(outputQueue, inputQueue, database):
@@ -27,19 +28,19 @@ def test_read_whitelist(outputQueue, inputQueue, database):
     """
     profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
     # 9 is the number of lines read after the comment lines at th ebegging of the file
-    assert profilerProcess.read_whitelist("tests/test_whitelist.conf") == 9
+    assert profilerProcess.read_whitelist() == 9
     assert '91.121.83.118' in profilerProcess.whitelisted_IPs.keys()
     assert 'apple.com' in profilerProcess.whitelisted_domains.keys()
     assert 'microsoft' in profilerProcess.whitelisted_orgs.keys()
 
 @pytest.mark.parametrize("org,asn",[('google','as19448')])
-def test_load_org_asn(org,outputQueue, inputQueue, asn):
+def test_load_org_asn(org, outputQueue, inputQueue, asn):
     profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
     assert profilerProcess.load_org_asn(org) != False
     assert asn in profilerProcess.load_org_asn(org)
 
 @pytest.mark.parametrize("org,subnet",[('google','216.73.80.0/20')])
-def test_load_org_IPs(org,outputQueue, inputQueue, subnet):
+def test_load_org_IPs(org, outputQueue, inputQueue, subnet):
     profilerProcess = create_profilerProcess_instance(outputQueue, inputQueue)
     assert profilerProcess.load_org_IPs(org) != False
     assert subnet in profilerProcess.load_org_IPs(org)
