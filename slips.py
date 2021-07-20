6618cba6a3ade0dd7d46aa48e52f93cc7aacd9de
alya <alyaggomaa@gmail.com>
2021-07-20 02:20:21 +0200

    add suricata option to ExportingAlerts module

--- slips.py
+++ slips.py
@@ -402,8 +402,8 @@ if __name__ == '__main__':
         # Convert string to list
         to_ignore = to_ignore.replace("[","").replace("]","").replace(" ","").split(",")
         # Ignore exporting alerts module if export_to is empty
-        export_to = config.get('ExportingAlerts', 'export_to').rstrip("][").replace(" ","")
-        if 'stix' not in export_to.lower() and 'slack' not in export_to.lower():
+        export_to = config.get('ExportingAlerts', 'export_to').rstrip("][").replace(" ","").lower()
+        if 'stix' not in export_to and 'slack' not in export_to and 'suricata' not in export_to:
             to_ignore.append('ExportingAlerts')
         # Disable blocking if was not asked and if it is not interface
         if not args.blocking or not args.interface:
