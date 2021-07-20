938dd95309507d12757df2e74b30fc7337bc6548
alya <alyaggomaa@gmail.com>
2021-07-20 20:15:26 +0200

    fix broken tests

--- tests/test_update_file_manager.py
+++ tests/test_update_file_manager.py
@@ -29,13 +29,21 @@ def test_download_file(outputQueue, url):
     filepath = '.'
     assert update_manager.download_file(url,filepath) == True
 
-
-@pytest.mark.parametrize('url,etag', [('https://raw.githubusercontent.com/stratosphereips/StratosphereLinuxIPS/master/modules/template/__init__.py','c53b10ca5dc87b9dd21a6618940553ac09e8213c22b2c11ad31e997970d70a11'), ('https://raw.githubusercontent.com/stratosphereips/StratosphereLinuxIPS/master/modules/template/__init__.py','c53b10ca5dc87b9dd21a6618940553ac09e8213c22b2c11ad31e997970d70a11***')])
-def test_download_malicious_file(outputQueue, database,url, etag):
+@pytest.mark.parametrize('url', [('https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Todays-Blacklists/AIP_blacklist_for_IPs_seen_last_24_hours.csv')])
+def test_download_malicious_file(outputQueue, database,url):
+    """we're tetsing this condition old_e_tag == new_e_tag"""
     update_manager = create_update_manager_instance(outputQueue)
-    # we're tetsing 2 conditions
-    # old_e_tag != new_e_tag  and old_e_tag == new_e_tag
-    database.set_malicious_file_info(url,{'e-tag':etag})
+    old_etag = update_manager.get_e_tag_from_web(url)
+    database.set_malicious_file_info(url.split('/')[-1],{'e-tag':old_etag})
     assert update_manager.download_malicious_file(url) == True
 
-
+@pytest.mark.parametrize('url', [('https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Todays-Blacklists/AIP_blacklist_for_IPs_seen_last_24_hours.csv')])
+def test_download_malicious_file2(outputQueue, database,url):
+    """we're tetsing old_e_tag != new_e_tag"""
+    update_manager = create_update_manager_instance(outputQueue)
+    # setup old e-tag
+    old_etag = update_manager.get_e_tag_from_web(url)
+    # edit old e-tag to be different from the new e-tag
+    old_etag = old_etag.replace('0','*')
+    database.set_malicious_file_info(url.split('/')[-1],{'e-tag':old_etag})
+    assert update_manager.download_malicious_file(url) == True
