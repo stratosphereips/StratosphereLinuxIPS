fe930c2c121b9e0e4793f87b07303167d55997aa
Kamila Babayeva <kamifai14@gmail.com>
2021-07-21 21:40:18 +0200

    fix uid in binetflows

--- profilerProcess.py
+++ profilerProcess.py
@@ -1613,6 +1613,7 @@ class ProfilerProcess(multiprocessing.Process):
                 # In the case of other tools that are not Zeek, there is no UID. So we generate a new one here
                 # Zeeks uses human-readable strings in Base62 format, from 112 bits usually. We do base64 with some bits just because we need a fast unique way
                 uid = base64.b64encode(binascii.b2a_hex(os.urandom(9))).decode('utf-8')
+                self.column_values['uid'] = uid
 
             flow_type = self.column_values['type']
             self.saddr = self.column_values['saddr']
