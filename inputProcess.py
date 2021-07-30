cf3718cf5ff8de362259c0ab3c680327679b957f
alya <alyaggomaa@gmail.com>
2021-07-30 02:03:04 +0200

    add zeek script to change old log file names

--- inputProcess.py
+++ inputProcess.py
@@ -447,13 +447,13 @@ class InputProcess(multiprocessing.Process):
             command = "rm " + self.zeek_folder + "/*.log 2>&1 > /dev/null"
             os.system(command)
 
-            # Run zeek on the pcap or interface. The redef is to have json files
-            # To add later the home net: "Site::local_nets += { 1.2.3.0/24, 5.6.7.0/24 }"
-            zeek_scripts_dir = os.getcwd() + '/zeek-scripts'
-            command = f'cd {self.zeek_folder}; {self.zeek_or_bro} -C {bro_parameter} {self.tcp_inactivity_timeout} local -f {self.packet_filter} {zeek_scripts_dir} 2>&1 > /dev/null &'
-            self.print(f'Zeek command: {command}', 3, 0)
-            # Run zeek.
-            os.system(command)
+        # Run zeek on the pcap or interface. The redef is to have json files
+        # To add later the home net: "Site::local_nets += { 1.2.3.0/24, 5.6.7.0/24 }"
+        zeek_scripts_dir = os.getcwd() + '/zeek-scripts'
+        command = f'cd {self.zeek_folder}; {self.zeek_or_bro} -C {bro_parameter} {self.tcp_inactivity_timeout} local -f {self.packet_filter} {zeek_scripts_dir} 2>&1 > /dev/null &'
+        self.print(f'Zeek command: {command}', 3, 0)
+        # Run zeek.
+        os.system(command)
 
         # Give Zeek some time to generate at least 1 file.
         time.sleep(3)
