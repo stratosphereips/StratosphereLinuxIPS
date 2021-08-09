7fc226e8ee3b340b6abd9787a25f05edaf1c817c
alya <alyaggomaa@gmail.com>
2021-08-09 14:35:28 +0200

    fix not finding total number of profiles in suricata

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -2,8 +2,9 @@ import os
 # clear the database
 os.system('./slips.py -c slips.conf -cc')
 # run all unit tests
-# os.system('python3 -m pytest tests/ -n 10 --ignore="tests/test_dataset.py" -p no:warnings -v ')
+os.system('python3 -m pytest tests/ -n 5 --ignore="tests/test_dataset.py" -p no:warnings -v ')
 # test everything in our dataset
-# the command to run dataset tests is separated from the rest because slips will quit if one instance is using a db and another instance is trying to use
-# the same db. so dataset tests are designed to run on their own
+# the command to run dataset tests is separated from the rest because it takes so much time,
+# so it's better to know and fix the failing unit tests from the above
+# command before running the dataset tests
 os.system('python3 -m pytest tests/test_dataset.py -n 5 -p no:warnings -v -s')
