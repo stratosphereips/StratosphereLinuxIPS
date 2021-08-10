48906ab3d2e6b4a21ada3362b8b9e0ae943be94a
alya <alyaggomaa@gmail.com>
2021-08-11 01:57:57 +0200

    publish in reload_whitelist whenever whitelist.conf is modified

--- slips_files/core/database.py
+++ slips_files/core/database.py
@@ -1282,7 +1282,7 @@ class Database(object):
         pubsub = self.r.pubsub()
         supported_channels = ['tw_modified' , 'evidence_added' , 'new_ip' ,  'new_flow' , 'new_dns', 'new_dns_flow','new_http', 'new_ssl' , 'new_profile',\
                     'give_threat_intelligence', 'new_letters', 'ip_info_change', 'dns_info_change', 'dns_info_change', 'tw_closed', 'core_messages',\
-                    'new_blocking', 'new_ssh','new_notice','new_url', 'finished_modules', 'new_downloaded_file']
+                    'new_blocking', 'new_ssh','new_notice','new_url', 'finished_modules', 'new_downloaded_file', 'reload_whitelist']
         for supported_channel in supported_channels:
             if supported_channel in channel:
                 pubsub.subscribe(channel)
