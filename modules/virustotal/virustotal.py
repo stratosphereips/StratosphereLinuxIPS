e62c29bbdede2464abbc0190095e86367c670593
alya <alyaggomaa@gmail.com>
2021-08-17 15:34:45 +0200

    make sure vt response is a dict before processing it

--- modules/virustotal/virustotal.py
+++ modules/virustotal/virustotal.py
@@ -145,7 +145,7 @@ class Module(Module, multiprocessing.Process):
         """
         response = self.api_query_(url)
         # Can't get url report
-        if response.get('response_code','') is -1:
+        if type(response)== dict and response.get('response_code','') is -1:
             self.print(f"VT API returned an Error - {response['verbose_msg']}")
             return 0
         try:
