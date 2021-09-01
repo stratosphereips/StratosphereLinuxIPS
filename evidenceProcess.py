b2ab235b33f59f36cbaf691ea7da6b6746b42e8c
alya <alyaggomaa@gmail.com>
2021-09-01 22:32:17 +0200

    fix printed evidence format

--- evidenceProcess.py
+++ evidenceProcess.py
@@ -152,7 +152,7 @@ class EvidenceProcess(multiprocessing.Process):
         elif detection_module == 'SSHSuccessful':
             evidence_string = f'{profileid}_{twid}: IP {ip} did a successful SSH. {description}.'
         else:
-            evidence_string = f'{profileid}_{twid}: IP {ip} detected {dns_resolution_ip_final} {description}.'
+            evidence_string = f'{profileid}_{twid}: IP {ip} {dns_resolution_ip_final} detected {description}.'
 
         return evidence_string
 
