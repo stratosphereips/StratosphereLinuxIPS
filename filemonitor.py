0fb18fced0448bbc1898ceaeb301002d0ece90c0
alya <alyaggomaa@gmail.com>
2021-09-07 11:54:22 +0200

    fix adding whitelist.conf to zeek_files in the db

--- filemonitor.py
+++ filemonitor.py
@@ -34,7 +34,8 @@ class FileEventHandler(RegexMatchingEventHandler):
 
     def process(self, event):
         filename, ext = os.path.splitext(event.src_path)
-        __database__.add_zeek_file(filename)
+        if not 'whitelist' in filename:
+            __database__.add_zeek_file(filename)
 
     def on_modified(self, event):
         filename, ext = os.path.splitext(event.src_path)
