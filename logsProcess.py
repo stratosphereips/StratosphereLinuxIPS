a29c2cbc4d3bf0a9842149eba4c205753532e901
alya <alyaggomaa@gmail.com>
2021-09-09 20:41:43 +0200

    remove print

--- logsProcess.py
+++ logsProcess.py
@@ -229,8 +229,7 @@ class LogsProcess(multiprocessing.Process):
                             state ='not established'
                             if 'UDP' in dport:
                                 state = 'not answered'
-                        to_print += f'{ts} : {dport_name} {preposition} {daddr} {dport} {critical_warning} {state}'
-                        if 'answered' in to_print: print(to_print)
+                        to_print += f'{ts} : {dport_name} {preposition} {daddr} {dport} {critical_warning} {state}.'
                         if query: to_print+= f' query: {query}'
                         if ans: to_print += f' answers: {ans}'
                         if 'No' in trusted:  to_print += f', not trusted.'
