ef76089fa76e6e3ea894424b8636e3f742a6726c
alya <alyaggomaa@gmail.com>
2021-09-09 19:12:14 +0200

    don't print invalid data in ti files

--- modules/UpdateManager/update_file_manager.py
+++ modules/UpdateManager/update_file_manager.py
@@ -438,7 +438,7 @@ class UpdateFileManager:
                         # Store the ja3 in our local dict
                         malicious_ja3_dict[ja3] = json.dumps({'description': description, 'source':filename})
                     else:
-                        self.print('The data {} is not valid. It was found in {}.'.format(data, filename), 1, 1)
+                        self.print('The data {} is not valid. It was found in {}.'.format(data, filename), 3, 3)
                         continue
 
             # Add all loaded malicious ja3 to the database
@@ -632,7 +632,7 @@ class UpdateFileManager:
                                 # Store the ip in our local dict
                                 malicious_domains_dict[str(domain)] = json.dumps({'description': description, 'source':data_file_name})
                             else:
-                                self.print('The data {} is not valid. It was found in {}.'.format(data, malicious_data_path), 1, 1)
+                                self.print('The data {} is not valid. It was found in {}.'.format(data, malicious_data_path), 3, 3)
                                 continue
             # Add all loaded malicious ips to the database
             __database__.add_ips_to_IoC(malicious_ips_dict)
