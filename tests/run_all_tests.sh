89ebfadbab81224e57d2cde1d22eec6ee894bba6
alya <alyaggomaa@gmail.com>
2021-09-21 23:21:24 +0200

    make run_all_tests a shell script

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,17 +1,17 @@
-import os
-
+#!/bin/bash
 # clear the cache database
-os.system('./slips.py -c slips.conf -cc')
-os.system('./slips.py --killall ')
+./slips.py -c slips.conf -cc
+./slips.py --killall
 
 # run all unit tests, -n *5 means distribute tests on 5 different process
-os.system('python3  -m pytest tests/ --ignore="tests/test_dataset.py" -n 5 -p no:warnings -v ')
+python3  -m pytest tests/ --ignore="tests/test_dataset.py" -n 5 -p no:warnings -v
 
 # kill all redis servers before running the dataset tests
-os.system('./slips.py --killall ')
-
+./slips.py -k
 
 # the command to run dataset tests is separated from the rest because it takes so much time,
 # so it's better to know and fix the failing unit tests from the above
 # command before running the dataset tests
-os.system('python3 -m pytest tests/test_dataset.py -n 5 -p no:warnings -v -s')
+# distribute on 3 workers only because every worker will be spawning around 10 processes
+python3 -m pytest tests/test_dataset.py -n 3 -p no:warnings -v -s
+./slips.py -k 
