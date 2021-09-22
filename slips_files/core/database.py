f98f88ad37627cfd17110bf6627385ae18cc5848
alya <alyaggomaa@gmail.com>
2021-09-22 18:03:00 +0200

    remove duplicate line in gitignore

--- slips_files/core/database.py
+++ slips_files/core/database.py
@@ -1429,6 +1429,7 @@ class Database(object):
         # Convert to json string
         data = json.dumps(data)
         # Store in the hash 10.0.0.1_timewindow1, a key uid, with data
+        # to avoid storing the same uid and data twice
         value = self.r.hset(profileid + self.separator + twid + self.separator + 'flows', uid, data)
         if value:
             # The key was not there before. So this flow is not repeated
