2cac498236951776e809bb79e22d0570b843add5
alya <alyaggomaa@gmail.com>
2021-10-11 16:35:42 +0200

    Create leak_detector module

---
+++ modules/leak_detector/leak_detector.py
@@ -0,0 +1,84 @@
+# Ths is a template module for you to copy and create your own slips module
+# Instructions
+# 1. Create a new folder on ./modules with the name of your template. Example:
+#    mkdir modules/anomaly_detector
+# 2. Copy this template file in that folder.
+#    cp modules/template/template.py modules/anomaly_detector/anomaly_detector.py
+# 3. Make it a module
+#    touch modules/template/__init__.py
+# 4. Change the name of the module, description and author in the variables
+# 5. The file name of the python module (template.py) MUST be the same as the name of the folder (template)
+# 6. The variable 'name' MUST have the public name of this module. This is used to ignore the module
+# 7. The name of the class MUST be 'Module', do not change it.
+
+# Must imports
+from slips_files.common.abstracts import Module
+import multiprocessing
+from slips_files.core.database import __database__
+import platform
+import sys
+
+# Your imports
+
+
+class Module(Module, multiprocessing.Process):
+    # Name: short name of the module. Do not use spaces
+    name = 'leak_detector'
+    description = 'Detect leaks of data in the traffic'
+    authors = ['Alya Gomaa']
+
+    def __init__(self, outputqueue, config):
+        multiprocessing.Process.__init__(self)
+        self.outputqueue = outputqueue
+        self.config = config
+        # Start the DB
+        __database__.start(self.config)
+        # self.c1 = __database__.subscribe('new_ip')
+        self.timeout = None
+
+    def print(self, text, verbose=1, debug=0):
+        """
+        Function to use to print text using the outputqueue of slips.
+        Slips then decides how, when and where to print this text by taking all the processes into account
+        :param verbose:
+            0 - don't print
+            1 - basic operation/proof of work
+            2 - log I/O operations and filenames
+            3 - log database/profile/timewindow changes
+        :param debug:
+            0 - don't print
+            1 - print exceptions
+            2 - unsupported and unhandled types (cases that may cause errors)
+            3 - red warnings that needs examination - developer warnings
+        :param text: text to print. Can include format like 'Test {}'.format('here')
+        """
+
+        levels = f'{verbose}{debug}'
+        self.outputqueue.put(f"{levels}|{self.name}|{text}")
+
+    def run(self):
+        # Main loop function
+        while True:
+            try:
+                message = self.c1.get_message(timeout=self.timeout)
+                # Check that the message is for you. Probably unnecessary...
+                if message['data'] == 'stop_process':
+                    # Confirm that the module is done processing
+                    __database__.publish('finished_modules', self.name)
+                    return True
+                # if message['channel'] == 'new_ip':
+                #     # Example of printing the number of profiles in the
+                #     # Database every second
+                #     data = len(__database__.getProfiles())
+                #     self.print('Amount of profiles: {}'.format(data),3,0)
+
+            except KeyboardInterrupt:
+                # On KeyboardInterrupt, slips.py sends a stop_process msg to all modules, so continue to receive it
+                continue
+            except Exception as inst:
+                exception_line = sys.exc_info()[2].tb_lineno
+                self.print(f'Problem on the run() line {exception_line}', 0, 1)
+                self.print(str(type(inst)), 0, 1)
+                self.print(str(inst.args), 0, 1)
+                self.print(str(inst), 0, 1)
+                return True
