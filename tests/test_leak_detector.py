a35ea6592d535696700e21397ac84d3e90888b7a
alya <alyaggomaa@gmail.com>
2021-10-14 13:47:18 +0200

    add unit tests for find_matches and set_evidence_yara_match

--- tests/test_leak_detector.py
+++ tests/test_leak_detector.py
@@ -9,6 +9,14 @@ def do_nothing(*args):
     """ Used to override the print function because using the self.print causes broken pipes """
     pass
 
+# this file will be used for storing the module output
+# and deleted when the tests are done
+output_file = 'test_leak_detector_output.txt'
+test_pcap =  'dataset/hide-and-seek-short.pcap'
+yara_rules_path = 'tests/yara_rules_for_testing/rules/'
+compiled_yara_rules_path = 'tests/yara_rules_for_testing/compiled/'
+compiled_test_rule = compiled_yara_rules_path + 'test_rule.yara_compiled'
+
 def create_leak_detector_instance(outputQueue):
     """ Create an instance of leak_detector.py
         needed by every other test in this file  """
@@ -17,18 +25,30 @@ def create_leak_detector_instance(outputQueue):
     # override the self.print function to avoid broken pipes
     leak_detector.print = do_nothing
     # this is the path containing 1 yara rule for testing, it matches every pcap
-    leak_detector.yara_rules_path = 'tests/yara_rules_for_testing/'
-    leak_detector.compiled_yara_rules_path = 'tests/yara_rules_for_testing/'
-    leak_detector.pcap = 'dataset/hide-and-seek-short.pcap'
-    # this file will be deleted when the tests are done
-    leak_detector.output_file = 'test_leak_detector_output.txt'
+    leak_detector.yara_rules_path = yara_rules_path
+    leak_detector.compiled_yara_rules_path = compiled_yara_rules_path
+    leak_detector.pcap = test_pcap
+    leak_detector.output_file = output_file
     return leak_detector
 
 def test_compile_and_save_rules(outputQueue):
     leak_detector = create_leak_detector_instance(outputQueue)
     leak_detector.compile_and_save_rules()
-    assert 'test_rule.yara_compiled' in os.listdir('tests/yara_rules_for_testing/')
+    assert 'test_rule.yara_compiled' in os.listdir(compiled_yara_rules_path)
     # delete teh compiled file so it doesn't affect further unit tests
-    os.remove('tests/yara_rules_for_testing/test_rule.yara_compiled')
+    os.remove(compiled_test_rule)
+
+def test_find_matches(outputQueue):
+    """ tests find_matches and set_evidence_yara_match """
+    leak_detector = create_leak_detector_instance(outputQueue)
+    # first compile the test rule
+    leak_detector.compile_and_save_rules()
+    leak_detector.find_matches()
+    with open(output_file,'r') as f:
+        evidence_found = f.read()
+        assert f'Test_rule detected in {test_pcap}' in evidence_found
+    os.remove(compiled_test_rule)
+    os.remove(output_file)
+
 
 
