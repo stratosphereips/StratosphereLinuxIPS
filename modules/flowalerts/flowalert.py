ed47a143ecef3463a6568aaea9ca4d734077bdaf
Sebastian Garcia <eldraco@gmail.com>
2021-11-13 20:17:19 +0100

    flowalert. Filter out cymru.com from dns without resolution since ipwhois uses it

--- modules/flowalerts/flowalert.py
+++ modules/flowalerts/flowalert.py
@@ -386,7 +386,8 @@ class Module(Module, multiprocessing.Process):
         ## - All reverse dns resolutions
         ## - All .local domains
         ## - The wildcard domain *
-        if 'arpa' in domain or '.local' in domain or '*' in domain:
+        ## - Subdomains of cymru.com, since it is used by the ipwhois library to get the ASN of an IP and its range.
+        if 'arpa' in domain or '.local' in domain or '*' in domain or '.cymru.com' in domain[-10:]:
             return False 
 
         contacted_ips = __database__.get_all_contacted_ips_in_profileid_twid(profileid,twid)
