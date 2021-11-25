05b049e8a33b753f6153a62f515ddb6f7155bd3a
alya <alyaggomaa@gmail.com>
2021-11-25 12:51:21 +0200

    database: move storing dns resolutions logic from profilerprocess to add_out_dns in the database

--- slips_files/core/database.py
+++ slips_files/core/database.py
@@ -1847,7 +1847,7 @@ class Database(object):
         self.publish('new_notice', to_send)
         self.print('Adding notice flow to DB: {}'.format(data), 3, 0)
 
-    def add_out_dns(self, profileid, twid, stime, flowtype, uid, query, qclass_name, qtype_name, rcode_name, answers, ttls):
+    def add_out_dns(self, profileid, twid, stime, flowtype, uid, query, qclass_name, qtype_name, rcode_name, answers, ttls, starttime):
         """
         Store in the DB a DNS request
         All the type of flows that are not netflows are stored in a separate hash ordered by uid.
@@ -1863,6 +1863,11 @@ class Database(object):
         data['answers'] = answers
         data['ttls'] = ttls
         data['stime'] = stime
+
+        # Add DNS resolution to the db if there are answers for the query
+        if answers:
+            __database__.set_dns_resolution(query, answers, stime, uid, qtype_name, profileid, twid)
+
         # Convert to json string
         data = json.dumps(data)
         # Set the dns as alternative flow
