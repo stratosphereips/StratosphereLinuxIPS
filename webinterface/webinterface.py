f60d80f6b8a6381bfebeac75f5ea051acc3342db
kamilababayeva <51053878+kamilababayeva@users.noreply.github.com>
2021-11-26 18:12:11 +0100

    interface: add page for profiles and tws

--- webinterface/webinterface.py
+++ webinterface/webinterface.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request
 import redis
 import json
 # from slips_files.core.database import __database__
+
 app = Flask(__name__)
 
 # Connection to Slips redis database
 config = ""
 # __database__.start(config)
-__database__ = redis.Redis("localhost")
+__database__ = redis.StrictRedis("localhost",6379,charset="utf-8", decode_responses=True)
+
+
 # alerts_channel = __database__.subscribe('evidence_added')
 
 #
@@ -27,10 +30,30 @@ def index():
     return render_template('interface.html', title='Slips')
 
 
-@app.route('/alerts/')
+@app.route('/profiles_tws')
+def profile_tws():
+    profile_tws = __database__.hgetall('Profiles_TWs')
+    data = []
+    id = 0
+    for profileid, tws in profile_tws.items():
+        data.append({"id": str(id), "profile": profileid, "tws": json.loads(tws)})
+        id = id + 1
+    data_length = id
+    total_filtered = id
+    return {
+        'data': data,
+        'recordsFiltered': total_filtered,
+        'recordsTotal': data_length,
+        'draw': request.args.get('draw', type=int)
+    }
+
+
+@app.route('/alerts')
 def alerts():
     """
-    Create a databse of alerts in flask
+    Create a datatable with Slips alerts in flask web.
+    Data is stored in a route "/alerts".
+
     """
     alerts = __database__.smembers('Evidence')
     alerts = [json.loads(element) for element in alerts]
@@ -43,7 +66,9 @@ def alerts():
     # pagination
     start = request.args.get('start', type=int)
     length = request.args.get('length', type=int)
-    alerts_page = alerts[start:(start+length)]
+    alerts_page = ""
+    # if start and length:
+    alerts_page = alerts[start:(start + length)]
 
     return {
         'data': alerts_page,
@@ -51,7 +76,6 @@ def alerts():
         'recordsTotal': data_length,
         'draw': request.args.get('draw', type=int)
     }
-
 
 
 if __name__ == '__main__':
