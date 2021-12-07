f1c474f052033bf2423f0c0795328e4238a96db1
alya <alyaggomaa@gmail.com>
2021-12-07 21:49:31 +0200

    Add warden.conf and CESNET module

---
+++ modules/CESNET/CESNET.py
@@ -0,0 +1,80 @@
+# Must imports
+from slips_files.common.abstracts import Module
+import multiprocessing
+from slips_files.core.database import __database__
+import sys
+
+# Your imports
+
+
+class Module(Module, multiprocessing.Process):
+    # Name: short name of the module. Do not use spaces
+    name = 'CESNET'
+    description = 'Send and receive alerts from warden servers.'
+    authors = ['Alya Gomaa']
+
+    def __init__(self, outputqueue, config):
+        multiprocessing.Process.__init__(self)
+        # All the printing output should be sent to the outputqueue.
+        # The outputqueue is connected to another process called OutputProcess
+        self.outputqueue = outputqueue
+        # In case you need to read the slips.conf configuration file for
+        # your own configurations
+        self.config = config
+        # Start the DB
+        __database__.start(self.config)
+        self.c1 = __database__.subscribe('new_ip')
+        self.timeout = 0.0000001
+
+    def print(self, text, verbose=1, debug=0):
+        """
+        Function to use to print text using the outputqueue of slips.
+        Slips then decides how, when and where to print this text by taking all the processes into account
+        :param verbose:
+            0 - don't print
+            1 - basic operation/proof of work
+            2 - log I/O operations and filenames
+            3 - log database/profile/timewindow changes
+        :param debug:
+            0 - don't print
+            1 - print exceptions
+            2 - unsupported and unhandled types (cases that may cause errors)
+            3 - red warnings that needs examination - developer warnings
+        :param text: text to print. Can include format like 'Test {}'.format('here')
+        """
+
+        levels = f'{verbose}{debug}'
+        self.outputqueue.put(f"{levels}|{self.name}|{text}")
+
+
+    def read_configuration(self):
+        send_to_warden = self.config.get('CESNET', 'send_alerts').lower()
+        receive_from_warden = self.config.get('CESNET', 'receive_alerts').lower()
+        # receive_from_warden = self.config.get('CESNET', 'receive_alerts').lower()
+
+
+
+    def run(self):
+        # Main loop function
+        while True:
+            try:
+                message = self.c1.get_message(timeout=self.timeout)
+                # if timewindows are not updated for a long time, Slips is stopped automatically.
+                if message and message['data'] == 'stop_process':
+                    # confirm that the module is done processing
+                    __database__.publish('finished_modules', self.name)
+                    return True
+
+                if __database__.is_msg_intended_for(message, ''):
+                    pass
+
+            except KeyboardInterrupt:
+                # On KeyboardInterrupt, slips.py sends a stop_process msg to all modules, so continue to receive it
+                continue
+            except Exception as inst:
+                exception_line = sys.exc_info()[2].tb_lineno
+                self.print(f'Problem on the run() line {exception_line}', 0, 1)
+                self.print(str(type(inst)), 0, 1)
+                self.print(str(inst.args), 0, 1)
+                self.print(str(inst), 0, 1)
+                return True
