7763fe19481810289ddcfe197cd03fceebfaad3e
alya <alyaggomaa@gmail.com>
2021-12-07 22:37:43 +0200

    CESNET: Stop module if the configuration file is invalid or not found

--- modules/CESNET/CESNET.py
+++ modules/CESNET/CESNET.py
@@ -5,7 +5,8 @@ from slips_files.core.database import __database__
 import sys
 
 # Your imports
-
+from ..CESNET.warden_client import Client, Error, read_cfg, format_timestamp
+import os
 
 class Module(Module, multiprocessing.Process):
     # Name: short name of the module. Do not use spaces
@@ -51,8 +52,8 @@ class Module(Module, multiprocessing.Process):
     def read_configuration(self):
         """ Read importing/exporting preferences from slips.conf """
 
-        send_to_warden = self.config.get('CESNET', 'send_alerts').lower()
-        if send_to_warden == 'yes':
+        self.send_to_warden = self.config.get('CESNET', 'send_alerts').lower()
+        if self.send_to_warden == 'yes':
             # how often should we push to the server?
             try:
                 self.push_delay = int(self.config.get('CESNET', 'push_delay'))
@@ -60,8 +61,8 @@ class Module(Module, multiprocessing.Process):
                 # By default push every 1 day
                 self.push_delay = 86400
 
-        receive_from_warden = self.config.get('CESNET', 'receive_alerts').lower()
-        if receive_from_warden == 'yes':
+        self.receive_from_warden = self.config.get('CESNET', 'receive_alerts').lower()
+        if self.receive_from_warden == 'yes':
             # how often should we get alerts from the server?
             try:
                 self.receive_delay = int(self.config.get('CESNET', 'receive_delay'))
@@ -70,29 +71,13 @@ class Module(Module, multiprocessing.Process):
                 self.receive_delay = 86400
 
         self.configuration_file = self.config.get('CESNET', 'configuration_file')
+        if not os.path.exists(self.configuration_file):
+            self.print(f"Can't find warden.conf at {self.configuration_file}. Stopping module.")
+            self.stop_module = True
 
 
     def run(self):
-        # Main loop function
-        while True:
-            try:
-                message = self.c1.get_message(timeout=self.timeout)
-                # if timewindows are not updated for a long time, Slips is stopped automatically.
-                if message and message['data'] == 'stop_process':
-                    # confirm that the module is done processing
-                    __database__.publish('finished_modules', self.name)
-                    return True
+        # Stop module if the configuration file is invalid or not found
+        if self.stop_module:
+            return False
 
-                if __database__.is_msg_intended_for(message, ''):
-                    pass
-
-            except KeyboardInterrupt:
-                # On KeyboardInterrupt, slips.py sends a stop_process msg to all modules, so continue to receive it
-                continue
-            except Exception as inst:
-                exception_line = sys.exc_info()[2].tb_lineno
-                self.print(f'Problem on the run() line {exception_line}', 0, 1)
-                self.print(str(type(inst)), 0, 1)
-                self.print(str(inst.args), 0, 1)
-                self.print(str(inst), 0, 1)
-                return True
