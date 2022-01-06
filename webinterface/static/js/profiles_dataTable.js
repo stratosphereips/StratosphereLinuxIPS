29541c51451f62b54c586e8dd3e3a646c877f0da
kamilababayeva <51053878+kamilababayeva@users.noreply.github.com>
2022-01-06 13:06:22 +0600

    profiles_dataTable.js: retrieve ip info data and put in the table

--- webinterface/static/js/profiles_dataTable.js
+++ webinterface/static/js/profiles_dataTable.js
@@ -1,14 +1,5 @@
-
-let timewindows = {
-    'update_timeline': function (profile, timewindow) {
-        let s = '/timeline/' + profile + '/' + timewindow
-        datatable.ajax.url(s).load();
-    }
-}
-
-
 let profiles = function(){
- let table = $('#profiles').DataTable({
+    let table = $('#profiles').DataTable({
             ajax: '/profiles_tws',
             serverSide: true,
             "scrollY":        "700px",
@@ -34,49 +25,42 @@ let profiles = function(){
             ],
             "order": [[1, 'asc']]
             });
-return{
-    onclick_tws: function(){
-        function add_tws(profile_tws) {
-            const open_string = '<table class="table table-striped">'
-            const close_string = '</table>'
-            let data = ""
-            profile_tws.tws.forEach(item => {
-            data = data + '<tr onclick="timewindows.update_timeline(' + "'" + profile_tws.profile+"'" + ',' + "'" + item + "'" +')">' + '<td>'+ item + '</td>' + '</tr>';})
-            return open_string + data + close_string;
-        }
-
-        $('#profiles').on('click', 'tbody td.dt-control', function () {
-            let tr = $(this).closest('tr');
-            let row = table.row( tr );
-            if (row.child.isShown()) {
-                row.child.hide();
+    return{
+        onclick_tws: function(){
+            function add_tws(profile_tws) {
+                const open_string = '<table class="table table-striped">'
+                const close_string = '</table>'
+                let data = ""
+                profile_tws.tws.forEach(item => {
+                data = data + '<tr onclick="timewindows.update_timeline(' + "'" + profile_tws.profile+"'" + ',' + "'" + item + "'" +')">' + '<td>'+ item + '</td>' + '</tr>';})
+                return open_string + data + close_string;
             }
-            else {
-                row.child(add_tws(row.data())).show();
-            }
-        });
-    },
 
-    onclick_buttons: function(){
-        $('#profiles ').on( 'click', 'tbody button', function () {
-            let data = table.row( $(this).parents('tr')).data();
-            let profile_IP = data.profile.split("_")[1]
-            let url = '/info/' + profile_IP
-
-            $.getJSON(url, data => {
-                  let modal = $('#exampleModalCenter')
-                  modal.find('.modal-body').text(data.asn.asnorg)
-                  modal.find('.modal-body').append(data.geocountry)
-                  modal.modal('show')
+            $('#profiles').on('click', 'tbody td.dt-control', function () {
+                let tr = $(this).closest('tr');
+                let row = table.row( tr );
+                if (row.child.isShown()) {
+                    row.child.hide();
+                }
+                else {
+                    row.child(add_tws(row.data())).show();
+                }
             });
-        });
-    }
+        },
+
+        onclick_buttons: function(){
+            $('#profiles ').on( 'click', 'tbody button', function () {
+                let data = table.row( $(this).parents('tr')).data();
+                let profile_IP = data.profile.split("_")[1]
+                let url = '/info/' + profile_IP
+                ipinfo.ajax.url(url).load();
+            });
+        }
     }
 }
 let profile = profiles();
 profile.onclick_tws();
 profile.onclick_buttons();
-
 
 let datatable = $('#timeline').DataTable({
             "bDestroy": true,
@@ -97,5 +81,24 @@ let datatable = $('#timeline').DataTable({
             ]
 });
 
+let timewindows = {
+    'update_timeline': function (profile, timewindow) {
+        let s = '/timeline/' + profile + '/' + timewindow
+        datatable.ajax.url(s).load();
+    }
+}
+
+let ipinfo = $('#ipinfo').DataTable({
+            "bDestroy": true,
+            ordering:   false,
+            searching:  false,
+            "paging":   false,
+            "bInfo" :   false,
+            columns: [
+              {data: 'field'},
+              {data: 'value'}
+
+            ]
+});
 
 
