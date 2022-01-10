e36e27a040e70d53ce9969cc4ad970d17f788066
alya <alyaggomaa@gmail.com>
2022-01-10 16:15:19 +0200

    profilerprocess: fix not recognizing zeek files.log

--- profilerProcess.py
+++ profilerProcess.py
@@ -1265,7 +1265,7 @@ class ProfilerProcess(multiprocessing.Process):
             self.column_values['msg'] = line.get('msg', '') # we're looking for self signed certs in this field
             self.column_values['scanned_port'] = line.get('p', '')
             self.column_values['scanning_ip'] = line.get('src', '')
-        elif '/files' in file_type:
+        elif 'files' in file_type:
             """ Parse the fields we're interested in in the files.log file """
             # the slash before files to distinguish between 'files' in the dir name and file.log
             self.column_values['type'] = 'files'
