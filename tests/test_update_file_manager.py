f6fae714d0b08510e2e6b0340fc522142d974f2a
alya <alyaggomaa@gmail.com>
2022-01-13 18:29:18 +0200

    update update_file_manager unit tests

--- tests/test_update_file_manager.py
+++ tests/test_update_file_manager.py
@@ -38,7 +38,7 @@ def test_download_malicious_file(outputQueue, database,url):
     update_manager = create_update_manager_instance(outputQueue)
     old_etag = update_manager.get_e_tag_from_web(url)
     database.set_TI_file_info(url.split('/')[-1], {'e-tag':old_etag})
-    assert update_manager.download_malicious_file(url) == True
+    assert update_manager.update_TI_file(url) == True
 
 @pytest.mark.parametrize('url', [('https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Todays-Blacklists/AIP_blacklist_for_IPs_seen_last_24_hours.csv')])
 def test_download_malicious_file2(outputQueue, database,url):
@@ -49,4 +49,4 @@ def test_download_malicious_file2(outputQueue, database,url):
     # edit old e-tag to be different from the new e-tag
     old_etag = old_etag.replace('0','*')
     database.set_TI_file_info(url.split('/')[-1], {'e-tag':old_etag})
-    assert update_manager.download_malicious_file(url) == True
+    assert update_manager.update_TI_file(url) == True
