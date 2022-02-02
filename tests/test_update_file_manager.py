0b289603bf3fe0dd5f0a713ca362215e41784453
alya <alyaggomaa@gmail.com>
2022-02-02 14:29:32 +0200

    update unit tests

--- tests/test_update_file_manager.py
+++ tests/test_update_file_manager.py
@@ -20,29 +20,35 @@ def create_update_manager_instance(outputQueue):
 
 
 
-@pytest.mark.parametrize('file,etag', [('https://raw.githubusercontent.com/stratosphereips/StratosphereLinuxIPS/master/modules/template/__init__.py','c53b10ca5dc87b9dd21a6618940553ac09e8213c22b2c11ad31e997970d70a11')])
-def test_get_e_tag_from_web(outputQueue, file, etag):
+@pytest.mark.parametrize('file,etag', [('https://raw.githubusercontent.com/stratosphereips/StratosphereLinuxIPS/master/modules/template/__init__.py',
+                                                     'W/"4920b25bf5708ae099fc36dcf3a7fcf9393754f9c92e170b2dd04c08b58e6dca"')])
+def test_getting_header_fields(outputQueue, file, etag):
     update_manager = create_update_manager_instance(outputQueue)
-    assert update_manager.get_e_tag_from_web(file) == etag
+    response = update_manager.download_file(file)
+    assert response != False
+    assert update_manager.get_e_tag_from_web(response) == etag
+
 
 @pytest.mark.parametrize('url', ['https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Todays-Blacklists/AIP_blacklist_for_IPs_seen_last_24_hours.csv'])
 def test_download_file(outputQueue, url):
     update_manager = create_update_manager_instance(outputQueue)
-    filename = 'tests/AIP_blacklist_for_IPs_seen_last_24_hours.csv'
-    assert update_manager.download_file(url, filename) == True
-    os.remove(filename)
+    response = update_manager.download_file(url)
+    assert str(response) == '<Response [200]>'
 
 @pytest.mark.parametrize('url', [('https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Todays-Blacklists/AIP_blacklist_for_IPs_seen_last_24_hours.csv')])
 def test_download_malicious_file(outputQueue, database, url):
     """we're tetsing this condition old_e_tag != new_e_tag"""
     update_manager = create_update_manager_instance(outputQueue)
     # modify old e-tag of this file and store it in the database
-    old_etag = update_manager.get_e_tag_from_web(url)
+    response = update_manager.download_file(url)
+    assert response != False
+    old_etag = update_manager.get_e_tag_from_web(response)
     old_etag = '*' + old_etag[1:]
     database.set_TI_file_info(url.split('/')[-1], {'e-tag':old_etag})
     # we call this function to set the new self.new_e_tag
     # to something different than the old modified one
-    assert update_manager._UpdateFileManager__check_if_update(url) == True
+    # check_if_update returns a response if we should update or false if we shouldn't update
+    assert update_manager._UpdateFileManager__check_if_update(url) != False
 
 @pytest.mark.parametrize('url', [('https://mcfp.felk.cvut.cz/publicDatasets/CTU-AIPP-BlackList/Todays-Blacklists/AIP_blacklist_for_IPs_seen_last_24_hours.csv')])
 def test_download_malicious_file2(outputQueue, database, url):
@@ -50,7 +56,9 @@ def test_download_malicious_file2(outputQueue, database, url):
     update_manager = create_update_manager_instance(outputQueue)
 
     # setup old e-tag to be the current e-tag
-    old_etag = update_manager.get_e_tag_from_web(url)
+    response = update_manager.download_file(url)
+    assert response != False
+    old_etag = update_manager.get_e_tag_from_web(response)
     database.set_TI_file_info(url.split('/')[-1], {'e-tag':old_etag})
 
     assert update_manager._UpdateFileManager__check_if_update(url) == False
