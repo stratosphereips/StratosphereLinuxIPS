05b6f1564aabebf49cd69dab54c63f3bf4ed9241
alya <alyaggomaa@gmail.com>
2022-02-03 21:57:22 +0200

    CI: separate test_dataset.py from the rest of unit tests

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -8,4 +8,4 @@ os.system('python3 -m pytest tests/ tests/test_dataset.py -p no:warnings -vv ')
 # the command to run dataset tests is separated from the rest because it takes so much time,
 # so it's better to know and fix the failing unit tests from the above
 # command before running the dataset tests
-# os.system('python3 -m pytest tests/test_dataset.py -p no:warnings -vv -s')
+os.system('python3 -m pytest tests/test_dataset.py -p no:warnings -vv -s')
