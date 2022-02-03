05a07f01d0f6918e106b7add458d91497e3097ab
alya <alyaggomaa@gmail.com>
2022-02-03 16:43:31 +0200

    CI: don't separate dataset unit tests

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -3,9 +3,9 @@ import os
 # clear the database
 os.system('./slips.py -c slips.conf -cc')
 # run all unit tests
-os.system('python3 -m pytest tests/ --ignore="tests/test_dataset.py" -p no:warnings -vv ')
+os.system('python3 -m pytest tests/ tests/test_dataset.py -p no:warnings -vv ')
 # test everything in our dataset
 # the command to run dataset tests is separated from the rest because it takes so much time,
 # so it's better to know and fix the failing unit tests from the above
 # command before running the dataset tests
-os.system('python3 -m pytest tests/test_dataset.py -p no:warnings -vv -s')
+# os.system('python3 -m pytest tests/test_dataset.py -p no:warnings -vv -s')
