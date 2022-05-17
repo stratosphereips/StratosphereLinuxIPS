79d1d28a2f93eb44725daefab9a4a2d2324cf977
alya <alyaggomaa@gmail.com>
2022-05-17 16:55:29 +0200

    run_all_tests.sh: run database unit tests serially, the rest are parallelized

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # clear the cache database
-./slips.py -c slips.conf -cc
+./slips.py -cc -I
 ./slips.py --killall
 
 # run all unit tests, -n *5 means distribute tests on 5 different process
-python3  -m pytest tests/ --ignore="tests/test_dataset.py" -n 5 -p no:warnings -v
+# -s to see print statements as they are executed
+python3  -m pytest tests/ --ignore="tests/test_dataset.py" --ignore="tests/test_database.py" -n 5 -p no:warnings -vv -s
+# run db tests serially/ using 1 worker
+python3  -m pytest tests/test_database.py -n 1 -p no:warnings -vv -s --dist=loadfile
 
-# kill all redis servers before running the dataset tests
-./slips.py -k
+# kill all redis servers and clear cache
+# before running the integration tests
+./slips.py --killall
+./slips.py -cc -I
 
 # the command to run dataset tests is separated from the rest because it takes so much time,
 # so it's better to know and fix the failing unit tests from the above
 # command before running the dataset tests
-# distribute on 3 workers only because every worker will be spawning around 10 processes
-python3 -m pytest tests/test_dataset.py -n 3 -p no:warnings -v -s
-./slips.py -k 
+# distribute on 3 workers only because every worker will be spawning 10+ processes
+python3 -m pytest tests/test_dataset.py -n 3 -p no:warnings -vv -s
+./slips.py --killall
+./slips.py -cc -I
