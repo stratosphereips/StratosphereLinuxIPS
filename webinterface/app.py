81fbdfbce4458e9e7d76ed8e689ad3273b8a4208
Kamila Babayeva <kamifai14@gmail.com>
2022-06-18 11:27:06 +0200

    webinterface/app.py: register blueprint and use debug mode

--- webinterface/app.py
+++ webinterface/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
 from hotkeys.hotkeys import hotkeys
-
-app = Flask(__name__)
-
+from general.general import general
 
 app = Flask(__name__)
 app.register_blueprint(hotkeys)
+app.register_blueprint(general, url_prefix="/general")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
