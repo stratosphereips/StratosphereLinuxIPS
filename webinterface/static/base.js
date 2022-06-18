761772108924e43f52feee00ed6c48f2ec741d16
Kamila Babayeva <kamifai14@gmail.com>
2022-06-18 11:31:43 +0200

    remove base.js for now

---
+++ webinterface/static/base.js
@@ -0,0 +1,8 @@
+//$(document).ready(function(){
+//
+//$(".nav-link").on("click", function(){
+//   $("one").removeClass("active");
+//   alert(this);
+//   $("two").addClass("active");
+//});
+//});
