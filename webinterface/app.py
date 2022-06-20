cb2a64f57c3c991ff5b37423cda4005bf056afb3
Kamila Babayeva <kamifai14@gmail.com>
2022-06-20 15:44:53 +0200

    app.py: add home route

--- webinterface/app.py
+++ webinterface/app.py
@@ -3,8 +3,12 @@ from hotkeys.hotkeys import hotkeys
 from general.general import general
 
 app = Flask(__name__)
-app.register_blueprint(hotkeys)
+app.register_blueprint(hotkeys, url_prefix="/hotkeys")
 app.register_blueprint(general, url_prefix="/general")
+
+@app.route('/')
+def index():
+    return render_template('base.html', title='Slips')
 
 if __name__ == '__main__':
     app.run(debug=True)
