6e0c24689a7d2e445d36dfb00edb0039f53f1e4b
Kamila Babayeva <kamifai14@gmail.com>
2022-06-20 15:46:54 +0200

    hotkeys.js: move profile to common base.js

--- webinterface/hotkeys/static/js/hotkeys.js
+++ webinterface/hotkeys/static/js/hotkeys.js
@@ -1,68 +1,3 @@
-/*
-Set profile and timewindow table.
-Functions:
-    onclick_tws:
-    onclick_ips: display a list of timewindows onclick, and the IP info
-*/
-let profiles = function () {
-    let profiles_table = $('#profiles').DataTable({
-        ajax: '/profiles_tws',
-        serverSide: true,
-        "scrollY": "700px",
-        "scrollCollapse": true,
-        "paging": false,
-        "bInfo": false,
-        ordering: false,
-        searching: false,
-        "rowId": 'id',
-        columns: [
-            {
-                "className": 'dt-control',
-                "orderable": false,
-                "data": null,
-                "defaultContent": ''
-            },
-            {
-                data: 'profile',
-                "className": 'r'
-            }
-
-        ],
-        "order": [[1, 'asc']]
-    });
-    return {
-        onclick_tws: function () {
-            function add_tws(profile_tws) {
-                const open_string = '<table class="table table-striped">'
-                const close_string = '</table>'
-                let data = ""
-                profile_tws.tws.forEach(item => {
-                    data = data + '<tr onclick="hotkey_hook.initialize_profile_timewindow(' + "'" + "profile_" + profile_tws.profile + "'" + ',' + "'" + item + "'" + ')">' + '<td>' + item + '</td>' + '</tr>';
-                })
-                return open_string + data + close_string;
-            }
-
-            $('#profiles').on('click', 'tbody td.dt-control', function () {
-                let tr = $(this).closest('tr');
-                let row = profiles_table.row(tr);
-                if (row.child.isShown()) {
-                    row.child.hide();
-                }
-                else {
-                    row.child(add_tws(row.data())).show();
-                }
-            });
-        },
-
-        onclick_ips: function () {
-            $('#profiles ').on('click', 'tbody td.r', function () {
-                let data = profiles_table.row($(this).parents('tr')).data();
-                let url = '/info/' + data.profile
-                ipinfo.ajax.url(url).load();
-            });
-        }
-    }
-}
 
  let operate_hotkeys = function () {
     let profile = '';
@@ -160,7 +95,7 @@ let profiles = function () {
                 active_hotkey_table = alerts
                 break;
         }
-        let link = "/" + active_hotkey_name + "/" + profile + "/" + timewindow
+        let link = "/hotkeys/" + active_hotkey_name + "/" + profile + "/" + timewindow
         active_hotkey_table.ajax.url(link).load();
         document.getElementById(active_hotkey_name).style.display = "block"
     }
@@ -192,7 +127,7 @@ let profiles = function () {
         onclick_timeline_flows_saddr: function () {
         $('#table_timeline_flows ').on('click', 'tbody td.saddr', function () {
                 let data = timeline_flows.row($(this).parents('tr')).data();
-                let url = '/info/' + data.saddr
+                let url = '/hotkeys/info/' + data.saddr
                 ipinfo.ajax.url(url).load();
             })
         },
@@ -200,7 +135,7 @@ let profiles = function () {
         onclick_timeline_flows_daddr: function () {
         $('#table_timeline_flows ').on('click', 'tbody td.daddr', function () {
                 let data = timeline_flows.row($(this).parents('tr')).data();
-                let url = '/info/' + data.daddr
+                let url = '/hotkeys/info/' + data.daddr
                 ipinfo.ajax.url(url).load();
             })
         }
@@ -221,9 +156,6 @@ let ipinfo = $('#ipinfo').DataTable({
     ]
 });
 
-let profile = profiles();
-profile.onclick_tws();
-profile.onclick_ips();
 
 let hotkeys = operate_hotkeys();
 hotkeys.onclick_buttons();
