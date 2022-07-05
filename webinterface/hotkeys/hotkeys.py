defb3b30397674294fd08cdc96f0bec3243b026f
Kamila Babayeva <kamifai14@gmail.com>
2022-07-05 23:49:44 +0200

    hotkeys.py: set documentation, remove dom filter/pagination functionality, fix python function naming

--- webinterface/hotkeys/hotkeys.py
+++ webinterface/hotkeys/hotkeys.py
@@ -14,12 +14,12 @@ class Hotkeys:
 
         # Routes should be set explicity, because Flask process self parameter in function wrong.
         self.bp.add_url_rule("/", view_func=self.index)
-        self.bp.add_url_rule("/profiles_tws", view_func=self.profile_tws)
+        self.bp.add_url_rule("/profiles_tws", view_func=self.set_profile_tws)
         self.bp.add_url_rule("/info/<ip>", view_func=self.set_ip_info)
-        self.bp.add_url_rule("/dstIP/<profile>/<timewindow>", view_func=self.setDstIPflow)
+        self.bp.add_url_rule("/dstIP/<profile>/<timewindow>", view_func=self.set_DstIPflow)
         self.bp.add_url_rule("/outtuples/<profile>/<timewindow>", view_func=self.set_outtuples)
-        self.bp.add_url_rule("/DstPortsClientUDPNotEstablished", view_func=self.setDstPortClientUDPNotEstablished)
         self.bp.add_url_rule("/intuples/<profile>/<timewindow>", view_func=self.set_intuples)
+        self.bp.add_url_rule("/DstPortsClientUDPNotEstablished", view_func=self.set_dstPortsClientUDPNotEstablished)
         self.bp.add_url_rule("/timeline_flows/<profile>/<timewindow>", view_func=self.set_timeline_flows)
         self.bp.add_url_rule("/timeline/<profile>/<timewindow>", view_func=self.set_timeline)
 
@@ -27,6 +27,11 @@ class Hotkeys:
         return render_template('hotkeys.html', title='Slips')
 
     def get_ip_info(self, ip):
+        """
+        Retrieve IP information from database
+        :param ip: active IP
+        :return: all data about the IP in database
+        """
         ip_info = json.loads(self.cache.hget('IPsInfo', ip))
 
         # Hardcoded decapsulation due to the complexity of data in side. Ex: {"asn":{"asnorg": "CESNET", "timestamp": 0.001}}
@@ -41,21 +46,21 @@ class Hotkeys:
     def set_ip_info(self, ip):
         '''
         Set info about the ip in route /info/<ip> (geocountry, asn, TI)
+        :param ip: active IP
+        :return: information about IP in database
         '''
         ip_info = self.get_ip_info(ip)
         ip_info["ip"] = ip
         data = [ip_info]
 
         return {
-            'data': data,
-            'recordsFiltered': len(data),
-            'recordsTotal': len(data),
-            'draw': request.args.get('draw', type=int)
+            'data': data
         }
 
-    def profile_tws(self):
+    def set_profile_tws(self):
         '''
-        Set profiles and their timewindows data into the tree.
+        Set profiles and their timewindows into the tree. Blocked are highligted in red.
+        :return: (profile, [tw, blocked], blocked)
         '''
 
         # Fetch blocked
@@ -86,20 +91,17 @@ class Hotkeys:
             data.append({"id": str(id), "profile": profile_ip, "tws": dict_tws, "blocked": blocked_profile})
             id = id + 1
 
-        # start = request.args.get('start', type=int)
-        # length = request.args.get('length', type=int)
-        data_length = id
-        total_filtered = id
-        # profiles_page = data[start:(start + length)]
-
         return {
-            'data': data,
-            'recordsFiltered': total_filtered,
-            'recordsTotal': data_length,
-            'draw': request.args.get('draw', type=int)
+            'data': data
         }
 
-    def setDstIPflow(self, profile, timewindow):
+    def set_DstIPflow(self, profile, timewindow):
+        """
+        Set flows per each destination IP
+        :param profile: active profile
+        :param timewindow: active timewindow
+        :return: data with flows per ip
+        """
         dst_ips = json.loads(self.db.hget(profile + '_' + timewindow, 'DstIPs'))
         data = []
         id = 0
@@ -107,17 +109,12 @@ class Hotkeys:
             data.append({"ip": ip, "flow": port})
             id = id + 1
 
-        data_length = id
-        total_filtered = id
-
         return {
-            'data': data,
-            'recordsFiltered': total_filtered,
-            'recordsTotal': data_length,
-            'draw': request.args.get('draw', type=int)
+            'data': data
         }
 
-    def setDstPortClientUDPNotEstablished(self):
+    def set_dstPortsClientUDPNotEstablished(self):
+
         dst_ips = json.loads(self.db.hget('profile_192.168.2.16_timewindow1', 'DstPortsClientUDPNotEstablished'))
         data = []
         id = 0
@@ -125,22 +122,35 @@ class Hotkeys:
             data.append({"port": port, "info": info})
             id = id + 1
 
-        data_length = id
-        total_filtered = id
-
         return {
-            'data': data,
-            'recordsFiltered': total_filtered,
-            'recordsTotal': data_length,
-            'draw': request.args.get('draw', type=int)
+            'data': data
         }
 
     def set_outtuples(self, profile, timewindow):
         """
-        Create a datatable with Slips outtuples.
+        Set outtuples of a chosen profile and timewindow.
+        :param profile: active profile
+        :param timewindow: active timewindow
+        :return: (tuple, key, ip_info)
         """
+
+        data = []
         outtuples = self.db.hget(profile + '_' + timewindow, 'OutTuples')
-        outtuples = json.loads(outtuples)
+        if outtuples:
+            outtuples = json.loads(outtuples)
+
+            for key, value in outtuples.items():
+                ip, port, protocol = key.split("-")
+                ip_info = self.get_ip_info(ip)
+                outtuple_dict = dict()
+                outtuple_dict.update({'tuple': key, 'string': value[0]})
+                outtuple_dict.update(ip_info)
+                data.append(outtuple_dict)
+
+        return {
+            'data': data
+        }
+
     def set_intuples(self, profile, timewindow):
         """
         Set intuples of a chosen profile and timewindow.
@@ -167,58 +177,32 @@ class Hotkeys:
 
     def set_timeline_flows(self, profile, timewindow):
         """
-        Set timeline flows of a chosen profile and timewindow. Supports pagination, sorting and seraching.
+        Set timeline flows of a chosen profile and timewindow.
+        :param profile: active profile
+        :param timewindow: active timewindow
+        :return: list of timeline flows as set initially in database
         """
-        timeline = self.db.hgetall(profile + "_" + timewindow + "_flows")
-        data = [json.loads(value) for key, value in timeline.items()]
-        data_length = len(data)
-        total_filtered = len(data)
-
-        # search
-        search = request.args.get('search[value]')
-        if search:
-            data = [element for element in data if element['proto'].lower() == search.lower()]
-            total_filtered = len(data)
-
-        # pagination
-        start = request.args.get('start', type=int)
-        length = request.args.get('length', type=int)
-        data_page = []
-        if start and length:
-            data_page = data[start:(start + length)]
+        data = []
+        timeline_flows = self.db.hgetall(profile + "_" + timewindow + "_flows")
+        if timeline_flows:
+            data = [json.loads(value) for key, value in timeline_flows.items()]
 
         return {
-            'data': data_page if data_page else data,
-            'recordsFiltered': total_filtered,
-            'recordsTotal': data_length,
-            'draw': request.args.get('draw', type=int)
+            'data': data
         }
 
     def set_timeline(self, profile, timewindow):
         """
-        Set timeline data of a chosen profile and timewindow. Supports pagination, sorting and seraching.
+        Set timeline data of a chosen profile and timewindow
+        :param profile: active profile
+        :param timewindow: active timewindow
+        :return: list of timeline as set initially in database
         """
+        data = []
         timeline = self.db.zrange(profile + "_" + timewindow + "_timeline", 0, -1)
-        data = [json.loads(line) for line in timeline]
-        data_length = len(data)
-        total_filtered = len(data)
-        search = request.args.get('search[value]')
-
-        # search
-        if search:
-            data = [element for element in data if element['proto'].lower() == search.lower()]
-            total_filtered = len(data)
-
-        # pagination
-        start = request.args.get('start', type=int)
-        length = request.args.get('length', type=int)
-        data_page = []
-        if start and length:
-            data_page = data[start:(start + length)]
+        if timeline:
+            data = [json.loads(line) for line in timeline]
 
         return {
-            'data': data_page if data_page else data,
-            'recordsFiltered': total_filtered,
-            'recordsTotal': data_length,
-            'draw': request.args.get('draw', type=int)
+            'data': data
         }
