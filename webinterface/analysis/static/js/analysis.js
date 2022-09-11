cfea37f79976221cc7ee897a9356f0d1255c49ac
Kamila Babayeva <kamifai14@gmail.com>
2022-09-11 12:26:03 +0200

    analhysis.js: remove function incharge of analysis tables

--- webinterface/analysis/static/js/analysis.js
+++ webinterface/analysis/static/js/analysis.js
@@ -15,39 +15,6 @@ function initAnalysisTables(){
     }
 }
 
- let operate_hotkeys = function () {
-
-
-
-    function add_table_evidence(d) {
-        let table_id = d["alert_id"]
-        let entry ='<table' + ' id="'+ table_id + '"' + 'class="table table-striped" cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'
-        let exit = '</table>'
-        let head ="<thead>"+
-         "<tr>"+
-         "<th>Evidence</th>" +
-         "<th>Confidence</th>" +
-         "<th>Threat Level</th>" +
-         "<th>Category</th>" +
-          "<th>Tag</th>" +
-         "<th>Description</th>" +
-         "</tr>"+
-         "</thead>"
-        return (entry + head  + exit);
-    }
-
-    function hide_hotkey() {
-        document.getElementById(last_active_hotkey_name).style.display = "none"
-        last_active_hotkey_name = active_hotkey_name;
-    }
-
-    function update_table(){
-        if(active_profile && active_timewindow){
-            let link = "/analysis/" + active_hotkey_name + "/" + active_profile + "/" + active_timewindow
-            active_hotkey_table.ajax.url(link).load();}
-        document.getElementById(active_hotkey_name).style.display = "block"
-    }
-
 function updateAnalysisTable(analysisTag){
     if(active_profile && active_timewindow){
         let link = "/analysis/" + active_hotkey_name + "/" + active_profile + "/" + active_timewindow
@@ -63,118 +30,13 @@ function updateAnalysisTable(analysisTag){
             }
         }
     }
-
-    return {
-
-        update_hook: function(){
-            update_hotkey()
-        },
-
-        search_reload: function(filter_parameter){
-           let link = "/analysis/" + active_hotkey_name + "/" + profile + "/" + timewindow
-            if (filter_parameter){ link += "/" + filter_parameter; }
-            active_hotkey_table.ajax.url(link).load();
-        },
-
-        onclick_buttons: function () {
-            $("#buttons .btn").click(function () {
-                $("#buttons .btn").removeClass('active');
-                $(this).toggleClass('active');
-                let [first, ...rest] = (this.id).split('_');
-                active_hotkey_name = rest.join('_');
-                if (active_hotkey_name != last_active_hotkey_name) {
-                    hide_hotkey();
-                }
-               update_hotkey()
-            });
-        },
-
-        onclick_timeline_flows_saddr: function () {
-        $('#table_timeline_flows ').on('click', 'tbody td.saddr', function () {
-                let data = timeline_flows.row($(this).parents('tr')).data();
-                let url = '/analysis/info/' + data.saddr;
-                ipinfo.ajax.url(url).load();
-            })
-        },
-
-        onclick_timeline_flows_daddr: function () {
-        $('#table_timeline_flows ').on('click', 'tbody td.daddr', function () {
-                let data = timeline_flows.row($(this).parents('tr')).data();
-                let url = '/analysis/info/' + data.daddr;
-                ipinfo.ajax.url(url).load();
-            })
-        },
-
-        onclick_timeline_daddr: function () {
-        $('#table_timeline ').on('click', 'tbody td.daddr', function () {
-                let data = timeline.row($(this).parents('tr')).data();
-                let url = '/analysis/info/' + data.daddr;
-                ipinfo.ajax.url(url).load();
-            })
-        },
-
-        onclick_alerts: function () {
-            $('#table_alerts ').on('click', 'tbody td.r', function () {
-                var tr = $(this).closest('tr');
-                var row = alerts.row(tr);
-                if (row.child.isShown()) {
-                    row.child.hide();
-                    tr.removeClass('shown');
-                } else {
-                    row.child(add_table_evidence(row.data())).show();
-                    let table_id = "#" + row.data()["alert_id"]
-                    let evidence = $(table_id).DataTable({
-                        "bDestroy": true,
-                        dom: custom_dom,
-                        bInfo: false,
-                        paging: false,
-                        searching: false,
-                        scrollY: "25vh", // hardcoded length of opened datatable
-                        columns: [
-                            { data: 'stime'},
-                            { data: 'confidence'},
-                            { data: 'threat_level'},
-                            { data: 'category'},
-                            { data: 'source_target_tag'},
-                            { data: 'description'}
-                        ]
-                    });
-                    let link = "/analysis/evidence/" + active_profile + "/" + active_timewindow + "/" + row.data()["alert_id"]
-                    evidence.ajax.url(link).load();
-                    tr.addClass('shown');
-                }
-            });
-        }
-    }
+    document.getElementById(active_hotkey_name).style.display = "block"
 }
 
 
 
-let profile_handle = profiles();
-profile_handle.onclick_tws();
-profile_handle.onclick_ips();
-
-let hotkeys = operate_hotkeys();
-hotkeys.onclick_buttons();
-hotkeys.onclick_timeline_flows_saddr();
-hotkeys.onclick_timeline_flows_daddr();
-hotkeys.onclick_timeline_daddr();
-hotkeys.onclick_alerts();
-
-let hotkey_hook = {
-    'initialize_profile_timewindow': function () {
-        hotkeys.update_hook();
-    }
 }
 
-$('#table_timeline_filter_button').click(function(){
-    var filter_gender = $('#table_timeline_filter_input').val();
-    if(filter_gender != ''){hotkeys.search_reload(filter_gender);}
-    else{hotkeys.search_reload(filter_gender);}
-});
-
-function updateTable(){
-    hotkeys.update_hook()
 function add_table_evidence(d) {
     let table_id = d["alert_id"]
     let entry ='<table' + ' id="'+ table_id + '"' + 'class="table table-striped" cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'
