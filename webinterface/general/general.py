fc8c2ee3c32d553a1a12b52acdad71cbef8906d7
Kamila Babayeva <kamifai14@gmail.com>
2022-09-17 13:08:08 +0200

    general: remove the class from the blueprint and add the database

--- webinterface/general/general.py
+++ webinterface/general/general.py
@@ -1,35 +1,32 @@
 from flask import Blueprint
 from flask import Flask, render_template, request
-import redis
-import json
+from database.database import __database__
 
-
-class General:
-
-    def __init__(self, database, cache):
-        self.db = database
-        self.cache = cache
-        self.bp = Blueprint('general', __name__, static_folder='static', static_url_path='/general/static',
+general = Blueprint('general', __name__, static_folder='static', static_url_path='/general/static',
                             template_folder='templates')
 
-        # Routes should be set explicity, because Flask process self parameter in function wrong.
-        self.bp.add_url_rule("/", view_func=self.index)
-        self.bp.add_url_rule("/blockedProfileTWs", view_func=self.setBlockedProfileTWs)
 
-    def index(self):
-        return render_template('general.html')
+@general.route("/")
+def index():
+    return render_template('general.html')
 
-    def setBlockedProfileTWs(self):
-        '''
-        Function to set blocked profiles and tws
-        '''
-        blockedProfileTWs = self.db.hgetall('BlockedProfTW')
-        data = []
 
-        if blockedProfileTWs:
-            for profile, tws in blockedProfileTWs.items():
-                data.append({"blocked": profile + str(tws)})
+@general.route("/blockedProfileTWs")
+def setBlockedProfileTWs():
+    '''
+    Function to set blocked profiles and tws
+    '''
+    blockedProfileTWs = __database__.db.hgetall('BlockedProfTW')
+    data = []
 
-        return {
-            'data': data,
-        }
+    if blockedProfileTWs:
+        for profile, tws in blockedProfileTWs.items():
+            data.append({"blocked": profile + str(tws)})
+
+    return {
+        'data': data,
+    }
+
+
+
+
