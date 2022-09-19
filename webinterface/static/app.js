750bc3d94920790ca3499d6744f2fbfb6386dca9
Kamila Babayeva <kamifai14@gmail.com>
2022-09-19 11:00:26 +0200

    app.js: init table with available redis ports

--- webinterface/static/app.js
+++ webinterface/static/app.js
@@ -12,4 +12,20 @@ fetch("/info", {
                         document.getElementById("fileSize").textContent=data['size_in_MB'];
                         document.getElementById("analysisStart").textContent=data['analysis_start'];
                         document.getElementById("analysisEnd").textContent=data['analysis_end'];
-                    });
+                    });    destroy: true,
+    searching: false,
+    ajax: '/redis',
+    "bInfo": false,
+    scrollY: 100,
+    paging: false,
+    select:true,
+    columns: [
+        { data: 'filename' },
+        { data: 'redis_port' }
+    ],
+    "fnInitComplete": function( settings, json ) {
+        $('#table_choose_redis tbody tr:eq(0)').click();
+    }
+})
+
+
