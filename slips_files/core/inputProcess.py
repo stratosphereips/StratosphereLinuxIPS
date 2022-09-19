7ed633c5167f3632b1ece7da83465fc22e0f845c
alya <alyaggomaa@gmail.com>
2022-09-19 11:59:18 +0200

    filemonitor: fix marking pcap zeek dirs and growing zeek directories

--- slips_files/core/inputProcess.py
+++ slips_files/core/inputProcess.py
@@ -484,7 +484,7 @@ class InputProcess(multiprocessing.Process):
         # some process to tell us which files to read in real time when they appear
         # Get the file eventhandler
         # We have to set event_handler and event_observer before running zeek.
-        event_handler = FileEventHandler(self.redis_port, self.zeek_folder)
+        event_handler = FileEventHandler(self.redis_port, self.zeek_folder, self.input_type)
         # Create an observer
         self.event_observer = Observer()
         # Schedule the observer with the callback on the file handler
