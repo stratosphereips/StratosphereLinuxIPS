531d6cd4b286e01ed7588068882f738b3cc9a761
Kamila Babayeva <kamifai14@gmail.com>
2022-10-06 21:19:21 +0200

    create widgets from common style listtable

---
+++ modules/kalipso/kalipso_widgets/ipinfo.js
@@ -0,0 +1,15 @@
+const { redis, blessed, blessed_contrib } = require("./libraries.js");
+const listTable = require("../lib_widgets/listtable.js")
+var async = require('async')
+var color = require('chalk')
+var stripAnsi = require('strip-ansi')
+
+class IpInfo extends listTable.ListTableClass{
+
+    constructor(grid, redis_database,screen, characteristics){
+        super(grid, redis_database, screen, characteristics)
+    }
+
+}
+
+module.exports = {IpInfoClass:IpInfo}
