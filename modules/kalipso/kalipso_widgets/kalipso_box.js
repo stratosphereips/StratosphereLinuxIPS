f2409cbb5d85619fd367dc01e99ffb7a41f5ae07
Kamila Babayeva <kamifai14@gmail.com>
2022-10-06 20:27:12 +0200

    kalipso_box: remove evidence function

--- modules/kalipso/kalipso_widgets/kalipso_box.js
+++ modules/kalipso/kalipso_widgets/kalipso_box.js
@@ -53,61 +53,6 @@ class Box{
     focus(){
         this.widget.focus()
     }
-
-    /*Widget 'Box' is used to display the evidences in the main screen of Kalipso.
-      This function generate the evidence data to be put in the box.
-      It retrieves the data from the Redis database and put it in the necessary format for the widget.
-      */
-    setEvidence(ip, timewindow){
-        try{
-            var evidence_data = ''
-            this.redis_database.getEvidence(ip, timewindow).then(redis_evidence_data=>{
-
-                if (redis_evidence_data==null){
-                    return this.setData(evidence_data)
-                }
-
-                var evidence_json = JSON.parse(redis_evidence_data);
-                var evidence_keys = Object.keys(evidence_json);
-
-                async.each(evidence_keys, (key,callback)=>{
-                    var evidence_details =  JSON.parse(evidence_json[key])
-                    // var key_dict = JSON.parse(key)
-                    // var key_values = Object.values(key_dict).join(':')
-                    if ((evidence_details['type_evidence'] == 'ThreatIntelligenceBlacklistIP')
-                        || (evidence_details['type_evidence'] == 'ThreatIntelligenceBlacklistDomain'))
-                    {
-                        evidence_data =
-                        evidence_data +
-                            '{bold}' +
-                            color.green('Detected '+evidence_details['type_detection'] + ' ' + evidence_details['detection_info']) +
-                            '{/bold}' +
-                            ". Blacklisted in " + evidence_details["description"] + '\n'
-                        // evidence_data = evidence_data + '{bold}' + key + '\n'
-                    }
-
-                  else{
-                    evidence_data = evidence_data +
-                        '{bold}'+
-                        color.green('Detected '+evidence_details['type_detection'] + ' ' +evidence_details['detection_info']) +
-                        '{/bold}' +
-                        ". " + evidence_details["description"] + '\n'
-                  }
-
-                  callback();
-                  }, (err)=>{
-                    if(err){console.log('Error to iterate through the evidences in the timewindow, check setEvidence() in kalipso_box.js. Error: ', err)}
-                    else{
-                    return this.setData(evidence_data)}
-                  }
-                );
-            });
-        }
-
-        catch (err){
-            console.log('Error in the setEvidence() in kalipso_box.js: ' ,err)
-        }
-    }
 }
 
 module.exports = Box;
