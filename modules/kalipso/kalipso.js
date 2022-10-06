01b155c4cfcef665fa136ff0f2a1c5c2ceac0382
Kamila Babayeva <kamifai14@gmail.com>
2022-10-06 20:38:09 +0200

    kalipso.js: remove unused classes

--- modules/kalipso/kalipso.js
+++ modules/kalipso/kalipso.js
@@ -5,8 +5,6 @@ const { redis } = require("./kalipso_widgets/libraries.js");
 var screen_class = require('./kalipso_widgets/kalipso_screen')
 var redis_database_class = require('./kalipso_widgets/kalipso_redis')
 var tree = require('./kalipso_widgets/kalipso_tree')
-var table = require('./kalipso_widgets/kalipso_table')
-var box = require('./kalipso_widgets/kalipso_box')
 var listtable = require('./kalipso_widgets/kalipso_listtable')
 var gauge = require('./kalipso_widgets/kalipso_gauge')
 var combine_listtable_gauge = require('./kalipso_widgets/kalipso_connect_listtable_gauge')
@@ -37,7 +35,7 @@ const redis_database = new redis_database_class(redis, redis_port)
 redis_database.createClient()
 
 // Initialize screen with all necessary widgets.
-const screen = new screen_class(redis_database, tree, table, box,listtable, gauge, combine_listtable_gauge, listbar,limit_letter_outtuple)
+const screen = new screen_class(redis_database, tree,listtable, gauge, combine_listtable_gauge, listbar,limit_letter_outtuple)
 screen.init()
 screen.render()
 
