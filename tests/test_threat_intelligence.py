351044cb71462dd1514cbca04663e9c5f21aec97
alya <alyaggomaa@gmail.com>
2023-01-18 22:15:15 +0200

    test_threat_intelligence.py: mock local ti files hashes

--- tests/test_threat_intelligence.py
+++ tests/test_threat_intelligence.py
@@ -3,6 +3,7 @@ from ..modules.threat_intelligence.threat_intelligence import Module
 from ..slips_files.common.slips_utils import utils
 import configparser
 import os
+import pytest
 
 
 def do_nothing(*args):
@@ -28,7 +29,30 @@ def test_parse_ti_file(database, outputQueue):
     assert database.search_IP_in_IoC('54.192.46.116')
 
 
-def test_check_local_ti_files_for_update(outputQueue):
+@pytest.mark.parametrize(
+    'current_hash, old_hash, expected_return',
+    [
+    ('111', '222', '111'),
+    ('111', '111', False),
+    (False , '222', False),
+    ],
+)
+def test_check_local_ti_files_for_update(
+        outputQueue, current_hash, old_hash, expected_return, mocker
+):
+    """
+    first case the cur hash is diff from the old hash so slips should update
+    second case the cur is the same so we shouldnt
+    third, cur hash is false meaning we cant get the file hash
+    """
+    # since this is a clear db, then we should update the local ti file
     threatintel = create_threatintel_instance(outputQueue)
-    dir_ = threatintel.path_to_local_ti_files
-    assert threatintel.check_local_ti_files_for_update(dir_) == True
+    own_malicious_iocs = os.path.join(threatintel.path_to_local_ti_files, 'own_malicious_iocs.csv')
+
+    mock_hash = mocker.patch("slips_files.common.slips_utils.Utils.get_hash_from_file")
+    mock_hash.return_value = current_hash
+
+    mock_hash = mocker.patch("slips_files.core.database.database.Database.get_TI_file_info")
+    mock_hash.return_value = {'hash': old_hash}
+
+    assert threatintel.should_update_local_ti_file(own_malicious_iocs) == expected_return
