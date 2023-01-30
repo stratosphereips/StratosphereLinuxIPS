bfbdaa8a935e6b5b811e9a6d3c93524bb8b51992
alya <alyaggomaa@gmail.com>
2023-01-30 10:38:09 +0200

    use "npm install" command instead of instead of hardcoding all package versions  in dockerfiles and install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Installing slips dependencies
-#!/bin/sh
-# Installing slips dependencies
-
 
 echo "[+] Installing zeek ...\n"
 sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python3 python3-dev swig zlib1g-dev
@@ -19,7 +15,6 @@ sudo apt install zeek
 
 
 # create a symlink to zeek so that slips can find it
-# create a symlink to zeek so that slips can find it
 echo "[+] Executing 'ln -s /opt/zeek/bin/zeek /usr/local/bin/bro'\n"
 sudo ln -s /opt/zeek/bin/zeek /usr/local/bin/bro
 echo "[+] Executing 'export PATH=$PATH:/usr/local/zeek/bin'\n"
@@ -28,48 +23,49 @@ echo "[+] Adding /usr/local/zeek/bin to ~/.bashrc\n"
 echo "export PATH=$PATH:/usr/local/zeek/bin" >> ~/.bashrc
 
 
- echo "[+] Installing Slips dependencies ...\n"
- sudo apt install -y --no-install-recommends \
-    python3 \
-    redis-server \
-    zeek \
-    python3-pip \
-    python3-certifi \
-    python3-dev \
-    build-essential \
-    file \
-    lsof \
-    net-tools \
-    iproute2 \
-    iptables \
-    python3-tzlocal \
-    nfdump \
-    tshark \
-    git \
-    whois \
-    golang \
-    nodejs \
-    notify-osd \
-    yara \
-    libnotify-bin
+echo "[+] Installing Slips dependencies ...\n"
+sudo apt install -y --no-install-recommends \
+  python3 \
+  redis-server \
+  zeek \
+  python3-pip \
+  python3-certifi \
+  python3-dev \
+  build-essential \
+  file \
+  lsof \
+  net-tools \
+  iproute2 \
+  iptables \
+  python3-tzlocal \
+  nfdump \
+  tshark \
+  git \
+  whois \
+  golang \
+  notify-osd \
+  yara \
+  libnotify-bin
 
 
- echo "[+] Executing 'python3 -m pip install --upgrade pip'\n"
- python3 -m pip install --upgrade pip
- echo "[+] Executing 'pip3 install -r requirements.txt'\n"
- pip3 install -r requirements.txt
- echo "[+] Executing pip3 install --ignore-installed six\n"
- pip3 install --ignore-installed six
+echo "[+] Executing 'python3 -m pip install --upgrade pip'\n"
+python3 -m pip install --upgrade pip
+echo "[+] Executing 'pip3 install -r requirements.txt'\n"
+pip3 install -r requirements.txt
+echo "[+] Executing pip3 install --ignore-installed six\n"
+pip3 install --ignore-installed six
 
- echo "[+] Executing 'sudo npm install blessed blessed-contrib redis async chalk strip-ansi@6.0.0 clipboardy fs sorted-array-async yargs '\n"
- sudo npm install blessed@0.1.81 blessed-contrib@4.10.0 redis@3.1.2 async@3.2.0 chalk@4.1.2 strip-ansi@6.0.0  clipboardy@2.3.0 fs@0.0.1-security sorted-array-async@0.0.7 yargs@17.0.1
+# For Kalipso
+echo "[+] Downloading nodejs v19 and npm dependencies"
+curl -fsSL https://deb.nodesource.com/setup_19.x | bash - && apt install -y --no-install-recommends nodejs
+cd /modules/kalipso && npm install
+cd ../..
+
+echo "[+] Installing p2p4slips\n"
+# build the pigeon and Add pigeon to path
+git submodule init && git submodule update && cd p2p4slips && go build && export PATH=$PATH:$(pwd) >> ~/.bashrc && cd ..
 
 
- echo "[+] Installing p2p4slips\n"
- # build the pigeon and Add pigeon to path
- git submodule init && git submodule update && cd p2p4slips && go build && export PATH=$PATH:$(pwd) >> ~/.bashrc && cd ..
-
-
- # running slips for the first time
- echo "[+] Executing 'redis-server --daemonize yes'\n"
- redis-server --daemonize yes
+# running slips for the first time
+echo "[+] Executing 'redis-server --daemonize yes'\n"
+redis-server --daemonize yes
