b1281e512dd96ea84f3ec296eb6dc91fb681e14c
Kamila Babayeva <kamifai14@gmail.com>
2023-02-08 10:16:02 +0100

    analysis.js: format the document

--- webinterface/analysis/static/js/analysis.js
+++ webinterface/analysis/static/js/analysis.js
@@ -8,17 +8,17 @@ let active_tw_id = "";
 let active_analysisTable = 'timeline';
 let last_analysisTable = 'timeline';
 
-function capitalizeFirstLetter(data){
+function capitalizeFirstLetter(data) {
     return data.charAt(0).toUpperCase() + data.slice(1);
 }
 
-function updateAnalysisTable(){
-    if(active_profile && active_timewindow){
+function updateAnalysisTable() {
+    if (active_profile && active_timewindow) {
         let link = "/analysis/" + active_analysisTable + "/" + active_profile + "/" + active_timewindow;
         $("#table_" + active_analysisTable).DataTable().ajax.url(link).load();
         removeListeners(last_analysisTable);
 
-        switch(active_analysisTable){
+        switch (active_analysisTable) {
             case "timeline": {
                 initTimelineListeners();
                 break;
@@ -40,26 +40,26 @@ function hideAnalysisTable(tableID) {
     document.getElementById(tableID).style.display = "none";
 }
 
-function updateIPInfo(row, field){
+function updateIPInfo(row, field) {
     let data = row.data();
     let url = '/analysis/info/' + data[field];
     $("#table_ipinfo").DataTable().ajax.url(url).load();
 }
 
 function addTableEvidence(table_id) {
-    let entry ='<table' + ' id="'+ table_id + '"' + 'class="table table-striped" cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'
+    let entry = '<table' + ' id="' + table_id + '"' + 'class="table table-striped" cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'
     let exit = '</table>'
-    let head ="<thead>"+
-     "<tr>"+
-     "<th>Evidence</th>" +
-     "<th>Confidence</th>" +
-     "<th>Threat Level</th>" +
-     "<th>Category</th>" +
-     "<th>Tag</th>" +
-     "<th>Description</th>" +
-     "</tr>"+
-     "</thead>"
-    return (entry + head  + exit);
+    let head = "<thead>" +
+        "<tr>" +
+        "<th>Evidence</th>" +
+        "<th>Confidence</th>" +
+        "<th>Threat Level</th>" +
+        "<th>Category</th>" +
+        "<th>Tag</th>" +
+        "<th>Description</th>" +
+        "</tr>" +
+        "</thead>"
+    return (entry + head + exit);
 }
 
 //function searchReload(filter_parameter){
@@ -69,22 +69,22 @@ function addTableEvidence(table_id) {
 //}
 
 function addTableTWs(tableID) {
-    let entry ='<table' + ' id="'+ tableID + '"' + ' class="table table-striped" >'
+    let entry = '<table' + ' id="' + tableID + '"' + ' class="table table-striped" >'
     let exit = '</table>'
-    let head ="<thead>"+
-     "<tr>" +
-     "<th>TW</th>" +
-     "</tr>" +
-     "</thead>"
-    return (entry + head  + exit);
+    let head = "<thead>" +
+        "<tr>" +
+        "<th>TW</th>" +
+        "</tr>" +
+        "</thead>"
+    return (entry + head + exit);
 };
 
 function hotkeyPress(e) {
-    let evtobj = window.event? event : e
-    if (evtobj.keyCode == 78 && evtobj.ctrlKey){
+    let evtobj = window.event ? event : e
+    if (evtobj.keyCode == 78 && evtobj.ctrlKey) {
         let table = $(active_tw_id).DataTable();
         $(table.row(active_timewindow_index).node()).removeClass('row_selected');
-        if(active_timewindow_index == table.data().count() - 1){
+        if (active_timewindow_index == table.data().count() - 1) {
             active_timewindow_index = -1
         }
         active_timewindow_index += 1
@@ -92,11 +92,11 @@ function hotkeyPress(e) {
         active_timewindow = table.row(active_timewindow_index).data()["tw"]
         updateAnalysisTable()
     }
-    if (evtobj.keyCode == 80 && evtobj.ctrlKey){
+    if (evtobj.keyCode == 80 && evtobj.ctrlKey) {
         let table = $(active_tw_id).DataTable();
         $(table.row(active_timewindow_index).node()).removeClass('row_selected');
         active_timewindow_index -= 1;
-        if(active_timewindow_index < 0){
+        if (active_timewindow_index < 0) {
             active_timewindow_index = table.data().count() - 1;
         }
         $(table.row(active_timewindow_index).node()).addClass('row_selected');
@@ -105,8 +105,8 @@ function hotkeyPress(e) {
     }
 }
 
-function convertDotToDash(string){
-    return string.replace(/\./g,'_');
+function convertDotToDash(string) {
+    return string.replace(/\./g, '_');
 }
 
 
@@ -117,15 +117,15 @@ function addTableAltFlows(data) {
     let middle = ""
     for (let [k, v] of Object.entries(data)) {
         middle += '<tr>'
-        middle += '<td>' + '<b>' + capitalizeFirstLetter(k) + ':' + '</b>'+'</td>' + '<td>' + v + '</td>'
+        middle += '<td>' + '<b>' + capitalizeFirstLetter(k) + ':' + '</b>' + '</td>' + '<td>' + v + '</td>'
         middle += '</tr>'
     }
-    return start +  middle + end
+    return start + middle + end
 }
 
 /* INITIALIZE LISTENERS FOR TABLES */
 /*--------------------------------------------------------------*/
-function initAnalysisTagListeners(){
+function initAnalysisTagListeners() {
     $("#buttons .btn").click(function () {
         $("#buttons .btn").removeClass('active');
         $(this).toggleClass('active');
@@ -138,27 +138,27 @@ function initAnalysisTagListeners(){
     });
 }
 
-function addTableTWsListener(table_tw_id,tr){
+function addTableTWsListener(table_tw_id, tr) {
     $("#" + table_tw_id).on('click', 'tbody tr', function () {
         let row = $("#" + table_tw_id).DataTable().row($(this))
         let rowData = row.data();
         let rowIndex = row.index();
         let t = $("#" + table_tw_id).DataTable();
-        if(active_tw_id){
-          $($(active_tw_id).DataTable().row(active_timewindow_index).node()).removeClass('row_selected');
+        if (active_tw_id) {
+            $($(active_tw_id).DataTable().row(active_timewindow_index).node()).removeClass('row_selected');
 
         }
         active_tw_id = "#" + table_tw_id
         active_timewindow_index = rowIndex;
         $(t.row(rowIndex).node()).addClass('row_selected');
-        active_profile =  $("#table_profiles").DataTable().row(tr).data()["profile"]
+        active_profile = $("#table_profiles").DataTable().row(tr).data()["profile"]
         active_timewindow = rowData["tw"]
         document.getElementById("active_profile_tw").innerText = "Selected: " + active_profile + " " + rowData["name"];
         updateAnalysisTable()
-     });
+    });
 }
 
-function initProfileTwListeners(){
+function initProfileTwListeners() {
     $('#table_profiles').on('click', 'tbody td.r', function () {
         let tr = $(this).closest('tr');
         let row = $("#table_profiles").DataTable().row(tr);
@@ -183,7 +183,7 @@ function initProfileTwListeners(){
     });
 }
 
-function initTimelineFlowsListeners(){
+function initTimelineFlowsListeners() {
     $('#table_timeline_flows').on('click', 'tbody td.saddr', function () {
         let row = $("#table_timeline_flows").DataTable().row($(this).parents('tr'));
         updateIPInfo(row, "saddr")
@@ -195,23 +195,23 @@ function initTimelineFlowsListeners(){
     })
 }
 
-function initTimelineListeners(){
+function initTimelineListeners() {
     $('#table_timeline').on('click', 'tbody td.daddr', function () {
         let row = $("#table_timeline").DataTable().row($(this).parents('tr'));
         updateIPInfo(row, "daddr")
     })
 
-//    $('#table_timeline_filter_button').click(function(){
-//        var filter_gender = $('#table_timeline_filter_input').val();
-//        searchReload(filter_gender);
-//    });
+    //    $('#table_timeline_filter_button').click(function(){
+    //        var filter_gender = $('#table_timeline_filter_input').val();
+    //        searchReload(filter_gender);
+    //    });
 
     $('#table_timeline').on('click', 'tbody tr', function () {
         let tr = $(this).closest('tr');
         let row = $("#table_timeline").DataTable().row(this)
         let data = row.data()["info"]
 
-        if(data){
+        if (data) {
             if (row.child.isShown()) {
                 row.child.hide();
                 tr.removeClass('shown');
@@ -223,7 +223,7 @@ function initTimelineListeners(){
     });
 }
 
-function initAlertListeners(){
+function initAlertListeners() {
     $('#table_alerts').on('click', 'tbody td.r', function () {
         var tr = $(this).closest('tr');
         var row = $("#table_alerts").DataTable().row(tr);
@@ -243,11 +243,11 @@ function initAlertListeners(){
 }
 /*--------------------------------------------------------------*/
 
-function removeListeners(analysisTag){
-     $("#table_"+analysisTag).off("click", "**")
+function removeListeners(analysisTag) {
+    $("#table_" + analysisTag).off("click", "**")
 }
 
-function initAllAnalysisTables(){
+function initAllAnalysisTables() {
 
     for (const [key, value] of Object.entries(analysisTableDefs)) {
         //remove the default 'Search' text for all DataTable search boxes
@@ -269,13 +269,13 @@ function initAllAnalysisTables(){
 
 }
 
-function initAnalysisPage(){
+function initAnalysisPage() {
     initAllAnalysisTables();  // Initialize all analysis tables
     initProfileTwListeners(); // Initialize all profile and tw tables' listeners
     initAnalysisTagListeners(); //Initialize analysisTags listeners
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     initAnalysisPage();
     document.onkeydown = hotkeyPress;
 });
