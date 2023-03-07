0c2d4546edc4ec79586fe0022393112221ba4ae9
loltyler1dotcom-discount-code-alpha <danielyangkang@gmail.com>
2023-03-06 22:52:03 -0800

    Added ProcessManager class to handle child processes and module loading

--- slips.py
+++ slips.py
@@ -25,6 +25,7 @@ from exclusiveprocess import Lock, CannotAcquireLock
 from redisman import RedisManager
 from uiman import UIManager
 from metadataman import MetadataManager
+from procman import ProcessManager
 from style import green
 
 import static
@@ -68,6 +69,7 @@ class Main:
         self.redis_man = RedisManager(terminate_slips=self.terminate_slips)
         self.ui_man = UIManager(self)
         self.metadata_man = MetadataManager(self)
+        self.proc_man = ProcessManager(self)
         self.conf = ConfigParser()
         self.args = self.conf.get_args()
         # in testing mode we manually set the following params
@@ -138,104 +140,6 @@ class Main:
             self.daemon.stop()
         sys.exit(0)
 
-
-    def get_modules(self, to_ignore):
-        """
-        Get modules from the 'modules' folder.
-        """
-        # This plugins import will automatically load the modules and put them in
-        # the __modules__ variable
-
-        plugins = {}
-        failed_to_load_modules = 0
-        # Walk recursively through all modules and packages found on the . folder.
-        # __path__ is the current path of this python program
-        for loader, module_name, ispkg in pkgutil.walk_packages(
-                modules.__path__, f'{modules.__name__}.'
-        ):
-            if any(module_name.__contains__(mod) for mod in to_ignore):
-                continue
-            # If current item is a package, skip.
-            if ispkg:
-                continue
-            # to avoid loading everything in the dir,
-            # only load modules that have the same name as the dir name
-            dir_name = module_name.split('.')[1]
-            file_name = module_name.split('.')[2]
-            if dir_name != file_name:
-                continue
-
-            # Try to import the module, otherwise skip.
-            try:
-                # "level specifies whether to use absolute or relative imports. The default is -1 which
-                # indicates both absolute and relative imports will be attempted. 0 means only perform
-                # absolute imports. Positive values for level indicate the number of parent
-                # directories to search relative to the directory of the module calling __import__()."
-                module = importlib.import_module(module_name)
-            except ImportError as e:
-                print(
-                    'Something wrong happened while importing the module {0}: {1}'.format(
-                        module_name, e
-                    )
-                )
-                failed_to_load_modules += 1
-                continue
-
-            # Walk through all members of currently imported modules.
-            for member_name, member_object in inspect.getmembers(module):
-                # Check if current member is a class.
-                if inspect.isclass(member_object) and (issubclass(
-                        member_object, Module
-                ) and member_object is not Module):
-                    plugins[member_object.name] = dict(
-                        obj=member_object,
-                        description=member_object.description,
-                    )
-
-        # Change the order of the blocking module(load it first)
-        # so it can receive msgs sent from other modules
-        if 'Blocking' in plugins:
-            plugins = OrderedDict(plugins)
-            # last=False to move to the beginning of the dict
-            plugins.move_to_end('Blocking', last=False)
-
-        return plugins, failed_to_load_modules
-
-    def load_modules(self):
-        to_ignore = self.conf.get_disabled_modules(self.input_type)
-        # Import all the modules
-        modules_to_call = self.get_modules(to_ignore)[0]
-        for module_name in modules_to_call:
-            if module_name in to_ignore:
-                continue
-
-            module_class = modules_to_call[module_name]['obj']
-            if 'P2P Trust' == module_name:
-                module = module_class(
-                    self.outputqueue,
-                    self.redis_port,
-                    output_dir=self.args.output
-                )
-            else:
-                module = module_class(
-                    self.outputqueue,
-                    self.redis_port
-                )
-            module.start()
-            __database__.store_process_PID(
-                module_name, int(module.pid)
-            )
-            description = modules_to_call[module_name]['description']
-            self.print(
-                f'\t\tStarting the module {green(module_name)} '
-                f'({description}) '
-                f'[PID {green(module.pid)}]', 1, 0
-                )
-        # give outputprocess time to print all the started modules
-        time.sleep(0.5)
-        print('-' * 27)
-        self.print(f"Disabled Modules: {to_ignore}", 1, 0)
-
     def setup_detailed_logs(self, LogsProcess):
         """
         Detailed logs are the ones created by logsProcess
@@ -282,48 +186,6 @@ class Main:
         except CannotAcquireLock:
             # another instance of slips is updating ports and orgs
             return
-
-    def kill(self, module_name, INT=False):
-        sig = signal.SIGINT if INT else signal.SIGKILL
-        try:
-            pid = int(self.PIDs[module_name])
-            os.kill(pid, sig)
-        except (KeyError, ProcessLookupError):
-            # process hasn't started yet
-            pass
-
-    def kill_all(self, PIDs):
-        for module in PIDs:
-            if module not in self.PIDs:
-                # modules the are last to kill aren't always started and there in self.PIDs
-                # ignore them
-                continue
-
-            self.kill(module)
-            self.print_stopped_module(module)
-
-    def stop_core_processes(self):
-        self.kill('Input')
-
-        if self.mode == 'daemonized':
-            # when using -D, we kill the processes because
-            # the queues are not there yet to send stop msgs
-            for process in (
-                        'ProfilerProcess',
-                        'logsProcess',
-                        'OutputProcess'
-
-            ):
-                self.kill(process, INT=True)
-
-        else:
-            # Send manual stops to the processes using queues
-            stop_msg = 'stop_process'
-            self.profilerProcessQueue.put(stop_msg)
-            self.outputqueue.put(stop_msg)
-            if hasattr(self, 'logsProcessQueue'):
-                self.logsProcessQueue.put(stop_msg)
-
 
     def save_the_db(self):
         # save the db to the output dir of this analysis
@@ -374,219 +236,6 @@ class Main:
         delete = self.conf.delete_zeek_files()
         if delete:
             shutil.rmtree(self.zeek_folder)
-
-    def print_stopped_module(self, module):
-        self.PIDs.pop(module, None)
-        # all text printed in green should be wrapped in the following
-
-        modules_left = len(list(self.PIDs.keys()))
-        # to vertically align them when printing
-        module += ' ' * (20 - len(module))
-        print(
-            f'\t{green(module)} \tStopped. '
-            f'{green(modules_left)} left.'
-        )
-
-    def get_already_stopped_modules(self):
-        already_stopped_modules = []
-        for module, pid in self.PIDs.items():
-            try:
-                # signal 0 is used to check if the pid exists
-                os.kill(int(pid), 0)
-            except ProcessLookupError:
-                # pid doesn't exist because module already stopped
-                # to be able to remove it's pid from the dict
-                already_stopped_modules.append(module)
-        return already_stopped_modules
-
-    def warn_about_pending_modules(self, finished_modules):
-        # exclude the module that are already stopped from the pending modules
-        pending_modules = [
-            module
-            for module in list(self.PIDs.keys())
-            if module not in finished_modules
-        ]
-        if not len(pending_modules):
-            return
-        print(
-            f'\n[Main] The following modules are busy working on your data.'
-            f'\n\n{pending_modules}\n\n'
-            'You can wait for them to finish, or you can '
-            'press CTRL-C again to force-kill.\n'
-        )
-        return True
-
-    def should_kill_all_modules(self, function_start_time, wait_for_modules_to_finish) -> bool:
-        """
-        checks if x minutes has passed since the start of the function
-        :param wait_for_modules_to_finish: time in mins to wait before force killing all modules
-                                            defined by wait_for_modules_to_finish in slips.conf
-        """
-        now = datetime.now()
-        diff = utils.get_time_diff(function_start_time, now, return_type='minutes')
-        return True if diff >= wait_for_modules_to_finish else False
-
-
-    def shutdown_gracefully(self):
-        """
-        Wait for all modules to confirm that they're done processing
-        or kill them after 15 mins
-        """
-        # 15 mins from this time, all modules should be killed
-        function_start_time = datetime.now()
-        try:
-            if not self.args.stopdaemon:
-                print('\n' + '-' * 27)
-            print('Stopping Slips')
-
-            wait_for_modules_to_finish  = self.conf.wait_for_modules_to_finish()
-            # close all tws
-            __database__.check_TW_to_close(close_all=True)
-
-            # set analysis end date
-            end_date = self.metadata_man.set_analysis_end_date()
-
-            start_time = __database__.get_slips_start_time()
-            analysis_time = utils.get_time_diff(start_time, end_date, return_type='minutes')
-            print(f'[Main] Analysis finished in {analysis_time:.2f} minutes')
-
-            # Stop the modules that are subscribed to channels
-            __database__.publish_stop()
-
-            finished_modules = []
-
-            # get dict of PIDs spawned by slips
-            self.PIDs = __database__.get_PIDs()
-
-            # we don't want to kill this process
-            self.PIDs.pop('slips.py', None)
-
-            if self.mode == 'daemonized':
-                profilesLen = __database__.getProfilesLen()
-                self.daemon.print(f'Total analyzed IPs: {profilesLen}.')
-
-
-            modules_to_be_killed_last = {
-                'EvidenceProcess',
-                'Blocking',
-                'Exporting Alerts',
-            }
-
-            self.stop_core_processes()
-            # only print that modules are still running once
-            warning_printed = False
-
-            # timeout variable so we don't loop forever
-            # give slips enough time to close all modules - make sure
-            # all modules aren't considered 'busy' when slips stops
-            max_loops = 430
-
-            # loop until all loaded modules are finished
-            # in the case of -S, slips doesn't even start the modules,
-            # so they don't publish in finished_modules. we don't need to wait for them we have to kill them
-            if not self.args.stopdaemon:
-                #  modules_to_be_killed_last are ignored when they publish a msg in finished modules channel,
-                # we will kill them aletr, so we shouldn't be looping and waiting for them to get outta the loop
-                slips_processes = len(list(self.PIDs.keys())) - len(modules_to_be_killed_last)
-
-                try:
-                    while (
-                        len(finished_modules) < slips_processes  and max_loops != 0
-                    ):
-                        # print(f"Modules not finished yet {set(loaded_modules) - set(finished_modules)}")
-                        try:
-                            message = self.c1.get_message(timeout=0.00000001)
-                        except NameError:
-                            continue
-
-                        if message and message['data'] in ('stop_process', 'stop_slips'):
-                            continue
-
-                        if utils.is_msg_intended_for(message, 'finished_modules'):
-                            # all modules must reply with their names in this channel after
-                            # receiving the stop_process msg
-                            # to confirm that all processing is done and we can safely exit now
-                            module_name = message['data']
-
-                            if module_name in modules_to_be_killed_last:
-                                # we should kill these modules the very last, or else we'll miss evidence generated
-                                # right before slips stops
-                                continue
-
-
-                            if module_name not in finished_modules:
-                                finished_modules.append(module_name)
-                                self.kill(module_name)
-                                self.print_stopped_module(module_name)
-
-                                # some modules publish in finished_modules channel before slips.py starts listening,
-                                # but they finished gracefully.
-                                # remove already stopped modules from PIDs dict
-                                for module in self.get_already_stopped_modules():
-                                    finished_modules.append(module)
-                                    self.print_stopped_module(module)
-
-                        max_loops -= 1
-                        # after reaching the max_loops and before killing the modules that aren't finished,
-                        # make sure we're not processing
-                        # the logical flow is self.pids should be empty by now as all modules
-                        # are closed, the only ones left are the ones we want to kill last
-                        if len(self.PIDs) > len(modules_to_be_killed_last) and max_loops < 2:
-                            if not warning_printed and self.warn_about_pending_modules(finished_modules):
-                                if 'Update Manager' not in finished_modules:
-                                    print(
-                                        f"[Main] Update Manager may take several minutes "
-                                        f"to finish updating 45+ TI files."
-                                    )
-                                warning_printed = True
-
-                            # -t flag is only used in integration tests,
-                            # so we don't care about the modules finishing their job when testing
-                            # instead, kill them
-                            if self.args.testing:
-                                break
-
-                            # delay killing unstopped modules until all of them
-                            # are done processing
-                            max_loops += 1
-
-                            # checks if 15 minutes has passed since the start of the function
-                            if self.should_kill_all_modules(function_start_time, wait_for_modules_to_finish):
-                                print(f"Killing modules that took more than "
-                                      f"{wait_for_modules_to_finish} mins to finish.")
-                                break
-
-                except KeyboardInterrupt:
-                    # either the user wants to kill the remaining modules (pressed ctrl +c again)
-                    # or slips was stuck looping for too long that the os sent an automatic sigint to kill slips
-                    # pass to kill the remaining modules
-                    pass
-
-            # modules that aren't subscribed to any channel will always be killed and not stopped
-            # comes here if the user pressed ctrl+c again
-            self.kill_all(self.PIDs.copy())
-            self.kill_all(modules_to_be_killed_last)
-
-            # save redis database if '-s' is specified
-            if self.args.save:
-                self.save_the_db()
-
-            # if store_a_copy_of_zeek_files is set to yes in slips.conf,
-            # copy the whole zeek_files dir to the output dir
-            self.store_zeek_dir_copy()
-
-            # if delete_zeek_files is set to yes in slips.conf,
-            # delete zeek_files/ dir
-            self.delete_zeek_files()
-
-            if self.mode == 'daemonized':
-                # if slips finished normally without stopping the daemon with -S
-                # then we need to delete the pidfile
-                self.daemon.delete_pidfile()
-
-            os._exit(-1)
-        except KeyboardInterrupt:
-            return False
     
     def is_debugger_active(self) -> bool:
         """Return if the debugger is currently active"""
@@ -1130,7 +779,7 @@ class Main:
             if not self.args.db:
                 # update local files before starting modules
                 self.update_local_TI_files()
-                self.load_modules()
+                self.proc_man.load_modules()
 
             # self.start_gui_process()
             if self.args.webinterface:
@@ -1138,7 +787,7 @@ class Main:
 
             # call shutdown_gracefully on sigterm
             def sig_handler(sig, frame):
-                self.shutdown_gracefully()
+                self.proc_man.shutdown_gracefully()
             # The signals SIGKILL and SIGSTOP cannot be caught, blocked, or ignored.
             signal.signal(signal.SIGTERM, sig_handler)
 
@@ -1243,7 +892,7 @@ class Main:
                     and utils.is_msg_intended_for(message, 'finished_modules')
                     and message['data'] == 'stop_slips'
                 ):
-                    self.shutdown_gracefully()
+                    self.proc_man.shutdown_gracefully()
 
                 # Sleep some time to do routine checks
                 time.sleep(sleep_time)
@@ -1286,7 +935,7 @@ class Main:
                 if modified_ips_in_the_last_tw == 0:
                     # waited enough. stop slips
                     if intervals_to_wait == 0:
-                        self.shutdown_gracefully()
+                        self.proc_man.shutdown_gracefully()
 
                     # If there were no modified TWs in the last timewindow time,
                     # then start counting down
@@ -1297,7 +946,7 @@ class Main:
         except KeyboardInterrupt:
             # the EINTR error code happens if a signal occurred while the system call was in progress
             # comes here if zeek terminates while slips is still working
-            self.shutdown_gracefully()
+            self.proc_man.shutdown_gracefully()
 
 
 ####################
