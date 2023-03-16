a66496a850191c1f16515c471c4ac81ee8f16fda
alya <alyaggomaa@gmail.com>
2023-03-16 19:57:54 +0200

    config_parser: run exporting module when --CYST is given

--- slips_files/common/config_parser.py
+++ slips_files/common/config_parser.py
@@ -722,6 +722,8 @@ class ConfigParser(object):
         if (
                 'stix' not in export_to
                 and 'slack' not in export_to
+                and '--CYST' not in sys.argv
+                and '-C' not in sys.argv
         ):
             to_ignore.append('exporting_alerts')
 
