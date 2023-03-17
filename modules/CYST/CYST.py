fe3f5e8357deff988387786f25de34d3370a24e9
alya <alyaggomaa@gmail.com>
2023-03-17 13:42:58 +0200

    CYST: fix problem setting a timeout  when getting cyst flows

--- modules/CYST/CYST.py
+++ modules/CYST/CYST.py
@@ -78,7 +78,7 @@ class Module(Module, multiprocessing.Process):
         returns a dict if the flow was received or False if there was an error
         """
         try:
-            self.sock.settimeout(5)
+            self.cyst_conn.settimeout(5)
             #todo handle multiple flows received at once i.e. split by \n
             flow: str = self.cyst_conn.recv(10000).decode()
         except ConnectionResetError:
