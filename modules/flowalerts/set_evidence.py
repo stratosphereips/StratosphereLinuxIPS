fde31dd1313c05d2d001483ef0a1ee453bd603c4
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2023-03-31 22:45:23 +0530

    updated to not consider 0.0.0.0 as private_ip

--- modules/flowalerts/set_evidence.py
+++ modules/flowalerts/set_evidence.py
@@ -344,6 +344,11 @@ class Helper:
     def set_evidence_conn_to_private_ip(
             self, daddr, dport, saddr, profileid, twid, uid, timestamp
     ):
+        
+        if daddr != '0.0.0.0' and daddr != '255.255.255.255':
+            return
+        if saddr != '0.0.0.0' and saddr != '255.255.255.255':
+            return
 
         confidence = 1
         threat_level = 'info'
