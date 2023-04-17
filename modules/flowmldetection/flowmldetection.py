86ad9371f882efecb2d52242d2266f2e9c0cdcac
haleelsada <haleelsada2.O@gmail.com>
2023-04-18 01:21:30 +0530

    avoided model.bin and scaler.bin from changing in test mode

--- modules/flowmldetection/flowmldetection.py
+++ modules/flowmldetection/flowmldetection.py
@@ -378,7 +378,8 @@ class Module(Module, multiprocessing.Process):
 
     def shutdown_gracefully(self):
         # Confirm that the module is done processing
-        self.store_model()
+        if self.mode == 'train':
+            self.store_model()
         __database__.publish('finished_modules', self.name)
 
     def run(self):
