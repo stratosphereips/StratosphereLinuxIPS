78192e4e1cab93f86cb960195fbe7799b74526d9
alya <alyaggomaa@gmail.com>
2023-05-11 00:03:10 +0300

    abstract Module: add a function that determines whether the module should stop or not based on the stop_msg and the msgs left in the modules channels

--- slips_files/common/abstracts.py
+++ slips_files/common/abstracts.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from slips_files.core.database.database import __database__
 # This is the abstract Module class to check against. Do not modify
 class Module(ABC):
     name = ''
@@ -7,6 +8,24 @@ class Module(ABC):
 
     def __init__(self, outputqueue):
         self.outputqueue = outputqueue
+        self.control_flow_channel = __database__.subscribe('control_flow')
+        self.msg_received = True
+
+    def should_stop(self) -> bool:
+        """
+        The module should stop on the following 2 conditions
+        1. slips.py publishes the stop_process msg in the control_flow channel
+        2. no msgs left to process in the module
+        This function calls the shutdown_gracefully pf the module when the 2 conditions are true
+        """
+        if self.msg_received:
+            # this module is still receiving msgs, don't stop
+            return False
+
+        message = __database__.get_message(self.control_flow_channel)
+        if message and message['data'] == 'stop_process':
+            self.shutdown_gracefully()
+            return True
 
     def print(self, text, verbose=1, debug=0):
         """
