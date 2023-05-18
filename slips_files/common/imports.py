a3785e6c811a28756d8b0db9c82ad793164f1df5
alya <alyaggomaa@gmail.com>
2023-05-18 13:33:31 +0300

    move the common imports in all modules to  slips_files/common/imports.py

---
+++ slips_files/common/imports.py
@@ -0,0 +1,6 @@
+# common imports for all modules
+from slips_files.common.abstracts import Module
+from slips_files.core.database.redis_database import Redis
+from slips_files.common.config_parser import ConfigParser
+from slips_files.common.slips_utils import utils
+import multiprocessing
