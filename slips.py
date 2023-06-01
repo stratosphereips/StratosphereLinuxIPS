f0d1dbfbf1c1334f050287267a31f67e28eadfe4
loltyler1dotcom-discount-code-alpha <danielyangkang@gmail.com>
2023-06-01 14:42:20 -0700

    removed old import

--- slips.py
+++ slips.py
@@ -21,7 +21,6 @@ import contextlib
 from slips_files.common.slips_utils import utils
 from slips_files.core.database.database import __database__
 from slips_files.common.config_parser import ConfigParser
-from slips_files.common.cpu_profiler import CPUProfiler
 from exclusiveprocess import Lock, CannotAcquireLock
 from redis_manager import RedisManager
 from metadata_manager import MetadataManager
