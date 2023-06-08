c422a19f90bd608b356a9cb2fe1c30a27ca642a6
alya <alyaggomaa@gmail.com>
2023-06-08 17:04:37 +0300

    update ip info unit tests

--- tests/test_ip_info.py
+++ tests/test_ip_info.py
@@ -11,7 +11,7 @@ def test_get_asn_info_from_geolite(mock_db):
     """
     geolite is an offline db
     """
-    ASN_info = ModuleFactory().create_asn_obj(db=mock_db)
+    ASN_info = ModuleFactory().create_asn_obj(mock_db)
     # check an ip that we know is in the db
     expected_asn_info = {'asn': {'number': 'AS7018', 'org': 'ATT-INTERNET4'}}
     assert ASN_info.get_asn_info_from_geolite('108.200.116.255') == expected_asn_info
@@ -20,12 +20,12 @@ def test_get_asn_info_from_geolite(mock_db):
 
 def test_cache_ip_range(mock_db):
     # Patch the database object creation before it is instantiated
-    ASN_info = ModuleFactory().create_asn_obj(db=mock_db)
+    ASN_info = ModuleFactory().create_asn_obj(mock_db)
     assert ASN_info.cache_ip_range('8.8.8.8') == {'asn': {'number': 'AS15169', 'org': 'GOOGLE, US'}}
 
 # GEOIP unit tests
 def test_get_geocountry(mock_db):
-    ip_info = ModuleFactory().create_ip_info_obj(db=mock_db)
+    ip_info = ModuleFactory().create_ip_info_obj(mock_db)
 
     #open the db we'll be using for this test
     # ip_info.wait_for_dbs()
@@ -42,7 +42,7 @@ def test_get_geocountry(mock_db):
 
 def test_get_vendor(mocker, mock_db):
     # make sure the mac db is download so that wai_for_dbs doesn't wait forever :'D
-    ip_info = ModuleFactory().create_ip_info_obj(db=mock_db)
+    ip_info = ModuleFactory().create_ip_info_obj(mock_db)
     profileid = 'profile_10.0.2.15'
     mac_addr = '08:00:27:7f:09:e1'
     host_name = 'FooBar-PC'
@@ -52,8 +52,10 @@ def test_get_vendor(mocker, mock_db):
     mock_requests = mocker.patch("requests.get")
     mock_requests.return_value.status_code = 200
     mock_requests.return_value.text = 'PCS Systemtechnik GmbH'
-    with patch.object(mock_db, 'get_mac_vendor_from_profile', return_value=False):
-        # tries to get vendor either online or from our offline db
-        mac_info = ip_info.get_vendor(mac_addr, host_name, profileid)
+    mock_db.get_mac_vendor_from_profile.return_value = False
+
+    # tries to get vendor either online or from our offline db
+    mac_info = ip_info.get_vendor(mac_addr, host_name, profileid)
+
     assert mac_info is not False
     assert mac_info['Vendor'].lower() == 'Pcs Systemtechnik GmbH'.lower()
