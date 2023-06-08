87074b7586a90e4c7e863e3114e5e676203f7b99
alya <alyaggomaa@gmail.com>
2023-06-08 17:04:14 +0300

    update leak detector unit tests

--- tests/test_leak_detector.py
+++ tests/test_leak_detector.py
@@ -3,8 +3,8 @@ from tests.module_factory import ModuleFactory
 import os
 
 
-def test_compile_and_save_rules(output_queue):
-    leak_detector = ModuleFactory().create_leak_detector_obj()
+def test_compile_and_save_rules(mock_db):
+    leak_detector = ModuleFactory().create_leak_detector_obj(mock_db)
     leak_detector.compile_and_save_rules()
     compiled_rules = os.listdir(leak_detector.compiled_yara_rules_path)
     assert 'test_rule.yara_compiled' in compiled_rules
