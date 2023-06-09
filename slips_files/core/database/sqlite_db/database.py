ced2c62a20d75e18cee6c80e4148a8759b169fd2
alya <alyaggomaa@gmail.com>
2023-06-09 16:12:39 +0300

    sqlite: acquire a lock before each call to commit()

--- slips_files/core/database/sqlite_db/database.py
+++ slips_files/core/database/sqlite_db/database.py
@@ -8,6 +8,8 @@ from threading import Lock
 class SQLiteDB():
     """Stores all the flows slips reads and handles labeling them"""
     _obj = None
+    # used to lock each call to commit()
+    cursor_lock = Lock()
 
     def __new__(cls, output_dir):
         # To treat the db as a singelton
@@ -18,7 +20,6 @@ class SQLiteDB():
             cls.conn = sqlite3.connect(cls._flows_db, check_same_thread=False)
             cls.cursor = cls.conn.cursor()
             cls.init_tables()
-            cls.cursor_lock = Lock()
         return cls._obj
 
 
@@ -134,7 +135,7 @@ class SQLiteDB():
                 self.cursor.execute(
                     query
                 )
-                self.conn.commit()
+                self.commit()
             except sqlite3.Error as e:
                 # An error occurred during execution
                 print(f"Error executing query ({query}): {e}")
@@ -210,12 +211,14 @@ class SQLiteDB():
             ):
 
         parameters = (profileid, twid, flow.uid, json.dumps(asdict(flow)), label)
+
         self.cursor.execute(
             'INSERT OR REPLACE INTO flows (profileid, twid, uid, flow, label) '
             'VALUES (?, ?, ?, ?, ?);',
             parameters,
         )
-        self.conn.commit()
+        self.commit()
+
 
     def add_altflow(
             self, flow, profileid: str, twid:str, label='benign'
@@ -226,38 +229,46 @@ class SQLiteDB():
             'VALUES (?, ?, ?, ?, ?);',
             parameters,
         )
-        self.conn.commit()
+        self.commit()
 
 
     def insert(self, table_name, values):
         query = f"INSERT INTO {table_name} VALUES ({values})"
         self.cursor.execute(query)
-        self.conn.commit()
+        self.commit()
 
     def update(self, table_name, set_clause, condition):
         query = f"UPDATE {table_name} SET {set_clause} WHERE {condition}"
         self.cursor.execute(query)
-        self.conn.commit()
+        self.commit()
 
     def delete(self, table_name, condition):
         query = f"DELETE FROM {table_name} WHERE {condition}"
         self.cursor.execute(query)
-        self.conn.commit()
+        self.commit()
 
     def select(self, table_name, columns="*", condition=None):
         query = f"SELECT {columns} FROM {table_name}"
         if condition:
             query += f" WHERE {condition}"
-        self.cursor_lock.acquire(True)
         self.cursor.execute(query)
         result = self.cursor.fetchall()
-        self.cursor_lock.release()
         return result
 
     def execute_query(self, query):
         self.cursor.execute(query)
-        self.conn.commit()
+        self.commit()
 
     def close(self):
         self.cursor.close()
         self.conn.close()
+
+    def commit(self):
+        """
+        since sqlite is terrible with multi-process applications
+        this will serve as a wrapper for the commit()
+        to be able to use a Lock()
+        """
+        self.cursor_lock.acquire(True)
+        self.conn.commit()
+        self.cursor_lock.release()
