a8e1f350b15181cf60b36e64a7f2aa593e95eb14
alya <alyaggomaa@gmail.com>
2023-06-15 17:13:31 +0300

    redis: add new_module_flow to the list of supported channels

--- slips_files/core/database/redis_db/database.py
+++ slips_files/core/database/redis_db/database.py
@@ -70,6 +70,7 @@ class RedisDB(IoCHandler, AlertHandler, ProfileHandler):
         'new_tunnel',
         'check_jarm_hash',
         'control_module',
+        'new_module_flow'
         }
     # The name is used to print in the outputprocess
     name = 'DB'
