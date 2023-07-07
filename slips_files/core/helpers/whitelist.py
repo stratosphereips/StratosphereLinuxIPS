a061ca11c6e8b2fb3f9a82908a425580cd0cb23b
alya <alyaggomaa@gmail.com>
2023-07-07 18:04:41 +0300

    whitelist: check the answers of each dns flow for whitelisted ips

--- slips_files/core/helpers/whitelist.py
+++ slips_files/core/helpers/whitelist.py
@@ -185,6 +185,7 @@ class Whitelist:
                 return True
 
 
+
         if whitelisted_IPs := self.db.get_whitelist('IPs'):
             # self.print('Check the IPs')
             # Check if the IPs are whitelisted
@@ -209,6 +210,18 @@ class Whitelist:
                 ):
                     # self.print(f"Whitelisting the dst IP {column_values['daddr']}")
                     return True
+
+            if flow_type == 'dns':
+                # check all answers
+                for answer in flow.answers:
+                    if answer in ips_to_whitelist:
+                        # #TODO the direction doesn't matter here right?
+                        # direction = whitelisted_IPs[daddr]['from']
+                        what_to_ignore = whitelisted_IPs[answer]['what_to_ignore']
+                        if self.should_ignore_flows(what_to_ignore):
+                            # self.print(f"Whitelisting the IP {answer} due to its presence in a dns answer")
+                            return True
+
 
         if whitelisted_macs := self.db.get_whitelist('mac'):
             # try to get the mac address of the current flow
