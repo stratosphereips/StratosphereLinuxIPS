8e423cd119fd15b8e87845078964f7c5a675cb6d
loltyler1dotcom-discount-code-alpha <danielyangkang@gmail.com>
2023-07-27 12:02:38 -0700

    memory profiler done

--- slips_files/common/_memory_profiler_example_no_import.py
+++ slips_files/common/_memory_profiler_example_no_import.py
@@ -3,121 +3,15 @@ import glob
 import os
 import subprocess
 from termcolor import colored
-from slips_files.common.abstracts import ProfilerInterface
 import time
 import multiprocessing
 from multiprocessing.managers import SyncManager
 from multiprocessing.synchronize import Lock, Event
 from multiprocessing.sharedctypes import SynchronizedBase
 import threading
-from typing import Dict
+from typing import Dict, List
 import psutil
 import random
-
-class MemoryProfiler(ProfilerInterface):
-    profiler = None
-    def __init__(self, output, db=None, mode="dev", multiprocess=True):
-        valid_modes = ["dev", "live"]
-        if mode not in valid_modes:
-            print("memory_profiler_mode = " + mode + " is invalid, must be one of " +
-                            str(valid_modes) + ", Memory Profiling will be disabled")
-        if mode == "dev":
-            self.profiler = DevProfiler(output, multiprocess)
-        elif mode == "live":
-            self.profiler = LiveProfiler(multiprocess, db=db)
-
-    def _create_profiler(self):
-        self.profiler._create_profiler()
-
-    def start(self):
-        print(colored("Memory Profiler Started", 'green'))
-        self.profiler.start()
-
-    def stop(self):
-        self.profiler.stop()
-        print(colored("Memory Profiler Ended", 'green'))
-
-    def print(self):
-        pass
-
-class DevProfiler(ProfilerInterface):
-    output = None
-    profiler = None
-    multiprocess = None
-    def __init__(self, output, multiprocess):
-        self.output = output
-        self.multiprocess = multiprocess
-        self.profiler = self._create_profiler()
-
-    def _create_profiler(self):
-        return memray.Tracker(file_name=self.output, follow_fork=self.multiprocess)
-
-    def start(self):
-        self.profiler.__enter__()
-
-    def stop(self):
-        self.profiler.__exit__(None, None, None)
-        print(colored("Converting memory profile bin files to html...", 'green'))
-        output_files = glob.glob(self.output + '*')
-        directory = os.path.dirname(self.output)
-        flamegraph_dir = directory + '/flamegraph/'
-        if not os.path.exists(flamegraph_dir):
-            os.makedirs(flamegraph_dir)
-        table_dir = directory + '/table/'
-        if not os.path.exists(table_dir):
-            os.makedirs(table_dir)
-        for file in output_files:
-            filename = os.path.basename(file)
-            flame_output = flamegraph_dir + filename + '.html'
-            subprocess.run(['memray', 'flamegraph', '--temporal', '--leaks', '--split-threads', '--output', flame_output, file])
-            table_output = table_dir + filename + '.html'
-            subprocess.run(['memray', 'table', '--output', table_output, file])
-
-    def print(self):
-        pass
-
-class LiveProfiler(ProfilerInterface):
-    multiprocess = None
-    profiler = None
-    def __init__(self, multiprocess=False, db=None):
-        self.multiprocess = multiprocess
-        if multiprocess:
-            self.profiler=LiveMultiprocessProfiler(db=db)
-        else:
-            self.profiler=LiveSingleProcessProfiler()
-    def _create_profiler(self):
-        self.profiler._create_profiler()
-
-    def start(self):
-        self.profiler.start()
-
-    def stop(self):
-        self.profiler.stop()
-
-    def print(self):
-        self.profiler.print()
-
-class LiveSingleProcessProfiler(ProfilerInterface):
-    profiler = None
-    port = 5000
-    def __init__(self):
-        self.profiler = self._create_profiler()
-    def _create_profiler(self):
-        print("Memory profiling running on port " + str(self.port))
-        print("Connect to continue")
-        with open(os.devnull, 'w') as devnull:
-            subprocess.Popen(["memray", "live", str(self.port)], stdout=devnull)
-        dest = memray.SocketDestination(server_port=self.port, address='127.0.0.1')
-        return memray.Tracker(destination=dest)
-
-    def start(self):
-        self.profiler.__enter__()
-
-    def stop(self):
-        self.profiler.__exit__(None, None, None)
-
-    def print(self):
-        pass
 
 def proc_is_running(pid):
     try:
@@ -127,12 +21,71 @@ def proc_is_running(pid):
         return True
     except psutil.NoSuchProcess:
         return False
-
-class LiveMultiprocessProfiler(ProfilerInterface):
+    
+class LiveMultiprocessProfiler:
     original_process_class: multiprocessing.Process
     signal_handler_thread: threading.Thread
     tracker_possessor: int
     db = None
+    # def __init__(self, db=None):
+    #     self.original_process_class = multiprocessing.Process
+    #     global mp_manager
+    #     mp_manager = multiprocessing.Manager()
+    #     global tracker_lock_global
+    #     tracker_lock_global = mp_manager.Lock()
+        
+    #     global proc_map_global
+    #     proc_map_global = {}
+    #     global proc_map_lock_global
+    #     proc_map_lock_global = mp_manager.Lock()
+    #     self.db = db
+    #     self.pid_channel = self.db.subscribe('memory_profile')
+    #     self.db.publish('memory_profile', 5)
+    #     self.db.publish('memory_profile', "Hello")
+
+    # def _create_profiler(self):
+    #     pass
+
+    # def _handle_signal(self):
+    #     global proc_map_global
+    #     global tracker_lock_holder_pid
+    #     while True:
+    #         # check redis channel
+    #         # poll for signal
+    #         timeout = 0.01
+    #         msg = self.pid_channel.get_message(timeout=timeout)
+    #         pid_to_profile: int = None
+    #         while msg:
+    #             pid: int = None
+    #             try:
+    #                 pid = int(msg['data'])
+    #             except ValueError:
+    #                 msg = self.pid_channel.get_message(timeout=timeout)
+    #                 continue
+    #             if pid in proc_map_global.keys():
+    #                 print(colored(f"Handle signal {pid}"))
+    #                 if proc_is_running(pid):
+    #                     pid_to_profile = pid
+    #                 else:
+    #                     try:
+    #                         proc_map_global.pop(pid)
+    #                     except KeyError:
+    #                         pass
+    #             msg = self.pid_channel.get_message(timeout=timeout)
+            
+    #         if pid_to_profile:
+    #             time.sleep(5)
+    #             print(colored(f"Sending end signal {tracker_lock_holder_pid}", "red"))
+    #             if tracker_lock_holder_pid in proc_map_global.keys():
+    #                 proc_map_global[tracker_lock_holder_pid].set_end_signal()
+    #             print(colored(f"Sending start signal {pid_to_profile}", "red"))
+    #             print(proc_map_global[pid_to_profile])
+
+    #             proc_map_global[pid_to_profile].set_start_signal()
+    #             #send stop first, send start new process
+            
+    #         time.sleep(1)
+    
     def __init__(self, db=None):
         self.original_process_class = multiprocessing.Process
         global mp_manager
@@ -146,7 +99,7 @@ class LiveMultiprocessProfiler(ProfilerInterface):
         proc_map_global = {} # port to process object mapping
         proc_map_lock_global = mp_manager.Lock() # hold when modifying proc_map_global
         self.db = db
-        self.pid_channel = self.db.subscribe("memory_profile")
+        self.pid_channel = self.db
 
     def _create_profiler(self):
         pass
@@ -158,15 +111,15 @@ class LiveMultiprocessProfiler(ProfilerInterface):
             # check redis channel
             # poll for signal
             timeout = 0.01
-            msg: str = self.pid_channel.get_message(timeout=timeout)
+            msg: str = None
             pid_to_profile: int = None
-            while msg:
-                # print(f"Msg {msg}")
+            while not self.pid_channel.empty():
+                msg = self.pid_channel.get()
+                print(f"Msg {msg}")
                 pid: int = None
                 try:
-                    pid = int(msg['data'])
-                except ValueError:
-                    msg = self.pid_channel.get_message(timeout=timeout)
+                    pid = int(msg)
+                except TypeError:
                     continue
                 if pid in proc_map_global.keys():
                     if proc_is_running(pid):
@@ -176,8 +129,6 @@ class LiveMultiprocessProfiler(ProfilerInterface):
                             proc_map_global.pop(pid)
                         except KeyError:
                             pass
-                msg = self.pid_channel.get_message(timeout=timeout)
-
             if pid_to_profile:
                 print(colored(f"Sending end signal {tracker_lock_holder_pid.value}", "red"))
                 if tracker_lock_holder_pid.value in proc_map_global.keys():
@@ -324,3 +275,52 @@ tracker_lock_global: Lock = None # process holds when running profiling
 tracker_lock_holder_pid: SynchronizedBase = None # process that holds the lock
 proc_map_global: Dict[int, MultiprocessPatch] = None # port to process object mapping
 proc_map_lock_global: Lock = None # hold when modifying proc_map_global
+
+def target_function():
+    print("Target function started")
+    time.sleep(5)
+
+def mem_function():
+    print("Mem function started")
+    while True:
+        time.sleep(1)
+        array = []
+        for i in range(1000000):
+            array.append(i)
+
+if __name__ == "__main__":
+    # mp_manager = multiprocessing.Manager()
+    # tracker_lock_global = mp_manager.Lock()
+    # proc_map_global = {}
+    # proc_map_lock_global = mp_manager.Lock()
+
+    db = multiprocessing.Queue()
+    profiler = LiveMultiprocessProfiler(db=db)
+    profiler.start()
+
+    processes: List[MultiprocessPatch] = []
+    num_processes = 3
+    
+    for _ in range(num_processes):
+        process = multiprocessing.Process(target=target_function if _%2 else mem_function)
+        process.start()
+        processes.append(process)
+    
+    # Message passing
+    db.put(processes[1].pid) # successful
+    time.sleep(5) # target_function will timeout and tracker will be cleared
+    db.put(processes[0].pid) # end but maybe don't start
+    time.sleep(5) # mem_function will get tracker started
+    db.put(processes[0].pid) # start successfully
+
+    # Direct process access
+    # processes[0].set_start_signal()
+    # time.sleep(5)
+    # processes[0].set_end_signal()
+    # time.sleep(2)
+    # processes[1].set_start_signal()
+    # time.sleep(10)
+    # processes[0].set_end_signal()
+
+    for process in processes:
+        process.join()
