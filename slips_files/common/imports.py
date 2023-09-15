2a1b840ffcefe94c73b806b89380787608cb1421
alya <alyaggomaa@gmail.com>
2023-09-16 00:17:21 +0300

    move parsers to slips_files/common/parsers

--- slips_files/common/imports.py
+++ slips_files/common/imports.py
@@ -1,7 +1,1 @@
 # common imports for all modules
-from slips_files.common.abstracts._module import IModule
-from slips_files.core.database.redis_db.database import RedisDB
-from slips_files.core.database.database_manager import DBManager
-from slips_files.common.config_parser import ConfigParser
-from slips_files.common.slips_utils import utils
-import multiprocessing
