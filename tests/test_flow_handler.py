33b7e6ac2e6bc6310569a2bb8229e9a102151fce
alya <alyaggomaa@gmail.com>
2023-11-03 17:06:25 +0200

    add a sample flow fixture

--- tests/test_flow_handler.py
+++ tests/test_flow_handler.py
@@ -1,26 +1,8 @@
 from tests.module_factory import ModuleFactory
-from slips_files.core.flows.zeek import Conn
 import pytest
 
 
-def get_flow():
-    """returns a dummy flow for testing"""
-    return Conn(
-        '1601998398.945854',
-        '1234',
-        '192.168.1.1',
-        '8.8.8.8',
-        5,
-        'TCP',
-        'dhcp',
-        80,88,
-        20,20,
-        20,20,
-        '','',
-        'Established',''
-    )
-def test_is_supported_flow_not_ts(mock_rdb):
-    flow = get_flow()
+def test_is_supported_flow_not_ts(flow, mock_rdb):
     flow.starttime = None
     flow_handler = ModuleFactory().create_flow_handler_obj(flow, mock_rdb)
     assert flow_handler.is_supported_flow() == False
@@ -35,8 +17,7 @@ def test_is_supported_flow_not_ts(mock_rdb):
      ]
 )
 def test_is_supported_flow_without_ts(
-        flow_type: str, expected_val: bool, mock_rdb):
-    flow = get_flow()
+        flow_type: str, expected_val: bool, flow, mock_rdb):
     # just change the flow_type
     flow.type_ = flow_type
     flow_handler = ModuleFactory().create_flow_handler_obj(flow, mock_rdb)
