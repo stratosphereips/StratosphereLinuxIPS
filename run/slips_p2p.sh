2854b273e87a225e008e5cee6bedd8d0e29c7940
Eden Berger <Eden2036@gmail.com>
2023-11-22 13:56:53 +0200

    Fixing typo in slips_p2p.sh echo command

--- run/slips_p2p.sh
+++ run/slips_p2p.sh
@@ -22,6 +22,6 @@ if [[ " ${interface_list[*]} " == *" ${1} "* ]]; then
   # first arg is an interface
   docker run -it -d --rm --name slips --net=host -p 55000:55000 -v $(pwd)/output:/StratosphereLinuxIPS/output -v $(pwd)/config:/StratosphereLinuxIPS/config stratosphereips/slips_p2p ./slips.py -e 1 -i ${1}
 else
-  exho "invalid interface."
+  echo "invalid interface."
 fi
 
