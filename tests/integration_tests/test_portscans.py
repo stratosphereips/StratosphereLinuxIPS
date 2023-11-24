5a42e8e8b33ee7d3385edec3655ed013e3079074
alya <alyaggomaa@gmail.com>
2023-11-24 17:07:00 +0200

    add an inetgration test for vertical portscans

--- tests/integration_tests/test_portscans.py
+++ tests/integration_tests/test_portscans.py
@@ -41,19 +41,71 @@ def test_horizontal(path, output_dir, redis_port):
 
     database = ModuleFactory().create_db_manager_obj(redis_port, output_dir=output_dir)
 
-    not_estab_ports: dict = database.get_data_from_profile_tw('profile_10.0.2.112', 'timewindow1', 'Dst',
-                                                          'Not Established', 'TCP',
-                                          'Client', 'Ports')
-    port_80_dstips: dict = len(not_estab_ports['80']['dstips'])
-    profiles: int = database.get_profiles_len()
     assert has_errors(output_dir) is False
+
     # make sure slips generated profiles for this file (can't
     # put the number of profiles exactly because slips
     # doesn't generate a const number of profiles per file)
+    profiles: int = database.get_profiles_len()
     assert profiles > 0
 
     log_file = os.path.join(output_dir, alerts_file)
     assert is_evidence_present(log_file, expected_evidence) == True
 
+    not_estab_ports: dict = database.get_data_from_profile_tw('profile_10.0.2.112', 'timewindow1', 'Dst',
+                                                          'Not Established', 'TCP',
+                                          'Client', 'Ports')
+    port_80_dstips: int = len(not_estab_ports['80']['dstips'])
     assert port_80_dstips == 8
+    shutil.rmtree(output_dir)
+
+@pytest.mark.parametrize(
+    'path, output_dir, redis_port',
+    [
+        (
+            'dataset/port-scans/vertical/conn.log',
+            'testing_vertical_ps/',
+            6660
+        )
+    ],
+)
+def test_vertical(path, output_dir, redis_port):
+    """
+    checks that slips is detecting horizontal ps no issue,
+    """
+    output_dir = create_output_dir(output_dir)
+
+    expected_evidence = 'vertical port scan to IP 45.33.32.156 from 192.168.1.9.'
+
+    output_file = os.path.join(output_dir, 'slips_output.txt')
+    command = f'./slips.py -e 1 -t -f {path}  -o {output_dir} -P {redis_port} > {output_file} 2>&1'
+    # this function returns when slips is done
+    run_slips(command)
+
+    database = ModuleFactory().create_db_manager_obj(redis_port, output_dir=output_dir)
+
+    assert has_errors(output_dir) is False
+
+    # make sure slips generated profiles for this file (can't
+    # put the number of profiles exactly because slips
+    # doesn't generate a const number of profiles per file)
+    profiles: int = database.get_profiles_len()
+    assert profiles > 0
+
+    log_file = os.path.join(output_dir, alerts_file)
+    assert is_evidence_present(log_file, expected_evidence) == True
+
+    not_estab_dstport: dict = database.get_data_from_profile_tw(
+        'profile_192.168.1.9',
+        'timewindow1',
+        'Dst',
+        'Not Established',
+        'TCP',
+        'Client',
+        'IPs'
+        )
+
+    not_estab_dstport: int = len(not_estab_dstport)
+
+    assert not_estab_dstport == 42
     shutil.rmtree(output_dir)
