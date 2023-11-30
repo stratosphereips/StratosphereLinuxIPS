6c43bcb7695147bca8a55b619b7b594be353bee7
alya <alyaggomaa@gmail.com>
2023-11-30 16:07:02 +0200

    fix unable to run portscan integration tests

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -28,8 +28,10 @@ printf "0" | ./slips.py -k
 # command before running the dataset tests
 # distribute on 3 workers only because every worker will be spawning 10+ processes
 
+python3 -m pytest -s tests/integration_tests/test_portscans.py  -p no:warnings -vv
 python3 -m pytest -s tests/integration_tests/test_dataset.py -p no:warnings -vv
 python3 -m pytest -s tests/integration_tests/test_config_files.py  -p no:warnings -vv
-python3 -m pytest -s tests/integration_tests/test_portscans.py.py  -p no:warnings -vv
+
 printf "0" | ./slips.py -k
+
 ./slips.py -cc
