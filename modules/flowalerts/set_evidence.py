815fff17f21a821f8e180d1575ae8a282c9aab45
alya <alyaggomaa@gmail.com>
2024-03-06 15:42:39 +0200

    flowalerts: fix setting ssh successful evidence

--- modules/flowalerts/set_evidence.py
+++ modules/flowalerts/set_evidence.py
@@ -696,7 +696,7 @@ class SetEvidnceHelper:
             self,
             proto: str,
             daddr: str,
-            dport: str,
+            dport: int,
             saddr: str,
             twid: str,
             uid: str,
@@ -837,7 +837,7 @@ class SetEvidnceHelper:
             twid: str,
             saddr: str,
             daddr: str,
-            size,
+            size: int,
             uid: str,
             timestamp: str,
             by='',
@@ -847,6 +847,7 @@ class SetEvidnceHelper:
         This is not strictly a detection, but we don't have
         a better way to show it.
         The threat_level is 0.01 to show that this is not a detection
+        :param size: src and dst bytes sent and recieved
         """
 
         confidence: float = 0.8
@@ -856,7 +857,7 @@ class SetEvidnceHelper:
         ip_identification: str = self.db.get_ip_identification(daddr)
         description: str = (
             f'SSH successful to IP {daddr}. {ip_identification}. '
-            f'From IP {saddr}. Size: {str(size)}. Detection model {by}.'
+            f'From IP {saddr}. Sent bytes: {str(size)}. Detection model {by}.'
             f' Confidence {confidence}'
         )
 
@@ -1387,7 +1388,7 @@ class SetEvidnceHelper:
                 value=saddr
             ),
             threat_level=ThreatLevel.INFO,
-            confidence=confidence,
+            confidence=0.6,
             description=description,
             profile=ProfileID(ip=saddr),
             timewindow=TimeWindow(number=twid_number),
