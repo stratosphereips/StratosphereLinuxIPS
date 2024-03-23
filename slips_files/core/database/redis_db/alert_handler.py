6750c5d3b904a3162449d884136ba81c7fe7d3c8
Sekhar-Kumar-Dash <119131588+Sekhar-Kumar-Dash@users.noreply.github.com>
2024-03-23 20:10:37 +0530

    fixed small typo in alert_handler.py

--- slips_files/core/database/redis_db/alert_handler.py
+++ slips_files/core/database/redis_db/alert_handler.py
@@ -31,8 +31,9 @@ class AlertHandler:
         """
         victim = "" if not victim else victim
         self.r.hincrby(
-            f"{attacker}_evidence_sumamry", f"{victim}_{evidence_type}", 1
-        )
+            f'{attacker}_evidence_summary',
+            f"{victim}_{evidence_type}",
+            1)
 
     def mark_profile_as_malicious(self, profileid: str):
         """keeps track of profiles that generated an alert"""
