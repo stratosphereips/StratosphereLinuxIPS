ba3059dc070e6fec729745f98d14d2287e070723
alya <alyaggomaa@gmail.com>
2024-04-29 20:34:27 +0300

    exporting_alerts.py: remove_sensitive_info before exporting evidence

--- modules/exporting_alerts/exporting_alerts.py
+++ modules/exporting_alerts/exporting_alerts.py
@@ -1,8 +1,9 @@
+import json
+
 from modules.exporting_alerts.slack_exporter import SlackExporter
 from modules.exporting_alerts.stix_exporter import StixExporter
 from slips_files.common.slips_utils import utils
 from slips_files.common.abstracts.module import IModule
-import json
 
 
 class ExportingAlerts(IModule):
@@ -29,20 +30,38 @@ class ExportingAlerts(IModule):
     def pre_main(self):
         utils.drop_root_privs()
 
-        if self.slack.should_export():
+        export_to_slack = self.slack.should_export()
+        export_to_stix = self.stix.should_export()
+
+        if export_to_slack:
             self.slack.send_init_msg()
 
-        if self.stix.should_export():
+        if export_to_stix:
             # This thread is responsible for waiting n seconds before
             # each push to the stix server
             # it starts the timer when the first alert happens
             self.stix.start_exporting_thread()
 
+        if not export_to_slack or export_to_stix:
+            return 1
+
+    def remove_sensitive_info(self, evidence: dict) -> str:
+        """
+        removes the leaked location co-ords from the evidence
+        description before exporting
+        returns the description without sensitive info
+        """
+        if "NETWORK_GPS_LOCATION_LEAKED" not in evidence["evidence_type"]:
+            return evidence["description"]
+
+        description = evidence["description"]
+        return description[: description.index("Leaked location")]
+
     def main(self):
+        # a msg is sent here for each evidence that was part of an alert
         if msg := self.get_msg("export_evidence"):
             evidence = json.loads(msg["data"])
-            description: str = evidence["description"]
-
+            description = self.remove_sensitive_info(evidence)
             if self.slack.should_export():
                 srcip = evidence["profile"]["ip"]
                 msg_to_send = f"Src IP {srcip} Detected {description}"
