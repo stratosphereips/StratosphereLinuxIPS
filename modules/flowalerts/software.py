09a4caaa909eaa89c7ece37d89fead29e8e41e56
alya <alyaggomaa@gmail.com>
2024-05-27 17:25:05 +0300

    flowalerts: move all new software logic to flowalerts/software.py

---
+++ modules/flowalerts/software.py
@@ -0,0 +1,74 @@
+import json
+
+from modules.flowalerts.set_evidence import SetEvidnceHelper
+from slips_files.common.abstracts.flowalerts_analyzer import (
+    IFlowalertsAnalyzer,
+)
+
+
+class Software(IFlowalertsAnalyzer):
+    def init(self, flowalerts=None):
+        self.flowalerts = flowalerts
+        self.set_evidence = SetEvidnceHelper(self.db)
+
+    def name(self) -> str:
+        return "software_analyzer"
+
+    def check_multiple_ssh_versions(
+        self, flow: dict, twid, role="SSH::CLIENT"
+    ):
+        """
+        checks if this srcip was detected using a different
+         ssh client or server versions before
+        :param role: can be 'SSH::CLIENT' or 'SSH::SERVER'
+        as seen in zeek software.log flows
+        """
+        if role not in flow["software"]:
+            return
+
+        profileid = f'profile_{flow["saddr"]}'
+        # what software was used before for this profile?
+        # returns a dict with
+        # software:
+        #   { 'version-major': ,'version-minor': ,'uid': }
+        cached_used_sw: dict = self.db.get_software_from_profile(profileid)
+        if not cached_used_sw:
+            # we have no previous software info about this saddr in out db
+            return False
+
+        # these are the versions that this profile once used
+        cached_ssh_versions = cached_used_sw[flow["software"]]
+        cached_versions = (
+            f"{cached_ssh_versions['version-major']}_"
+            f"{cached_ssh_versions['version-minor']}"
+        )
+
+        current_versions = f"{flow['version_major']}_{flow['version_minor']}"
+        if cached_versions == current_versions:
+            # they're using the same ssh client version
+            return False
+
+        # get the uid of the cached versions, and the uid
+        # of the current used versions
+        uids = [cached_ssh_versions["uid"], flow["uid"]]
+        self.set_evidence.multiple_ssh_versions(
+            flow["saddr"],
+            cached_versions,
+            current_versions,
+            flow["starttime"],
+            twid,
+            uids,
+            role=role,
+        )
+        return True
+
+    def analyze(self):
+        msg = self.flowalerts.get_msg("new_software")
+        if not msg:
+            return
+
+        msg = json.loads(msg["data"])
+        flow: dict = msg["sw_flow"]
+        twid = msg["twid"]
+        self.check_multiple_ssh_versions(flow, twid, role="SSH::CLIENT")
+        self.check_multiple_ssh_versions(flow, twid, role="SSH::SERVER")
