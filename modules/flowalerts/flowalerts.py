c309f0a0c196b51762b2b8f8b906c3b02a3df225
alya <alyaggomaa@gmail.com>
2024-05-27 17:14:37 +0300

    flowalerts: move all smtp logic to flowalerts/smtp.py

--- modules/flowalerts/flowalerts.py
+++ modules/flowalerts/flowalerts.py
@@ -14,6 +14,7 @@ from slips_files.common.slips_utils import utils
 from slips_files.common.abstracts.module import IModule
 from .dns import DNS
 from .notice import Notice
+from .smtp import SMTP
 from .timer_thread import TimerThread
 from .set_evidence import SetEvidnceHelper
 from slips_files.core.helpers.whitelist import Whitelist
@@ -48,12 +49,6 @@ class FlowAlerts(IModule):
         # Usually the computer resolved DNS already, so we need to wait a little to report
         # In mins
         self.conn_without_dns_interface_wait_time = 30
-        # when the ctr reaches the threshold in 10 seconds,
-        # we detect an smtp bruteforce
-        self.smtp_bruteforce_threshold = 3
-        # dict to keep track of bad smtp logins to check for bruteforce later
-        # format {profileid: [ts,ts,...]}
-        self.smtp_bruteforce_cache = {}
         # If 1 flow uploaded this amount of MBs or more, slips will alert data upload
         self.flow_upload_threshold = 100
         # after this number of failed ssh logins, we alert pw guessing
@@ -69,6 +64,7 @@ class FlowAlerts(IModule):
         )
         self.dns = DNS(self.db, flowalerts=self)
         self.notice = Notice(self.db, flowalerts=self)
+        self.smtp = SMTP(self.db, flowalerts=self)
 
     def subscribe_to_channels(self):
         self.c1 = self.db.subscribe("new_flow")
@@ -909,55 +905,6 @@ class FlowAlerts(IModule):
 
         self.db.setReconnections(profileid, twid, current_reconnections)
 
-    def check_smtp_bruteforce(self, profileid, twid, flow):
-        uid = flow["uid"]
-        daddr = flow["daddr"]
-        saddr = flow["saddr"]
-        stime = flow.get("starttime", False)
-        last_reply = flow.get("last_reply", False)
-
-        if "bad smtp-auth user" not in last_reply:
-            return False
-
-        try:
-            timestamps, uids = self.smtp_bruteforce_cache[profileid]
-            timestamps.append(stime)
-            uids.append(uid)
-            self.smtp_bruteforce_cache[profileid] = (timestamps, uids)
-        except KeyError:
-            # first time for this profileid to make bad smtp login
-            self.smtp_bruteforce_cache.update({profileid: ([stime], [uid])})
-
-        self.set_evidence.bad_smtp_login(saddr, daddr, stime, twid, uid)
-
-        timestamps = self.smtp_bruteforce_cache[profileid][0]
-        uids = self.smtp_bruteforce_cache[profileid][1]
-
-        # check if 3 bad login attemps happened within 10 seconds or less
-        if len(timestamps) != self.smtp_bruteforce_threshold:
-            return
-
-        # check if they happened within 10 seconds or less
-        diff = utils.get_time_diff(timestamps[0], timestamps[-1])
-
-        if diff > 10:
-            # didnt happen within 10s!
-            # remove the first login from cache so we
-            # can check the next 3 logins
-            self.smtp_bruteforce_cache[profileid][0].pop(0)
-            self.smtp_bruteforce_cache[profileid][1].pop(0)
-            return
-
-        self.set_evidence.smtp_bruteforce(
-            flow,
-            twid,
-            uids,
-            self.smtp_bruteforce_threshold,
-        )
-
-        # remove all 3 logins that caused this alert
-        self.smtp_bruteforce_cache[profileid] = ([], [])
-
     def detect_connection_to_multiple_ports(
         self,
         saddr,
@@ -1540,20 +1487,15 @@ class FlowAlerts(IModule):
 
         # if msg := self.get_msg("new_dns"):
         #     ...
+
         self.dns.analyze()
+        self.smtp.analyze()
 
         # if msg := self.get_msg("new_downloaded_file"):
         #     ssl_info = json.loads(msg["data"])
         #     self.check_malicious_ssl(ssl_info)
         #
-        # # --- Detect Bad SMTP logins ---
-        # if msg := self.get_msg("new_smtp"):
-        #     smtp_info = json.loads(msg["data"])
-        #     profileid = smtp_info["profileid"]
-        #     twid = smtp_info["twid"]
-        #     flow: dict = smtp_info["flow"]
-        #
-        #     self.check_smtp_bruteforce(profileid, twid, flow)
+
         # # --- Detect multiple used SSH versions ---
         # if msg := self.get_msg("new_software"):
         #     msg = json.loads(msg["data"])
