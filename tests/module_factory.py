8f688d2624c3a0eb92293d32f3c4f3d7db94a2c3
Sekhar-Kumar-Dash <119131588+Sekhar-Kumar-Dash@users.noreply.github.com>
2024-07-27 16:09:48 +0530

    Update module_factory.py

--- tests/module_factory.py
+++ tests/module_factory.py
@@ -39,6 +39,7 @@ from modules.network_discovery.horizontal_portscan import HorizontalPortscan
 from modules.network_discovery.network_discovery import NetworkDiscovery
 from modules.network_discovery.vertical_portscan import VerticalPortscan
 from modules.arp.arp import ARP
+from slips.daemon import Daemon
 from slips_files.core.evidence_structure.evidence import (
     Attacker,
     Direction,
@@ -417,4 +418,19 @@ class ModuleFactory:
             network_discovery = NetworkDiscovery(mock_db)
             network_discovery.db = mock_db 
         return network_discovery
+
+    def create_daemon_object(self):
+        with patch("slips.daemon.Daemon.__init__", return_value=None):
+            daemon = Daemon(None)
+            daemon.stderr = "errors.log"
+            daemon.stdout = "slips.log"
+            daemon.stdin = "/dev/null"
+            daemon.logsfile = "slips.log"
+            daemon.pidfile_dir = "/tmp"
+            daemon.pidfile = os.path.join(daemon.pidfile_dir, "slips_daemon.lock")
+            daemon.slips = MagicMock()
+            daemon.daemon_start_lock = "slips_daemon_start"
+            daemon.daemon_stop_lock = "slips_daemon_stop"
+            daemon.pid = None
+            return daemon
 
