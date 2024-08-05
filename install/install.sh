deaf5b6abfd3ab8598fd1817d46755d2e950019b
alya <alyaggomaa@gmail.com>
2024-08-05 22:00:18 +0300

    install.sh: exit on any command failure and disable prompts

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
 
 
-sudo apt-get update
+print_green() {
+  # Prints text in green color
+    echo "[+] \e[32m$1\e[0m\n"
+}
 
-echo "[+] Installing Slips dependencies ...\n"
-sudo apt-get install  -y --no-install-recommends \
+exit_on_cmd_failure() {
+  if [ $? -ne 0 ]; then
+      echo "Problem installing Slips. Aborting."
+      exit 1
+  fi
+}
+
+# to disable prompts
+export DEBIAN_FRONTEND=noninteractive
+
+print_green "Setting up local timezone"
+
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
+
+exit_on_cmd_failure
+
+print_green "Running apt update"
+apt-get update
+
+exit_on_cmd_failure
+
+print_green "Installing Slips dependencies ..."
+ apt-get install  -y --no-install-recommends \
     cmake \
     make \
     gcc\
@@ -43,43 +68,48 @@ sudo apt-get install  -y --no-install-recommends \
     notify-osd \
     libnotify-bin \
     net-tools \
-    lsb_release
+    lsb-release
 
+
+exit_on_cmd_failure
+
+
+print_green "Installing Zeek"
 UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}' | sed 's/\./_/')
 ZEEK_REPO_URL="http://download.opensuse.org/repositories/security:/zeek/xUbuntu_${UBUNTU_VERSION}/"
 
 # Add the repository to the sources list
-echo "deb ${ZEEK_REPO_URL} /" | sudo tee /etc/apt/sources.list.d/security:zeek.list
+echo "deb ${ZEEK_REPO_URL} /" |  tee /etc/apt/sources.list.d/security:zeek.list \
+&& curl -fsSL "${ZEEK_REPO_URL}/Release.key" | gpg --dearmor |  tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null \
+&& apt install -y  --no-install-recommends zeek
 
-# Add the zeek repository key
-curl -fsSL "${ZEEK_REPO_URL}/Release.key" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
-
-# install redis
-curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-
-sudo apt-get update
-
-sudo apt install -y --no-install-recommends \
-  python3 \
-  redis \
-  zeek
 
 # create a symlink to zeek so that slips can find it
-sudo ln -s /opt/zeek/bin/zeek /usr/local/bin/bro
+ ln -s /opt/zeek/bin/zeek /usr/local/bin/bro
 export PATH=$PATH:/usr/local/zeek/bin
 echo "export PATH=$PATH:/usr/local/zeek/bin" >> ~/.bashrc
 
+exit_on_cmd_failure
 
-echo "[+] Executing 'python3 -m pip install --upgrade pip'"
-python3 -m pip install --upgrade pip
-echo "[+] Executing 'pip3 install -r install/requirements.txt'"
-pip3 install -r install/requirements.txt
-echo "[+] Executing pip3 install --ignore-installed six"
-pip3 install --ignore-installed six
+
+print_green "Installing Redis"
+curl -fsSL https://packages.redis.io/gpg |  gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" > /etc/apt/sources.list.d/redis.list
+apt-get update
+apt install -y --no-install-recommends  redis
+
+exit_on_cmd_failure
+
+print_green "Installing Python requirements"
+python3 -m pip install --upgrade pip \
+&& pip3 install -r install/requirements.txt \
+&& pip3 install --ignore-installed six
+
+exit_on_cmd_failure
+
 
 # For Kalipso
-echo "[+] Downloading nodejs v19 and npm dependencies"
+print_green "Installing nodejs and npm dependencies"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash - \
     && export NVM_DIR="$HOME/.nvm" \
     && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" \
@@ -87,12 +117,16 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash 
     && cd modules/kalipso && npm install \
     && cd ../..
 
+exit_on_cmd_failure
 
-echo "[+] Installing p2p4slips\n"
+print_green "Installing p2p4slips"
 # build the pigeon and Add pigeon to path
 git submodule init && git submodule update && cd p2p4slips && go build && export PATH=$PATH:$(pwd) >> ~/.bashrc && cd ..
 
+exit_on_cmd_failure
 
 # running slips for the first time
-echo "[+] Executing 'redis-server --daemonize yes'\n"
+print_green "Executing 'redis-server --daemonize yes'"
 redis-server --daemonize yes
+
+exit_on_cmd_failure
