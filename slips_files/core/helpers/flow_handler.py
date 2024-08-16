bfd5e54e80ce3fe17d8150f64a06989529afec28
alya <alyaggomaa@gmail.com>
2024-08-16 18:45:44 +0300

    idmefv2.py: Add an idmef handler to convert slip evidence to imefv2

--- slips_files/core/helpers/flow_handler.py
+++ slips_files/core/helpers/flow_handler.py
@@ -249,7 +249,8 @@ class FlowHandler:
 
     def handle_files(self):
         """
-        Send files.log data to new_downloaded_file channel in the TI module to see if it's malicious
+        Send files.log data to new_downloaded_file channel in the TI module to
+         see if it's malicious
         """
 
         # files slips sees can be of 2 types: suricata or zeek
