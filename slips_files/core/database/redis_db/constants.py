626f01d87c7c170c87c11ab51b00b10cd197ba3d
alya <alyaggomaa@gmail.com>
2024-08-19 15:14:17 +0300

    move all redis hash names from ioc_handler to constants.py

---
+++ slips_files/core/database/redis_db/constants.py
@@ -0,0 +1,20 @@
+class Constants:
+    LOADED_TI_FILES = "loaded TI files"
+    TI_FILES_INFO = "TI_files_info"
+    GIVE_TI = "give_threat_intelligence"
+    IOC_IPS = "IoC_ips"
+    IOC_DOMAINS = "IoC_domains"
+    IOC_IP_RANGES = "IoC_ip_ranges"
+    IOC_ASN = "IoC_ASNs"
+    IOC_JA3 = "IoC_JA3"
+    IOC_JARM = "IoC_JARM"
+    IOC_SSL = "IoC_SSL"
+    MALICIOUS_IPS = "MaliciousIPs"
+    MALICIOUS_DOMAINS = "MaliciousDomains"
+    LABELED_AS_MALICIOUS = "labeled_as_malicious"
+    URLS_INFO = "URLsInfo"
+    DOMAINS_INFO = "DomainsInfo"
+
+
+class Channels:
+    DNS_INFO_CHANGE = "dns_info_change"
