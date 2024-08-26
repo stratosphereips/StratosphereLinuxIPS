b4d205cc074461c2ffc8e7b4ab9fc50e365302f4
Sekhar-Kumar-Dash <119131588+Sekhar-Kumar-Dash@users.noreply.github.com>
2024-08-26 15:06:32 +0530

    Updated module_factory.py

--- tests/module_factory.py
+++ tests/module_factory.py
@@ -5,6 +5,7 @@ from unittest.mock import (
     MagicMock,
     mock_open,
 )
+import sys
 import os
 from multiprocessing import Queue
 
@@ -19,6 +20,7 @@ from slips_files.core.database.redis_db.constants import (
     Channels,
 )
 from slips_files.core.evidencehandler import EvidenceHandler
+from modules.rnn_cc_detection.rnn_cc_detection import CCDetection
 from slips_files.core.helpers.notify import Notify
 from modules.flowalerts.dns import DNS
 from modules.flowalerts.downloaded_file import DownloadedFile
@@ -690,3 +692,20 @@ class ModuleFactory:
             setattr(metadata_manager.main, attr, value)
 
         return metadata_manager
+
+    def create_rnn_detection_object(self):
+        logger = Mock()
+        output_dir = "/tmp"
+        redis_port = 6379
+        termination_event = Mock()
+        
+        with patch.object(CCDetection, '__init__', return_value=None):
+            cc_detection = CCDetection(logger, output_dir, redis_port, termination_event)
+            cc_detection.db = Mock()
+            cc_detection.exporter = Mock()
+            return cc_detection
+  
+
+
+
+
