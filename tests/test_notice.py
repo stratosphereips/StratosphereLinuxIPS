2d1b2cc5143235b167cc5d793899b00687bfaf82
alya <alyaggomaa@gmail.com>
2024-09-18 20:03:03 +0300

    update notice.py unit tests

--- tests/test_notice.py
+++ tests/test_notice.py
@@ -1,7 +1,9 @@
 """Unit test for modules/flowalerts/notice.py"""
 
+from dataclasses import asdict
 from unittest.mock import Mock
 
+from slips_files.core.flows.zeek import Notice
 from tests.module_factory import ModuleFactory
 import json
 import pytest
@@ -26,6 +28,7 @@ import pytest
                 "stime": 1234567890,
                 "msg": "Other message",
                 "note": "Other_Note",
+                "scanning_ip": "192.168.1.1",
             },
             0,
         ),
@@ -34,7 +37,20 @@ import pytest
 def test_check_vertical_portscan(flow, expected_call_count):
     notice = ModuleFactory().create_notice_analyzer_obj()
     notice.set_evidence.vertical_portscan = Mock()
-    notice.check_vertical_portscan(flow, "test_uid", "test_twid")
+    flow = Notice(
+        starttime=flow["stime"],
+        saddr="192.168.1.60",
+        daddr="",
+        sport="",
+        dport="",
+        note=flow["note"],
+        msg="",
+        scanned_port="",
+        dst="",
+        scanning_ip=flow["scanning_ip"],
+        uid="1364",
+    )
+    notice.check_vertical_portscan("timewindow1", flow)
 
     assert (
         notice.set_evidence.vertical_portscan.call_count == expected_call_count
@@ -69,20 +85,24 @@ def test_check_horizontal_portscan(mocker, flow, expected_call_count):
     mock_horizontal_portscan = mocker.patch.object(
         notice.set_evidence, "horizontal_portscan"
     )
-
-    notice.check_horizontal_portscan(
-        flow, "test_uid", "test_profileid", "test_twid"
+    flow = Notice(
+        starttime=flow["stime"],
+        saddr="192.168.1.60",
+        daddr="",
+        sport="",
+        dport="",
+        note=flow["note"],
+        msg=flow["msg"],
+        scanned_port="",
+        dst="",
+        scanning_ip="",
+        uid="1364",
     )
+    notice.check_horizontal_portscan(flow, "test_profileid", "test_twid")
 
     assert mock_horizontal_portscan.call_count == expected_call_count
     expected_calls = [
-        mocker.call(
-            flow["msg"],
-            flow["stime"],
-            "test_profileid",
-            "test_twid",
-            "test_uid",
-        )
+        mocker.call("test_profileid", "test_twid", flow)
     ] * expected_call_count
     mock_horizontal_portscan.assert_has_calls(expected_calls)
 
@@ -113,15 +133,23 @@ def test_check_horizontal_portscan(mocker, flow, expected_call_count):
 def test_check_password_guessing(mocker, flow, expected_call_count):
     notice = ModuleFactory().create_notice_analyzer_obj()
     mock_pw_guessing = mocker.patch.object(notice.set_evidence, "pw_guessing")
-
-    notice.check_password_guessing(flow, "test_uid", "test_twid")
+    flow = Notice(
+        starttime=flow["stime"],
+        saddr="192.168.1.60",
+        daddr="",
+        sport="",
+        dport="",
+        note=flow["note"],
+        msg=flow["msg"],
+        scanned_port="",
+        dst="",
+        scanning_ip="",
+        uid="1364",
+    )
+    notice.check_password_guessing("test_twid", flow)
 
     assert mock_pw_guessing.call_count == expected_call_count
-    expected_calls = [
-        mocker.call(
-            flow["msg"], flow["stime"], "test_twid", "test_uid", by="Zeek"
-        )
-    ] * expected_call_count
+    expected_calls = [mocker.call("test_twid", flow)] * expected_call_count
     mock_pw_guessing.assert_has_calls(expected_calls)
 
 
@@ -135,15 +163,21 @@ def test_check_password_guessing(mocker, flow, expected_call_count):
                     {
                         "profileid": "test_profile",
                         "twid": "test_twid",
-                        "flow": json.dumps(
-                            {
-                                "stime": 1234567890,
-                                "msg": "Test message",
-                                "note": "Port_Scan",
-                                "scanning_ip": "192.168.1.1",
-                            }
+                        "flow": asdict(
+                            Notice(
+                                starttime="1234567890",
+                                saddr="192.168.1.1",
+                                daddr="",
+                                sport="",
+                                dport="",
+                                note="Port_Scan",
+                                msg="Test message",
+                                scanned_port="",
+                                dst="",
+                                scanning_ip="192.168.1.1",
+                                uid="1234",
+                            )
                         ),
-                        "uid": "test_uid",
                     }
                 )
             },
