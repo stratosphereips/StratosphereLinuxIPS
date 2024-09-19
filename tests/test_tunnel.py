fbbc6aa19b44ab0bb40dfb5e4cd5240f2560dcf2
alya <alyaggomaa@gmail.com>
2024-09-20 01:38:43 +0300

    update tunnel.py unit tests

--- tests/test_tunnel.py
+++ tests/test_tunnel.py
@@ -1,43 +1,64 @@
 """Unit test for modules/flowalerts/tunnel.py"""
 
+from dataclasses import asdict
 from unittest.mock import Mock
 
+from slips_files.core.flows.zeek import Tunnel
 from tests.module_factory import ModuleFactory
 import json
 import pytest
 
 
 @pytest.mark.parametrize(
-    "test_input, expected_call_count",
+    "tunnel_type, expected_call_count",
     [
         # testcase 1: Check if GRE tunnel is detected
         # and evidence is set
-        ({"flow": {"tunnel_type": "Tunnel::GRE"}}, True),
+        ("Tunnel::GRE", 1),
         # testcase 2: Check if non-GRE tunnel is ignored
-        ({"flow": {"tunnel_type": "Tunnel::IP"}}, False),
+        ("Tunnel::IP", 0),
         # testcase 3: Check if invalid tunnel type is ignored
-        ({"flow": {"tunnel_type": "Invalid"}}, False),
+        ("Invalid", 0),
     ],
 )
-def test_check_GRE_tunnel(mocker, test_input, expected_call_count):
+def test_check_gre_tunnel(mocker, tunnel_type, expected_call_count):
     """
     Tests the check_gre_tunnel function for various
     tunnel types and checks if the evidence is set correctly.
     """
     tunnel = ModuleFactory().create_tunnel_analyzer_obj()
     mock_set_evidence = mocker.patch(
-        "modules.flowalerts.set_evidence.SetEvidnceHelper.GRE_tunnel"
+        "modules.flowalerts.set_evidence.SetEvidnceHelper.gre_tunnel"
     )
-    tunnel.check_gre_tunnel(test_input)
+    flow = Tunnel(
+        starttime="1726655400.0",
+        uid="1234",
+        saddr="192.168.0.1",
+        daddr="10.0.0.1",
+        sport="",
+        dport="",
+        tunnel_type=tunnel_type,
+        action="TUNNEL",
+    )
+    tunnel.check_gre_tunnel("timewindow1", flow)
     assert mock_set_evidence.call_count == expected_call_count
 
 
 def test_analyze_with_message(mocker):
     """Tests analyze when flowalerts.get_msg returns data."""
-
+    flow = Tunnel(
+        starttime="1726655400.0",
+        uid="1234",
+        saddr="192.168.0.1",
+        daddr="10.0.0.1",
+        sport="",
+        dport="",
+        tunnel_type="Tunnel::GRE",
+        action="TUNNEL",
+    )
     msg = {
         "channel": "new_tunnel",
-        "data": json.dumps({"tunnel_type": "Tunnel::GRE"}),
+        "data": json.dumps({"twid": "timewindow1", "flow": asdict(flow)}),
     }
     expected_check_gre_call_count = 1
     tunnel = ModuleFactory().create_tunnel_analyzer_obj()
