75c70afe22a1293a1f961a0011f6af00c118c8d3
alya <alyaggomaa@gmail.com>
2024-09-27 17:13:19 +0300

    flowalerts: implement asyncmodule instead of IModule

--- modules/flowalerts/flowalerts.py
+++ modules/flowalerts/flowalerts.py
@@ -1,5 +1,10 @@
+import asyncio
+import inspect
+from asyncio import Task
+from typing import List
+
 from slips_files.common.slips_utils import utils
-from slips_files.common.abstracts.module import IModule
+from slips_files.common.abstracts.async_module import AsyncModule
 from .conn import Conn
 from .dns import DNS
 from .downloaded_file import DownloadedFile
@@ -8,12 +13,11 @@ from .smtp import SMTP
 from .software import Software
 from .ssh import SSH
 from .ssl import SSL
+from .tunnel import Tunnel
 from slips_files.core.helpers.whitelist.whitelist import Whitelist
 
-from .tunnel import Tunnel
 
-
-class FlowAlerts(IModule):
+class FlowAlerts(AsyncModule):
     name = "Flow Alerts"
     description = (
         "Alerts about flows: long connection, successful ssh, "
@@ -33,6 +37,8 @@ class FlowAlerts(IModule):
         self.downloaded_file = DownloadedFile(self.db, flowalerts=self)
         self.tunnel = Tunnel(self.db, flowalerts=self)
         self.conn = Conn(self.db, flowalerts=self)
+        # list of async functions to await before flowalerts shuts down
+        self.tasks: List[Task] = []
 
     def subscribe_to_channels(self):
         channels = (
@@ -51,6 +57,9 @@ class FlowAlerts(IModule):
             channel_obj = self.db.subscribe(channel)
             self.channels.update({channel: channel_obj})
 
+    async def shutdown_gracefully(self):
+        await asyncio.gather(*self.tasks)
+
     def pre_main(self):
         utils.drop_root_privs()
         self.analyzers_map = {
@@ -66,11 +75,23 @@ class FlowAlerts(IModule):
             "new_ssl": [self.ssl.analyze],
         }
 
-    def main(self):
+    async def main(self):
         for channel, analyzers in self.analyzers_map.items():
             msg: dict = self.get_msg(channel)
             if not msg:
                 continue
 
             for analyzer in analyzers:
-                analyzer(msg)
+                # some analyzers are async functions
+                if inspect.iscoroutinefunction(analyzer):
+                    # analyzer will run normally, until it finishes.
+                    # tasks inside this analyzer will run asynchrously,
+                    # and finish whenever they finish, we'll not wait for them
+                    loop = asyncio.get_event_loop()
+                    task = loop.create_task(analyzer(msg))
+                    # to wait for these functions before flowalerts shuts down
+                    self.tasks.append(task)
+                    # Allow the event loop to run the scheduled task
+                    await asyncio.sleep(0)
+                else:
+                    analyzer(msg)
