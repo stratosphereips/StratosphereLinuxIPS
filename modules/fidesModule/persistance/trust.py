436748a4db5a7c325f31aa91d2ba03b88d41ebaa
d-strat <do.dipl.strat@gmail.com>
2024-10-22 16:14:22 +0200

    Adds a function that gives all peers in form of PeerInfo that have recommendation trust above given trash-hold.

--- modules/fidesModule/persistance/trust.py
+++ modules/fidesModule/persistance/trust.py
@@ -22,8 +22,6 @@ from ..utils.time import Time, now
 class SlipsTrustDatabase(TrustDatabase):
     """Trust database implementation that uses Slips redis as a storage."""
 
-    # TODO: [S] implement this
-
     def __init__(self, configuration: TrustModelConfiguration, db : DBManager, sqldb : SQLiteDB):
         super().__init__(configuration)
         self.db = db
@@ -38,7 +36,7 @@ class SlipsTrustDatabase(TrustDatabase):
 
     def get_connected_peers(self) -> List[PeerInfo]:
         """Returns list of peers that are directly connected to the Slips."""
-        json_peers = self.db.get_connected_peers()
+        json_peers = self.db.get_connected_peers() # on no data returns []
         if not json_peers:
             current_peers = self.sqldb.get_connected_peers()
         else:
@@ -48,22 +46,28 @@ class SlipsTrustDatabase(TrustDatabase):
     def get_peers_with_organisations(self, organisations: List[OrganisationId]) -> List[PeerInfo]:
         """Returns list of peers that have one of given organisations."""
         self.sqldb.get_peers_by_organisations(organisations)
-        #TODO implement this for redis
+        #TODO implement this for Redis
 
     def get_peers_with_geq_recommendation_trust(self, minimal_recommendation_trust: float) -> List[PeerInfo]:
         """Returns peers that have >= recommendation_trust then the minimal."""
-        connected_peers = self.get_connected_peers()
+        connected_peers = self.get_connected_peers() # returns data or []
         out = []
-        for peer in connected_peers:
-            td = self.get_peer_trust_data(peer.id)
 
-            if td is not None and td.recommendation_trust >= minimal_recommendation_trust:
-                out.append(peer)
+        # if no peers present in Redis, try SQLite DB
+        if connected_peers:
+            for peer in connected_peers:
+                td = self.get_peer_trust_data(peer.id)
+
+                if td is not None and td.recommendation_trust >= minimal_recommendation_trust:
+                    out.append(peer)
+        else:
+            out = self.sqldb.get_peers_by_minimal_recommendation_trust(minimal_recommendation_trust)
+
         return out
-
 
     def store_peer_trust_data(self, trust_data: PeerTrustData):
         """Stores trust data for given peer - overwrites any data if existed."""
+        # TODO add SQLite backup
         id = trust_data.id
         td_json = json.dumps(trust_data.to_dict())
         self.db.store_peer_trust_data(id, td_json)
@@ -84,20 +88,24 @@ class SlipsTrustDatabase(TrustDatabase):
 
         td_json = self.db.get_peer_trust_data(peer.id)
         if td_json is None:
+            # TODO add SQLite backup
             return None
         return PeerTrustData(**json.loads(td_json))
 
 
     def get_peers_trust_data(self, peer_ids: List[Union[PeerId, PeerInfo]]) -> TrustMatrix:
         """Return trust data for each peer from peer_ids."""
+        # TODO add SQLite backup
         return {peer_id: self.get_peer_trust_data(peer_id) for peer_id in peer_ids}
 
     def cache_network_opinion(self, ti: SlipsThreatIntelligence):
         """Caches aggregated opinion on given target."""
+        # TODO add SQLite backup
         self.db.cache_network_opinion(ti.target, ti.to_dict())
 
     def get_cached_network_opinion(self, target: Target) -> Optional[SlipsThreatIntelligence]:
         """Returns cached network opinion. Checks cache time and returns None if data expired."""
+        # TODO add SQLite backup
         rec = self.db.get_cached_network_opinion(target, self.__configuration.network_opinion_cache_valid_seconds, now())
         if rec is None:
             return None
