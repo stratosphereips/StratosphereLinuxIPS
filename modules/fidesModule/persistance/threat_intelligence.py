eea662d449f2f28543bf14c3d6c72bb7117d2416
d-strat <do.dipl.strat@gmail.com>
2024-10-23 14:18:27 +0200

    Add save() to Slips' Redis database and accommodate get_for() to the changes.

--- modules/fidesModule/persistance/threat_intelligence.py
+++ modules/fidesModule/persistance/threat_intelligence.py
@@ -8,19 +8,24 @@ from ..model.threat_intelligence import SlipsThreatIntelligence
 from ..persistence.threat_intelligence import ThreatIntelligenceDatabase
 
 from slips_files.core.database.database_manager import DBManager
+import json
 
 class SlipsThreatIntelligenceDatabase(ThreatIntelligenceDatabase):
     """Implementation of ThreatIntelligenceDatabase that uses Slips native storage for the TI."""
 
-    def __init__(self, configuration: TrustModelConfiguration, db: DBManager):
+    def __init__(self, configuration: TrustModelConfiguration, db: DBManager, sqldb):
         self.__configuration = configuration
         self.db = db
 
     def get_for(self, target: Target) -> Optional[SlipsThreatIntelligence]:
         """Returns threat intelligence for given target or None if there are no data."""
-        # TODONE: [S] implement this
-        return self.db.get_fides_ti(target)
+        out = self.db.get_fides_ti(target) # returns str containing dumped dict of STI or None
+        if out:
+            out = SlipsThreatIntelligence(**json.loads(out))
+        else:
+            pass #TODO implement SQLite fall back
+        return out
 
     def save(self, ti: SlipsThreatIntelligence):
-        raise(NotImplementedError)
+        self.db.save_fides_ti(ti.target, json.dumps(ti.to_dict()))
 
