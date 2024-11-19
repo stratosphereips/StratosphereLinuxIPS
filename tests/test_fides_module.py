2e845402d28ab2bb0ad75fd2b8a3de20bcaa786c
d-strat <do.dipl.strat@gmail.com>
2024-11-19 12:48:08 +0100

    Add descriptions to tests and fidesModule.py

--- tests/test_fides_module.py
+++ tests/test_fides_module.py
@@ -15,6 +15,10 @@ from modules.http_analyzer.http_analyzer import utils
 from modules.fidesModule.fidesModule import FidesModule
 import requests
 
+"""
+The sqlite database used by and implemented in FidesModule has its own unit tests. You may find them here here: modules/fidesModule/tests/test_sqlite_db.py
+"""
+
 @pytest.fixture
 def cleanup_database():
     # name of the database created by Fides
@@ -31,117 +35,3 @@ def test_pre_main(mocker, cleanup_database):
     mocker.patch("slips_files.common.slips_utils.Utils.drop_root_privs")
     fides_module.pre_main()
     utils.drop_root_privs.assert_called_once()
-
-
-# @pytest.mark.parametrize(
-#     "uri, request_body_len, expected_result",
-#     [
-#         ("/path/to/file", 0, False),  # Non-empty URI
-#         ("/", 100, False),  # Non-zero request body length
-#         ("/", "invalid_length", False),  # Invalid request body length
-#     ],
-# )
-#
-# def test_check_multiple_empty_connections(
-#     uri, request_body_len, expected_result
-# ):
-#     http_analyzer = ModuleFactory().create_http_analyzer_obj()
-#     host = "google.com"
-#     flow = HTTP(
-#         starttime="1726593782.8840969",
-#         uid=str("uid_55"),
-#         saddr="192.168.1.5",
-#         daddr="147.32.80.7",
-#         method="WEIRD_METHOD",
-#         host="google.com",
-#         uri=uri,
-#         version=0,
-#         user_agent="",
-#         request_body_len=request_body_len,
-#         response_body_len=10,
-#         status_code="",
-#         status_msg="",
-#         resp_mime_types="",
-#         resp_fuids="",
-#     )
-#     result = http_analyzer.check_multiple_empty_connections(twid, flow)
-#     assert result is expected_result
-#
-#     if uri == "/" and request_body_len == 0 and expected_result is False:
-#         for i in range(http_analyzer.empty_connections_threshold):
-#             flow = HTTP(
-#                 starttime="1726593782.8840969",
-#                 uid=str(f"uid_{i}"),
-#                 saddr="192.168.1.5",
-#                 daddr="147.32.80.7",
-#                 method="WEIRD_METHOD",
-#                 host="google.com",
-#                 uri=uri,
-#                 version=0,
-#                 user_agent="",
-#                 request_body_len=request_body_len,
-#                 response_body_len=10,
-#                 status_code="",
-#                 status_msg="",
-#                 resp_mime_types="",
-#                 resp_fuids="",
-#             )
-#             http_analyzer.check_multiple_empty_connections(twid, flow)
-#         assert http_analyzer.connections_counter[host] == ([], 0)
-#
-#
-# @pytest.mark.parametrize(
-#     "host, response_body_len, method, expected_result",
-#     [
-#         ("pastebin.com", "invalid_length", "GET", False),
-#         ("8.8.8.8", "1024", "GET", False),
-#         ("pastebin.com", "512", "GET", False),
-#         ("pastebin.com", "2048", "POST", False),
-#         ("pastebin.com", "2048", "GET", True),  # Large download from Pastebin
-#     ],
-# )
-# def test_check_pastebin_downloads(
-#     host, response_body_len, method, expected_result
-# ):
-#     http_analyzer = ModuleFactory().create_http_analyzer_obj()
-#     flow = HTTP(
-#         starttime="1726593782.8840969",
-#         uid=str("uid_1"),
-#         saddr="192.168.1.5",
-#         daddr="147.32.80.7",
-#         method=method,
-#         host="google.com",
-#         uri=host,
-#         version=0,
-#         user_agent="",
-#         request_body_len=5,
-#         response_body_len=response_body_len,
-#         status_code="",
-#         status_msg="",
-#         resp_mime_types="",
-#         resp_fuids="",
-#     )
-#     if host != "pastebin.com":
-#         http_analyzer.db.get_ip_identification.return_value = (
-#             "Not a Pastebin domain"
-#         )
-#     else:
-#         http_analyzer.db.get_ip_identification.return_value = "pastebin.com"
-#         http_analyzer.pastebin_downloads_threshold = 1024
-#     result = http_analyzer.check_pastebin_downloads(twid, flow)
-#     assert result == expected_result
-#
-#
-# @pytest.mark.parametrize(
-#     "mock_response",
-#     [
-#         # Unexpected response format
-#         MagicMock(status_code=200, text="Unexpected response format"),
-#         # Timeout
-#         MagicMock(side_effect=requests.exceptions.ReadTimeout),
-#     ],
-# )
-# def test_get_ua_info_online_error_cases(mock_response):
-#     http_analyzer = ModuleFactory().create_http_analyzer_obj()
-#     with patch("requests.get", return_value=mock_response):
-#         assert http_analyzer.get_ua_info_online(SAFARI_UA) is False
