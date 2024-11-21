053097394aecb030d145b2b5709b99f889fde141
alya <alyaggomaa@gmail.com>
2024-11-21 20:31:09 +0200

    app.py: use the db manager instead of the web interface's own db

--- webinterface/app.py
+++ webinterface/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, current_app
 
 from slips_files.common.parsers.config_parser import ConfigParser
-from .database.database import __database__
+from .database.database import db
 from .database.signals import message_sent
 from .analysis.analysis import analysis
 from .general.general import general
 from .documentation.documentation import documentation
-from .utils import read_db_file
+from .utils import get_open_redis_ports_in_order
 
 
 def create_app():
@@ -22,8 +22,9 @@ app = create_app()
 def read_redis_port():
     """
     is called when changing the db from the button at the top right
+    prints the available redis dbs and ports for the user to choose ffrom
     """
-    res = read_db_file()
+    res = get_open_redis_ports_in_order()
     return {"data": res}
 
 
@@ -34,6 +35,11 @@ def index():
 
 @app.route("/db/<new_port>")
 def get_post_javascript_data(new_port):
+    """
+    is called when the user chooses another db to connect to from the
+    button at the top right (from /redis)
+    should send a msg to update_db() in database.py
+    """
     message_sent.send(
         current_app._get_current_object(), port=int(new_port), dbnumber=0
     )
@@ -45,12 +51,12 @@ def set_pcap_info():
     """
     Set information about the pcap.
     """
-    info = __database__.db.hgetall("analysis")
+    info = db.get_analysis_info()
 
-    profiles = __database__.db.smembers("profiles")
+    profiles = db.get_profiles()
     info["num_profiles"] = len(profiles) if profiles else 0
 
-    alerts_number = __database__.db.get("number_of_alerts")
+    alerts_number = db.get_number_of_alerts_so_far()
     info["num_alerts"] = int(alerts_number) if alerts_number else 0
 
     return info
@@ -58,9 +64,6 @@ def set_pcap_info():
 
 if __name__ == "__main__":
     app.register_blueprint(analysis, url_prefix="/analysis")
-
     app.register_blueprint(general, url_prefix="/general")
-
     app.register_blueprint(documentation, url_prefix="/documentation")
-
     app.run(host="0.0.0.0", port=ConfigParser().web_interface_port)
