58dee958780a31f1b2884854ae9c094270e133cf
alya <alyaggomaa@gmail.com>
2025-05-14 21:30:20 +0300

    update _time_since_last_repoison everytime an arp poison is done

--- modules/arp_poisoner/arp_poisoner.py
+++ modules/arp_poisoner/arp_poisoner.py
@@ -1,12 +1,18 @@
 # SPDX-FileCopyrightText: 2021 Sebastian Garcia <sebastian.garcia@agents.fel.cvut.cz>
 # SPDX-License-Identifier: GPL-2.0-only
+import os
 import time
+from threading import Lock
+
 from scapy.all import ARP, send
 from scapy.layers.l2 import Ether
 from scapy.sendrecv import srp
 
 from slips_files.common.abstracts.module import IModule
+from modules.arp_poisoner.unblocker import ARPUnblocker
 import json
+
+from slips_files.common.slips_utils import utils
 
 
 class Template(IModule):
@@ -21,11 +27,34 @@ class Template(IModule):
             "new_blocking": self.c1,
             "tw_closed": self.c2,
         }
-        self.time_since_last_repoison = time.time()
+        self._time_since_last_repoison = time.time()
+        self.log_file_path = os.path.join(self.output_dir, "arp_poisoning.log")
+        self.blocking_logfile_lock = Lock()
+        # clear it
+        try:
+            open(self.log_file_path, "w").close()
+        except FileNotFoundError:
+            pass
+        self.unblocker = ARPUnblocker(
+            self.db, self.should_stop, self.logger, self.log
+        )
+
+    def log(self, text):
+        """Logs the given text to the blocking log file"""
+        with self.blocking_logfile_lock:
+            with open(self.log_file_path, "a") as f:
+                human_readable_datetime = utils.convert_ts_format(
+                    time.time(), utils.alerts_format
+                )
+                f.write(f"{human_readable_datetime} - {text}\n")
 
     def _is_time_to_repoison(self) -> bool:
         """returns true if 10s passed since the last poison time"""
-        return time.time() - self.time_since_last_repoison >= 10
+        if time.time() - self._time_since_last_repoison >= 10:
+            self._time_since_last_repoison = time.time()
+            return True
+
+        return False
 
     def keep_attackers_poisoned(self):
         """
@@ -33,10 +62,14 @@ class Template(IModule):
         repoisons all ips in self.unblocker.requests
         """
         if not self._is_time_to_repoison():
+            print("@@@@@@@@@@@@@@@@ not _is_time_to_repoison yet!")
             return
+
+        print("@@@@@@@@@@@@@@@@ time to repoison")
 
         # the unblocker will remove ips that should be unblocked from this dict
         for ip in self.unblocker.requests:
+            print(f"@@@@@@@@@@@@@@@@ calling _arp_poison({ip})")
             self._arp_poison(ip)
 
     @staticmethod
@@ -51,17 +84,19 @@ class Template(IModule):
             return answered[0][1].hwsrc
         return None
 
-    def _arp_poison(self, target_ip: str, interval: int = 10):
+    def _arp_poison(self, target_ip: str):
+        print(f"@@@@@@@@@@@@@@@@  _arp_poison is called ({target_ip})")
         fake_mac = "aa:aa:aa:aa:aa:aa"
         gateway_ip: str = self.db.get_gateway_ip()
 
         target_mac: str = self._get_mac(target_ip)
         if not target_mac:
-            print(f"could not resolve MAC for {target_ip}")
+            print(f"@@@@@@@@@@@@ could not resolve MAC for {target_ip}")
             return
 
         print(
-            f"starting ARP poison loop for ip {target_ip} at MAC {target_mac}"
+            f"@@@@@@@@@@@@@@@@@@ poison the target: tell it the gateway is at "
+            f"fake_mac {target_ip} at MAC {target_mac}"
         )
         # poison the target: tell it the gateway is at fake_mac
         pkt1 = ARP(
@@ -72,6 +107,12 @@ class Template(IModule):
             hwsrc=fake_mac,
         )
         send(pkt1, verbose=0)
+
+        print(
+            f"@@@@@@@@@@@@@@@@@@ poison the network: tell everyone that the"
+            f" target is at fake_mac "
+            f" {target_ip} at MAC {target_mac}"
+        )
 
         # poison the network: tell everyone that the target is at fake_mac
         pkt2 = ARP(
@@ -85,15 +126,20 @@ class Template(IModule):
 
     def main(self):
         self.keep_attackers_poisoned()
+
         if msg := self.get_msg("new_blocking"):
             data = json.loads(msg["data"])
             ip = data.get("ip")
-            # tw: int = data.get("tw")
+            tw: int = data.get("tw").replace("timewindow", "")
+            print(
+                f"@@@@@@@@@@@@@@@@ arp poison new blocking requets for "
+                f"{ip} {tw}"
+            )
             # whether this ip is blocked now, or was already blocked, make an
             # unblocking request to either extend its
             # blocking period, or block it until the next timewindow is over.
             self._arp_poison(ip)
-            self.unblocker.add_req(ip)
+            self.unblocker.unblock_request(ip, tw)
 
-        if msg := self.get_msg("tw_closed"):
+        if self.get_msg("tw_closed"):
             self.unblocker.update_requests()
