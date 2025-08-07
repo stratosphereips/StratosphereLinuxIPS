84e68cb1ac8ea4d0d9619707774851b3773c6220
alya <alyaggomaa@gmail.com>
2025-08-07 03:49:02 +0300

    delete unused imports.py

--- slips_files/common/imports.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2021 Sebastian Garcia <sebastian.garcia@agents.fel.cvut.cz>
-# SPDX-License-Identifier: GPL-2.0-only
-# common imports for all modules
-from slips_files.common.parsers.config_parser import ConfigParser
-from slips_files.common.slips_utils import utils
-import multiprocessing
-from slips_files.common.abstracts.imodule import IModule
