7b5069c1c7acfeabf6e5752a3861d34807a2ce5a
alya <alyaggomaa@gmail.com>
2025-08-07 03:17:09 +0300

    iasync_module.py: add a factory method and move all abstracts from imodule.py here

--- slips_files/common/abstracts/iasync_module.py
+++ slips_files/common/abstracts/iasync_module.py
@@ -1,37 +1,172 @@
 # SPDX-FileCopyrightText: 2021 Sebastian Garcia <sebastian.garcia@agents.fel.cvut.cz>
 # SPDX-License-Identifier: GPL-2.0-only
+import sys
+import traceback
+import warnings
 import asyncio
+import os
+import threading
 from asyncio import Task
 from typing import (
+    Dict,
     Callable,
     List,
 )
-from slips_files.common.abstracts.imodule import IModule
+from abc import ABC, abstractmethod
+from argparse import Namespace
+from multiprocessing import Process, Event
+
+from slips_files.common.printer import Printer
+from slips_files.core.database.database_manager import DBManager
+from slips_files.core.output import Output
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
-class IAsyncModule(IModule):
+class IAsyncModule(ABC, Process):
     """
-    An abstract class for asynchronous slips modules
+    An async interface for all slips modules
     """
 
     name = "AsyncModule"
+    description = "Template module"
+    authors = ["Template Author"]
+    # should be filled with the channels each module subscribes to and
+    # their handlers
+    channels = {}
     # by default, all slips modules' main() methods run in a loop
     # unless this is set to False in the module's init()
     should_run_in_a_loop = True
 
-    def __init__(self, *args, **kwargs):
-        IModule.__init__(self, *args, **kwargs)
+    def __init__(
+        self,
+        logger: Output,
+        output_dir,
+        redis_port,
+        termination_event,
+        slips_args,
+        conf,
+        ppid: int,
+        **kwargs,
+    ):
+        Process.__init__(self)
+        self.redis_port = redis_port
+        self.output_dir = output_dir
+        self.msg_received = False
+        # as parsed by arg_parser, these are the cli args
+        self.args: Namespace = slips_args
+        # to be able to access the configuration file
+        self.conf = conf
+        # the parent pid of this module, used for strating the db
+        self.ppid = ppid
+        # used to tell all slips.py children to stop
+        self.termination_event: Event = termination_event
+        self.logger = logger
+        self.printer = Printer(self.logger, self.name)
+        self.keyboard_int_ctr = 0
         self.did_main_run = False
         # list of async functions to await before flowalerts shuts down
         self.tasks: List[Task] = []
-        self.init_args = args
         self.init_kwargs = kwargs
 
+    @classmethod
+    def create(cls, **kwargs):
+        """
+        Creates an instance of the module and initializes it.
+        """
+        # run the __init_ method of this class
+        self = cls(**kwargs)
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        try:
+            self.run_async_function(self.init_db)
+            self.run_async_function(self.init, **self.init_kwargs)
+            self.channel_tracker = self.init_channel_tracker()
+
+            error: bool = self.run_async_function(self.pre_main)
+            if error or self.should_stop():
+                self.run_async_function(
+                    self.gather_tasks_and_shutdown_gracefully
+                )
+                return None
+
+        except KeyboardInterrupt:
+            self.run_async_function(self.gather_tasks_and_shutdown_gracefully)
+            return None
+        except RuntimeError as e:
+            if "Event loop stopped before Future completed" in str(e):
+                self.run_async_function(
+                    self.gather_tasks_and_shutdown_gracefully
+                )
+                return None
+        except Exception:
+            self.print_traceback()
+            return None
+
+        return self
+
+    async def init_db(self):
+        self.db = await DBManager.create(
+            logger=self.logger,
+            output_dir=self.args.output,
+            redis_port=self.redis_port,
+            conf=self.conf,
+            slips_args=self.args,
+            main_pid=int(os.getpid()),
+            # these should only be true if it's the first time ever
+            # creating a db instance in the main.py
+            flush_db=False,
+            start_redis_server=False,
+        )
+        self.pubsub = self.db.pubsub()
+
+    @abstractmethod
     async def init(self, **kwargs):
-        # PS don't call the self.db from any of the modukes' init()
-        # methods, init can't be async, and most db calls are async,
-        # so instead, put your async logic in the pre_main() method
+        """
+        handles the initialization of modules
+        the goal of this is to have one common __init__() for all
+        modules, which is the one in this file, and a different init() per
+        module
+        this init will have access to all keyword args passes when
+        initializing the module
+        """
         ...
+
+    def print(self, *args, **kwargs):
+        return self.printer.print(*args, **kwargs)
+
+    def init_channel_tracker(self) -> Dict[str, Dict[str, bool]]:
+        """
+        tracks if in the last loop, a msg was received in any of the
+        subscribed channels or not
+        the goal of this is to keep looping if only 1 channel did receive
+        a msg, bc it's possible that that 1 channel will receive another msg
+        return a dict with the channel name and the values are either 0 or 1
+        False: received a msg in the last loop for this channel
+        True: didn't receive a msg
+        The goal of this whole thing is to terminate the module only if no
+        channels receive msgs in the last iteration, but keep looping
+        otherwise.
+        """
+        tracker = {}
+        for channel_name in self.channels:
+            tracker[channel_name] = {"msg_received": False}
+        return tracker
+
+    def is_msg_received_in_any_channel(self) -> bool:
+        """
+        return True if a msg was received in any channel of the ones
+        this module is subscribed to
+        """
+        return any(
+            info["msg_received"] for info in self.channel_tracker.values()
+        )
+
+    def print_traceback(self):
+        exception_line = sys.exc_info()[2].tb_lineno
+        self.print(f"Problem in line {exception_line}", 0, 1)
+        self.print(traceback.format_exc(), 0, 1)
 
     def create_task(self, func, *args) -> Task:
         """
@@ -64,16 +199,6 @@ class IAsyncModule(IModule):
             pass
         except Exception:
             self.print_traceback()
-
-    async def pre_main(self): ...
-
-    async def main(self): ...
-
-    async def shutdown_gracefully(self):
-        """
-        Implement the async shutdown logic here
-        """
-        pass
 
     async def gather_tasks_and_shutdown_gracefully(self):
         await asyncio.gather(*self.tasks, return_exceptions=True)
@@ -139,36 +264,58 @@ class IAsyncModule(IModule):
             channel, data = msg
             self.run_async_function(self.call_msg_handler, channel, data)
 
+    def create_thread(self, func: Callable, *args, **kwargs):
+        """
+        to ensure all created threads by all modules have their own
+        event loop
+        """
+
+        def wrapper(*args, **kwargs):
+            asyncio.run(func(*args, **kwargs))
+
+        t = threading.Thread(
+            target=wrapper, args=args, kwargs=kwargs, name=func.__name__
+        )
+        t.daemon = True
+        return t
+
+    def should_stop(self) -> bool:
+        """
+        The module should stop on the following 2 conditions
+        1. no new msgs are received in any of the channels the
+            module is subscribed to
+        2. the termination event is set by the process_manager.py
+        """
+        if (
+            self.is_msg_received_in_any_channel()
+            or not self.termination_event.is_set()
+        ):
+            # this module is still receiving msgs,
+            # don't stop
+            return False
+        return True
+
+    async def shutdown_gracefully(self):
+        """
+        Tells slips.py that this module is
+        done processing and does necessary cleanup
+        """
+        pass
+
+    async def pre_main(self) -> bool:
+        """
+        This function is for initializations that are
+        executed once before the main loop
+        """
+
+    @abstractmethod
+    async def main(self):
+        """
+        Main function of every module, all the logic implemented
+        here will be executed in a loop
+        """
+
     def run(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        try:
-            self.run_async_function(self.init, **self.init_kwargs)
-            # should be after the module's init() so the module has a chance to
-            # set its own channels
-            self.channel_tracker = self.init_channel_tracker()
-
-            error: bool = self.run_async_function(self.pre_main)
-            if error or self.should_stop():
-                self.run_async_function(
-                    self.gather_tasks_and_shutdown_gracefully
-                )
-                return
-
-        except KeyboardInterrupt:
-            self.run_async_function(self.gather_tasks_and_shutdown_gracefully)
-            return
-        except RuntimeError as e:
-            if "Event loop stopped before Future completed" in str(e):
-                self.run_async_function(
-                    self.gather_tasks_and_shutdown_gracefully
-                )
-                return
-        except Exception:
-            self.print_traceback()
-            return
-
         while True:
             try:
                 if self.should_stop():
@@ -180,7 +327,7 @@ class IAsyncModule(IModule):
                 self.dispatch_msgs()
 
                 if not self.should_run_in_a_loop and self.did_main_run:
-                    continue
+                    return
 
                 # if a module's main() returns 1, it means there's an
                 # error and it needs to stop immediately
@@ -209,3 +356,8 @@ class IAsyncModule(IModule):
             except Exception:
                 self.print_traceback()
                 return
+
+    def __del__(self):
+        # each module has its own sqlite db connection. once this module is
+        # done the connection should be closed
+        self.db.close_sqlite()
