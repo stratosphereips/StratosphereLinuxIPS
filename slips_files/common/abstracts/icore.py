b9f4d165f813e8a2ac126f10e8856d4e29ed8303
alya <alyaggomaa@gmail.com>
2025-11-26 23:04:58 +0200

    store the flows processed per second for the pinput and profiler

--- slips_files/common/abstracts/icore.py
+++ slips_files/common/abstracts/icore.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2021 Sebastian Garcia <sebastian.garcia@agents.fel.cvut.cz>
 # SPDX-License-Identifier: GPL-2.0-only
+import time
 import traceback
 from multiprocessing import Process
 
@@ -26,6 +27,41 @@ class ICore(IModule, Process):
         IModule.__init__(self, *args, **kwargs)
 
     def pre_main(self): ...
+
+    def did_five_mins_pass_since_last_fps_check(self) -> bool:
+        """
+        returns true if 5 mins passed since the last time we checked
+        the flows read per second
+        """
+        if not hasattr(self, "last_fps_check_time"):
+            # first time checking
+            self.last_fps_check_time = time.time()
+            return False
+
+        now = time.time()
+        diff = now - self.last_fps_check_time
+        return diff >= 300
+
+    def store_flows_read_per_second(self):
+        """
+        updates the db about the flows read per second
+        """
+        if not self.did_five_mins_pass_since_last_fps_check():
+            return
+
+        now = time.time()
+        flows_now = self.lines
+
+        # delta since last check
+        flows_delta = flows_now - self.last_flows_count
+        time_delta = now - self.last_fps_check_time
+
+        flows_per_sec = int(flows_delta / time_delta)
+
+        self.db.store_module_flows_per_second(self.name, flows_per_sec)
+
+        self.last_fps_check_time = now
+        self.last_flows_count = flows_now
 
     def run(self):
         """
