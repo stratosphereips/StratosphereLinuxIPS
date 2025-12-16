68269dc87ce8012f324d725903d975af3011a671
alya <alyaggomaa@gmail.com>
2025-12-16 14:48:35 +0200

    FlowQuery: Add valiation for the query attributes and make it return a ready-to-use redis key in __repr__

--- slips_files/core/structures/flow_attributes.py
+++ slips_files/core/structures/flow_attributes.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
 from enum import Enum, auto
+from typing import Optional
 
-from slips_files.core.structures.evidence import Direction
+from slips_files.core.structures.evidence import (
+    Direction,
+    ProfileID,
+    TimeWindow,
+)
 
 
 # These Enums are used to construct a redis query in an organized way.
@@ -81,40 +86,99 @@ class Request(Enum):
 @dataclass(frozen=True, slots=True)
 class FlowQuery:
     """
-    Describes a flow query, splitted and categorized and ready to be processed
-    by redis.
+    Describes a Redis flow query and is able to fully render itself
+    into the Redis key format via __repr__.
+
+    Redis key format:
+        profile_{ip}_{tw}:
+            {role}:{protocol}:{state}:{dir}_{key_type}[:<{ip}|{port}>]:{request}
+
+    Examples:
+    profile_1.1.1.1_timewindow2:client:tcp:not_est:dst:ports:dst_ips
+    profile_1.1.1.1_timewindow2:server:udp:est:src:ips:192.168.1.2:dst_ports
 
     The combination of `key_type` and `request` is constrained so that
     the request attr is always the opposite dimension of `key_type`
     (e.g. if the request is PORT → the key_type should always be *_IPS).
+    AND you can't use a request without specifying an IP/port attr
     """
+
+    profileid: ProfileID
+    timewindow: TimeWindow
 
     direction: Direction
     state: State
     protocol: Protocol
     role: Role
+
     key_type: KeyType
     # this should be the answer to the following question:
     # what do i want to know about this profile & tw?
     # what kind of values is expected as the return of this query? do i
     # want dst ips? dst ports? etc.
-    request: Request
+    request: Optional[Request]
+
+    # optional query filters
+    ip: Optional[str] = None
+    port: Optional[int] = None
 
     def __post_init__(self):
-        if self.key_type == KeyType.PORT and self.request not in {
-            Request.SRC_IPS,
-            Request.DST_IPS,
-        }:
+        #  key_type and request consistency
+        if self.key_type == KeyType.PORT:
+            if self.request not in {Request.SRC_IPS, Request.DST_IPS}:
+                raise ValueError(
+                    "When key_type=PORT, request must be SRC_IPS or DST_IPS"
+                )
+
+        if self.key_type == KeyType.IP:
+            if self.request not in {Request.SRC_PORTS, Request.DST_PORTS}:
+                raise ValueError(
+                    "When key_type=IP, request must be SRC_PORTS or DST_PORTS"
+                )
+
+        # filter validation
+        if self.ip is not None and self.port is not None:
+            raise ValueError("Only one of ip or port may be set")
+
+        if self.ip is not None and self.key_type != KeyType.IP:
+            raise ValueError("ip filter requires key_type=IP")
+
+        if self.port is not None and self.key_type != KeyType.PORT:
+            raise ValueError("port filter requires key_type=PORT")
+
+        if not self.request and (self.ip or self.port):
             raise ValueError(
-                f"Invalid self.request {self.request}. must "
-                f"be [Request.SRC_IPS | Request.DST_IPS]"
+                f"Using request {self.request} requires ip or " f"port."
             )
 
-        if self.key_type == KeyType.IP and self.request not in {
-            Request.SRC_PORTS,
-            Request.DST_PORTS,
-        }:
-            raise ValueError(
-                f"Invalid self.request {self.request}. must "
-                f"be [Request.SRC_PORTS | Request.DST_PORTS]"
-            )
+    def __repr__(self) -> str:
+        role = self.role.name.lower()
+        protocol = self.protocol.name.lower()
+        state = self.state.name.lower()
+        direction = self.direction.name.lower()
+        key_type = self.key_type.name.lower()
+        # request example:
+        # if key_type=PORT: request will be IP
+        # if key_type=IP: request will be PORT
+        request = self.request.name.lower()
+
+        base = (
+            f"{self.profileid}_{self.timewindow}"
+            f":{role}:{protocol}:{state}:{direction}_{key_type}"
+        )
+
+        if self.port is not None:
+            # e.g something like
+            # profile_1.1.1.1_timewindow2:server:udp:est:src:ips:192
+            # .168.12.2:dst_ports
+            # aka get me all the dst ports my priv ip connected to 1.1.1.1 on
+            return f"{base}:{self.port}:{request}"
+
+        if self.ip is not None:
+            # e.g something like
+            # profile_1.1.1.1_timewindow2:server:udp:est:src:ips:192
+            # .168.12.2:dst_ports
+            # aka get me all the dst ports my priv ip connected to 1.1.1.1 on
+            return f"{base}:{self.ip}:{request}"
+
+        return f"{base}"
