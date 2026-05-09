ae0d29e8eff35d0eac7379f0d5fda5e774adb7e6
alya <alyaggomaa@gmail.com>
2026-05-09 03:01:03 +0300

    sanitize iptables cmds before executing them

--- modules/blocking/exec_iptables_cmd.py
+++ modules/blocking/exec_iptables_cmd.py
@@ -1,7 +1,17 @@
+import ipaddress
 import os
+from typing import Dict
+
+from slips_files.common.slips_utils import utils
 
 
-def exec_iptables_command(sudo: str, action, ip_to_block, flag, options):
+def exec_iptables_command(
+    sudo: str,
+    action: str,
+    ip_to_block: str,
+    flag: str,
+    options: Dict[str, str],
+):
     """
     Constructs the iptables rule/command based on the options sent
 
@@ -12,14 +22,24 @@ def exec_iptables_command(sudo: str, action, ip_to_block, flag, options):
       insert : to insert a new rule at the top of slipsBlocking list
       delete : to delete an existing rule
     """
+    # sanitize cmd params
+    if action not in ("insert", "delete"):
+        return 1
+
+    if flag not in ("-s", "-d"):
+        return 1
+
+    if not ipaddress.ip_address(ip_to_block):
+        return 1
+
     command = (
         f"{sudo} iptables --{action} slipsBlocking {flag} {ip_to_block} "
         f'-m comment --comment "Slips rule" >/dev/null 2>&1'
     )
     # Add the options constructed in block_ip or unblock_ip to the
     # iptables command
-    for key in options.keys():
-        command += options[key]
+    for cmd_parameter in options.values():
+        command += utils.sanitize(cmd_parameter)
     command += " -j DROP"
 
     exit_status = os.system(command)
