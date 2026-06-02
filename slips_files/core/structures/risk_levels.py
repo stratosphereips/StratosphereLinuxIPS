cb89489c4142a716c2c39e1e7acf498b2cf75a55
alya <alyaggomaa@gmail.com>
2026-06-03 01:49:13 +0300

    use tuples as RiskLevels instead of floats to make it more clear that each level is a range

--- slips_files/core/structures/risk_levels.py
+++ slips_files/core/structures/risk_levels.py
@@ -1,13 +1,21 @@
 from enum import Enum
+from math import inf
 
 
 class RiskLevel(Enum):
     """
     This risk level determines the sensitivity of slips.
-    the more risk -> the more sensitive slips is -> the more alerts are
-    generated
+    the more risk -> the more sensitive slips is -> the more alerts are generated
     """
 
-    LOW = 0
-    MEDIUM = 0.15
-    HIGH = 0.25
+    LOW = (0.0, 0.15)
+    MEDIUM = (0.15, 0.25)
+    HIGH = (0.25, inf)
+
+    @property
+    def min_score(self):
+        return self.value[0]
+
+    @property
+    def max_score(self):
+        return self.value[1]
