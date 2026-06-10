8c6fb4a657659afc8f374976fdd9daa9ac27e980
alya <alyaggomaa@gmail.com>
2026-06-10 21:02:46 +0300

    to avoid having "unknown port" evidence per scanned port, mark the scanner in th db as "scanner" for 3hrs. (some scans can take up to 3 tws, slips needs to keep track of scanners longer)

--- slips_files/core/database/redis_db/constants.py
+++ slips_files/core/database/redis_db/constants.py
@@ -121,6 +121,7 @@ class Constants:
     FLOWS_PER_MINUTE_MODULES = "flows_per_minute_modules"
     FLOWS_PER_MINUTE_LAST_LOGGED = "flows_per_minute_last_logged"
     FLOWS_PER_MINUTE_LOG_LOCK = "flows_per_minute_log_lock"
+    DETECTED_DOING_PORTSCAN = "detected_doing_port_scan"
 
 
 class Channels:
