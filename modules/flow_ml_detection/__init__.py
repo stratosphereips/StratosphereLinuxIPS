6fe31144e401c365e271712bf344ca32feb41693
Seba Garcia <sebastian.garcia@agents.fel.cvut.cz>
2026-08-22 18:59:13 +0200

    Remove obsolete model and scaler binaries along with their licenses from flow_ml_detection module

--- modules/flow_ml_detection/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2021 Sebastian Garcia <sebastian.garcia@agents.fel.cvut.cz>
-# SPDX-License-Identifier: GPL-2.0-only
-# This file is part of Viper - https://github.com/botherder/viper
-# See the file 'LICENSE' for copying permission.
