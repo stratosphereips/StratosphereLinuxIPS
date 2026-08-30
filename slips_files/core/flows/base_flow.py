eb3d6fa0981a7639a068c3edef71ba98536b0da9
Seba Garcia <sebastian.garcia@agents.fel.cvut.cz>
2026-08-30 23:01:43 +0200

    Add flow_tags field to BaseFlow for enhanced flow management

--- slips_files/core/flows/base_flow.py
+++ slips_files/core/flows/base_flow.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2021 Sebastian Garcia <sebastian.garcia@agents.fel.cvut.cz>
 # SPDX-License-Identifier: GPL-2.0-only
 from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass(kw_only=True)
@@ -8,3 +9,4 @@ class BaseFlow:
     """A base class for zeek flows, containing common fields."""
 
     interface: str = field(default="default")
+    flow_tags: List[str] = field(default_factory=list)
