99998e878386c0bab76ca567a153a9ad05187053
Sebastian Garcia <eldraco@gmail.com>
2026-09-09 00:14:29 +0200

    feat: improve protocol handling and ensure integer conversion for packet counts in nfdump

--- slips_files/core/input_profilers/nfdump.py
+++ slips_files/core/input_profilers/nfdump.py
@@ -22,27 +22,31 @@ class Nfdump(IInputType):
 
         def get_value_at(indx, default_=False):
             try:
-                val = nline[indx]
+                val = nline[indx].strip()
                 return val or default_
             except (IndexError, KeyError):
                 return default_
 
         starttime = utils.convert_ts_format(get_value_at(0), "unixtimestamp")
         endtime = utils.convert_ts_format(get_value_at(1), "unixtimestamp")
+        protocol = str(get_value_at(7)).lower()
+        protocol = {"1": "icmp", "6": "tcp", "17": "udp", "58": "icmp6"}.get(
+            protocol, protocol
+        )
         self.flow: NfdumpConn = NfdumpConn(
             starttime=starttime,
             endtime=endtime,
             dur=get_value_at(2),
-            proto=get_value_at(7),
+            proto=protocol,
             saddr=get_value_at(3),
             sport=get_value_at(5),
             dir_=get_value_at(22),
             daddr=get_value_at(4),
             dport=get_value_at(6),
             state=get_value_at(8),
-            spkts=get_value_at(11),
-            dpkts=get_value_at(13),
-            sbytes=get_value_at(12),
-            dbytes=get_value_at(14),
+            spkts=int(get_value_at(11, 0)),
+            dpkts=int(get_value_at(13, 0)),
+            sbytes=int(get_value_at(12, 0)),
+            dbytes=int(get_value_at(14, 0)),
         )
         return self.flow, ""
